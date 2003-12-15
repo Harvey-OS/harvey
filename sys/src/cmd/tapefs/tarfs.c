f0a4034c8173d6ce9ca220f8ae3e29fbedc1b801
David du Colombier <0intro@gmail.com>
2003-12-15 05:00:00 +0000

    Plan 9 from Bell Labs 2003-12-15

--- sys/src/cmd/tapefs/tarfs.c
+++ sys/src/cmd/tapefs/tarfs.c
@@ -56,7 +56,9 @@ populate(char *name)
 		f.mdate = strtoul(dblock.dbuf.mtime, 0, 8);
 		chksum = strtoul(dblock.dbuf.chksum, 0, 8);
 		/* the mode test is ugly but sometimes necessary */
-		if (dblock.dbuf.linkflag == '5' || (f.mode&0170000) == 040000)
+		if (dblock.dbuf.linkflag == '5'
+		|| (f.mode&0170000) == 040000
+		||  strrchr(dblock.dbuf.name, '\0')[-1] == '/')
 			f.mode |= DMDIR;
 		f.mode &= DMDIR|0777;
 		linkflg = dblock.dbuf.linkflag=='s' || dblock.dbuf.linkflag=='1';
