f42a0a310684e4fde198d0f0261cc6243e7b2b2a
David du Colombier <0intro@gmail.com>
2003-12-18 05:00:00 +0000

    Plan 9 from Bell Labs 2003-12-18

--- sys/src/9/port/mul64fract.c
+++ sys/src/9/port/mul64fract.c
@@ -1,11 +1,22 @@
 #include <u.h>
 
-// multiply two 64 numbers and return the middle 64 bits of the 128 bit result.
-// the assummption is that one of the numbers is a fixed point number with the
-// decimal to the left of the low order 32 bits.
-//
-// there should be an assembler version of this routine for each architecture.
-// this one is provided to make ports easier.
+/* mul64fract(uvlong*r, uvlong a, uvlong b)
+ *
+ * Multiply two 64 numbers and return the middle 64 bits of the 128 bit result.
+ *
+ * The assumption is that one of the numbers is a 
+ * fixed point number with the integer portion in the
+ * high word and the fraction in the low word.
+ *
+ * There should be an assembler version of this routine
+ * for each architecture.  This one is intended to
+ * make ports easier.
+ *
+ *	ignored		r0 = lo(a0*b0)
+ *	lsw of result	r1 = hi(a0*b0) +lo(a0*b1) +lo(a1*b0)
+ *	msw of result	r2 = 		hi(a0*b1) +hi(a1*b0) +lo(a1*b1)
+ *	ignored		r3 = hi(a1*b1)
+ */
 
 void
 mul64fract(uvlong *r, uvlong a, uvlong b)
--- sys/src/libdraw/init.c
+++ sys/src/libdraw/init.c
@@ -387,7 +387,7 @@ drawerror(Display *d, char *s)
 {
 	char err[ERRMAX];
 
-	if(d->error)
+	if(d && d->error)
 		d->error(d, s);
 	else{
 		errstr(err, sizeof err);
