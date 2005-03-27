c760013fa6d5dacb018ba60049536709d4b6a4c6
David du Colombier <0intro@gmail.com>
2005-03-27 07:00:04 +0200

    Plan 9 from Bell Labs Fourth Edition 2005-03-27

--- sys/src/9/mtx/fns.h
+++ sys/src/9/mtx/fns.h
@@ -1,14 +1,11 @@
 #include "../port/portfns.h"
 
-void	archinit(void);
 int	cistrcmp(char*, char*);
 int	cistrncmp(char*, char*, int);
-void	clockcheck(void);
 void	clockinit(void);
 void	clockintr(Ureg*);
 void	clockintrsched(void);
 #define coherence()	eieio()
-void	cpuidentify(void);
 void	cpuidprint(void);
 #define cycles(x)
 void	dcflush(void*, ulong);
@@ -18,22 +15,16 @@ void	delayloopinit(void);
 void	eieio(void);
 void	evenaddr(ulong);
 void	faultpower(Ureg*, ulong addr, int read);
-void	fpinit(void);
 void	fprestore(FPsave*);
 void	fpsave(FPsave*);
-void	fptrap(Ureg*);
 char*	getconf(char*);
 ulong	getdar(void);
 ulong	getdec(void);
-ulong	getdepn(void);
 ulong	getdsisr(void);
 ulong	gethid0(void);
 ulong	gethid1(void);
-ulong	getimmr(void);
 ulong	getmsr(void);
 ulong	getpvr(void);
-ulong	gettbl(void);
-ulong	gettbu(void);
 void	gotopc(ulong);
 int	havetimer(void);
 void	hwintrinit(void);
@@ -44,7 +35,6 @@ int	i8259enable(Vctl*);
 int	i8259vecno(int);
 int	i8259disable(int);
 void	icflush(void*, ulong);
-void	idle(void);
 #define	idlehands()			/* nothing to do in the runproc */
 int	inb(int);
 void	insb(int, void*, int);
@@ -60,12 +50,9 @@ void	ioinit(void);
 int	iprint(char*, ...);
 int	isaconfig(char*, int, ISAConf*);
 void	kbdinit(void);
-void	kbdreset(void);
-void	kernelmmu(void);
 #define kexit(a)
 #define	kmapinval()
 void	links(void);
-void	mathinit(void);
 void	mmuinit(void);
 void	mmusweep(void*);
 void	mpicdisable(int);
@@ -104,9 +91,6 @@ void	putmsr(ulong);
 void	putsdr1(ulong);
 void	putsr(int, ulong);
 void	raveninit(void);
-void	screeninit(void);
-int	screenprint(char*, ...);			/* debugging */
-int	segflush(void*, ulong);
 void	sync(void);
 int	tas(void*);
 void	timeradd(Timer *);
@@ -116,11 +100,8 @@ void	trapinit(void);
 void	trapvec(void);
 void	tlbflush(ulong);
 void	tlbflushall(void);
-void	uartinstall(void);
-void	uartwait(void);	/* debugging */
 #define	userureg(ur) (((ur)->status & MSR_PR) != 0)
 void	watchreset(void);
-void	wbflush(void);
 
 #define	waserror()	(up->nerrlab++, setlabel(&up->errlab[up->nerrlab-1]))
 #define KADDR(a)	((void*)((ulong)(a)|KZERO))
