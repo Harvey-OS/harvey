00580da515719bdf55c166b53ba7eac22c2e4dd7
David du Colombier <0intro@gmail.com>
2007-09-12 06:00:06 +0200

    Plan 9 from Bell Labs Fourth Edition 2007-09-12

--- sys/src/cmd/fossil/fns.h
+++ sys/src/cmd/fossil/fns.h
@@ -1,105 +1,106 @@
-Source* sourceRoot(Fs*, u32int, int);
-Source* sourceOpen(Source*, ulong, int, int);
-Source* sourceCreate(Source*, int, int, u32int);
-Block* sourceBlock(Source*, ulong, int);
-Block* _sourceBlock(Source*, ulong, int, int, ulong);
-int sourceGetEntry(Source*, Entry*);
-int sourceSetEntry(Source*, Entry*);
-int sourceSetSize(Source*, uvlong);
-uvlong sourceGetSize(Source*);
-int sourceSetDirSize(Source*, ulong);
-ulong sourceGetDirSize(Source*);
-int sourceTruncate(Source*);
-int sourceRemove(Source*);
-void sourceClose(Source*);
-int sourceLock(Source*, int);
-void sourceUnlock(Source*);
-int sourceLock2(Source*, Source*, int);
+Block*	sourceBlock(Source*, ulong, int);
+Block*	_sourceBlock(Source*, ulong, int, int, ulong);
+void	sourceClose(Source*);
+Source*	sourceCreate(Source*, int, int, u32int);
+ulong	sourceGetDirSize(Source*);
+int	sourceGetEntry(Source*, Entry*);
+uvlong	sourceGetSize(Source*);
+int	sourceLock2(Source*, Source*, int);
+int	sourceLock(Source*, int);
+char	*sourceName(Source *s);
+Source*	sourceOpen(Source*, ulong, int, int);
+int	sourceRemove(Source*);
+Source*	sourceRoot(Fs*, u32int, int);
+int	sourceSetDirSize(Source*, ulong);
+int	sourceSetEntry(Source*, Entry*);
+int	sourceSetSize(Source*, uvlong);
+int	sourceTruncate(Source*);
+void	sourceUnlock(Source*);
 
-Cache* cacheAlloc(Disk*, VtSession*, ulong, int);
-void cacheCountUsed(Cache*, u32int, u32int*, u32int*, u32int*);
-void cacheFree(Cache*);
-Block* cacheLocal(Cache*, int, u32int, int);
-Block* cacheLocalData(Cache*, u32int, int, u32int, int, u32int);
-Block* cacheGlobal(Cache*, uchar[VtScoreSize], int, u32int, int);
-Block* cacheAllocBlock(Cache*, int, u32int, u32int, u32int);
-void cacheFlush(Cache*, int);
-u32int cacheLocalSize(Cache*, int);
-int cacheDirty(Cache*);
-int readLabel(Cache*, Label*, u32int addr);
+Block*	cacheAllocBlock(Cache*, int, u32int, u32int, u32int);
+Cache*	cacheAlloc(Disk*, VtSession*, ulong, int);
+void	cacheCountUsed(Cache*, u32int, u32int*, u32int*, u32int*);
+int	cacheDirty(Cache*);
+void	cacheFlush(Cache*, int);
+void	cacheFree(Cache*);
+Block*	cacheGlobal(Cache*, uchar[VtScoreSize], int, u32int, int);
+Block*	cacheLocal(Cache*, int, u32int, int);
+Block*	cacheLocalData(Cache*, u32int, int, u32int, int, u32int);
+u32int	cacheLocalSize(Cache*, int);
+int	readLabel(Cache*, Label*, u32int addr);
 
-Block* blockCopy(Block*, u32int, u32int, u32int);
-void blockDupLock(Block*);
-void blockPut(Block*);
-void blockDependency(Block*, Block*, int, uchar*, Entry*);
-int blockDirty(Block*);
-void blockRemoveLink(Block*, u32int, int, u32int, int);
-int blockSetLabel(Block*, Label*, int);
-Block* _blockSetLabel(Block*, Label*);
-void blockSetIOState(Block*, int);
-int blockWrite(Block*);
-uchar* blockRollback(Block*, uchar*);
+Block*	blockCopy(Block*, u32int, u32int, u32int);
+void	blockDependency(Block*, Block*, int, uchar*, Entry*);
+int	blockDirty(Block*);
+void	blockDupLock(Block*);
+void	blockPut(Block*);
+void	blockRemoveLink(Block*, u32int, int, u32int, int);
+uchar*	blockRollback(Block*, uchar*);
+void	blockSetIOState(Block*, int);
+Block*	_blockSetLabel(Block*, Label*);
+int	blockSetLabel(Block*, Label*, int);
+int	blockWrite(Block*);
 
-Disk* diskAlloc(int);
-void diskFree(Disk*);
-int diskReadRaw(Disk*, int, u32int, uchar*);
-int diskWriteRaw(Disk*, int, u32int, uchar*);
-void diskRead(Disk*, Block*);
-void diskWrite(Disk*, Block*);
-void	diskWriteAndWait(Disk*, Block*);
-int diskFlush(Disk*);
-u32int diskSize(Disk*, int);
-int diskBlockSize(Disk*);
+Disk*	diskAlloc(int);
+int	diskBlockSize(Disk*);
+int	diskFlush(Disk*);
+void	diskFree(Disk*);
+void	diskRead(Disk*, Block*);
+int	diskReadRaw(Disk*, int, u32int, uchar*);
+u32int	diskSize(Disk*, int);
+void	diskWriteAndWait(Disk*,	Block*);
+void	diskWrite(Disk*, Block*);
+int	diskWriteRaw(Disk*, int, u32int, uchar*);
 
-char* bsStr(int);
-char* bioStr(int);
-char* btStr(int);
-u32int globalToLocal(uchar[VtScoreSize]);
-void localToGlobal(u32int, uchar[VtScoreSize]);
+char*	bioStr(int);
+char*	bsStr(int);
+char*	btStr(int);
+u32int	globalToLocal(uchar[VtScoreSize]);
+void	localToGlobal(u32int, uchar[VtScoreSize]);
 
-int headerUnpack(Header*, uchar*);
-void headerPack(Header*, uchar*);
+void	headerPack(Header*, uchar*);
+int	headerUnpack(Header*, uchar*);
 
-int labelFmt(Fmt*);
-int labelUnpack(Label*, uchar*, int);
-void labelPack(Label*, uchar*, int);
+int	labelFmt(Fmt*);
+void	labelPack(Label*, uchar*, int);
+int	labelUnpack(Label*, uchar*, int);
 
-int scoreFmt(Fmt*);
+int	scoreFmt(Fmt*);
 
-int superUnpack(Super*, uchar*);
-void superPack(Super*, uchar*);
+void	superPack(Super*, uchar*);
+int	superUnpack(Super*, uchar*);
 
-int entryUnpack(Entry*, uchar*, int);
-void entryPack(Entry*, uchar*, int);
-int entryType(Entry*);
+void	entryPack(Entry*, uchar*, int);
+int	entryType(Entry*);
+int	entryUnpack(Entry*, uchar*, int);
 
 Periodic* periodicAlloc(void (*)(void*), void*, int);
-void periodicKill(Periodic*);
+void	periodicKill(Periodic*);
 
-File* fileRoot(Source*);
-int fileSnapshot(File*, File*, u32int, int);
-int fileGetSources(File*, Entry*, Entry*);
-int mkVac(VtSession*, uint, Entry*, Entry*, DirEntry*, uchar[VtScoreSize]);
-int fsNextQid(Fs*, u64int*);
-Block* superGet(Cache*, Super*);
+int	fileGetSources(File*, Entry*, Entry*);
+File*	fileRoot(Source*);
+int	fileSnapshot(File*, File*, u32int, int);
+int	fsNextQid(Fs*, u64int*);
+int	mkVac(VtSession*, uint, Entry*, Entry*, DirEntry*, uchar[VtScoreSize]);
+Block*	superGet(Cache*, Super*);
 
-Arch* archInit(Cache*, Disk*, Fs*, VtSession*);
-void archFree(Arch*);
-void archKick(Arch*);
+void	archFree(Arch*);
+Arch*	archInit(Cache*, Disk*, Fs*, VtSession*);
+void	archKick(Arch*);
 
-void bwatchLock(Block*);
-void bwatchUnlock(Block*);
-void bwatchInit(void);
-void bwatchSetBlockSize(uint);
-void bwatchDependency(Block*);
-void bwatchReset(uchar[VtScoreSize]);
+void	bwatchDependency(Block*);
+void	bwatchInit(void);
+void	bwatchLock(Block*);
+void	bwatchReset(uchar[VtScoreSize]);
+void	bwatchSetBlockSize(uint);
+void	bwatchUnlock(Block*);
 
-void initWalk(WalkPtr*, Block*, uint);
-int nextWalk(WalkPtr*, uchar[VtScoreSize], uchar*, u32int*, Entry**);
+void	initWalk(WalkPtr*, Block*, uint);
+int	nextWalk(WalkPtr*, uchar[VtScoreSize], uchar*, u32int*, Entry**);
 
-void snapGetTimes(Snap*, u32int*, u32int*, u32int*);
-void snapSetTimes(Snap*, u32int, u32int, u32int);
+void	snapGetTimes(Snap*, u32int*, u32int*, u32int*);
+void	snapSetTimes(Snap*, u32int, u32int, u32int);
 
-void fsCheck(Fsck*);
+void	fsCheck(Fsck*);
 
 #pragma varargck type "L" Label*
