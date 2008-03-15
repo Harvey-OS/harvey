312bcb687bb7305a339ca57aeef5995948cb54a0
David du Colombier <0intro@gmail.com>
2008-03-15 05:00:00 +0000

    Plan 9 from Bell Labs 2008-03-15

--- sys/include/ape/lib9.h
+++ sys/include/ape/lib9.h
@@ -5,6 +5,7 @@
 #endif
 #pragma lib "/$M/lib/ape/lib9.a"
 
+#include <u.h>		/* ick; need Rune defined below */
 
 #define	MORDER	0x0003	/* mask for bits defining order of mounting */
 #define	MREPL	0x0000	/* mount replaces object */
@@ -36,6 +37,27 @@ enum
 	RFCFDG		= (1<<12),
 	RFREND		= (1<<13)
 };
+
+extern char *argv0;
+#define	ARGBEGIN	for((argv0||(argv0=*argv)),argv++,argc--;\
+			    argv[0] && argv[0][0]=='-' && argv[0][1];\
+			    argc--, argv++) {\
+				char *_args, *_argt;\
+				Rune _argc;\
+				_args = &argv[0][1];\
+				if(_args[0]=='-' && _args[1]==0){\
+					argc--; argv++; break;\
+				}\
+				_argc = 0;\
+				while(*_args && (_args += chartorune(&_argc, _args)))\
+				switch(_argc)
+#define	ARGEND		SET(_argt);USED(_argt,_argc,_args);}USED(argv, argc);
+#define	ARGF()		(_argt=_args, _args="",\
+				(*_argt? _argt: argv[1]? (argc--, *++argv): 0))
+#define	EARGF(x)	(_argt=_args, _args="",\
+				(*_argt? _argt: argv[1]? (argc--, *++argv): ((x), abort(), (char*)0)))
+
+#define	ARGC()		_argc
 
 extern	int	errstr(char*, unsigned int);
 extern	int	bind(char*, char*, int);
