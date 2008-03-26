88d4b9616d9fa7109c9e3a8587cf5456d92e2bb4
David du Colombier <0intro@gmail.com>
2008-03-26 05:00:00 +0000

    Plan 9 from Bell Labs 2008-03-26

--- sys/src/cmd/cdfs/mmc.c
+++ sys/src/cmd/cdfs/mmc.c
@@ -772,6 +772,7 @@ mmcgettoc(Drive *drive)
 	 */
 	mmcreadtoc(drive, 0, 0, resp, sizeof(resp));
 	if(drive->Scsi.changetime == 0) {	/* no media present */
+		drive->mmctype = Mmcnone;
 		drive->ntrack = 0;
 		return 0;
 	}
