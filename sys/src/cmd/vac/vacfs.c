69de934f9ee6d7f50925b51e1f93fdba47dd4bfb
David du Colombier <0intro@gmail.com>
2008-10-10 06:00:10 +0200

    Plan 9 from Bell Labs Fourth Edition 2008-10-10

--- sys/src/cmd/vac/vacfs.c
+++ sys/src/cmd/vac/vacfs.c
@@ -207,7 +207,8 @@ threadmain(int argc, char *argv[])
 		close(p[0]);
 		if(defsrv){
 			srvname = smprint("/srv/%s", defsrv);
-			fd = create(srvname, OWRITE|ORCLOSE, 0666);
+			/* child doesn't share fd group, so can't use ORCLOSE */
+			fd = create(srvname, OWRITE, 0666);
 			if(fd < 0)
 				sysfatal("create %s: %r", srvname);
 			if(fprint(fd, "%d", srvfd) < 0)
@@ -233,7 +234,8 @@ srv(void *a)
 void
 usage(void)
 {
-	fprint(2, "usage: %s [-sd] [-h host] [-c ncache] [-m mountpoint] vacfile\n", argv0);
+	fprint(2, "usage: %s [-sd] [-c ncache] [-h host] [-m mountpoint] [-S svc] vacfile\n",
+		argv0);
 	threadexitsall("usage");
 }
 
