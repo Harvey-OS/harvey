d25feb0baf63765258123b139ca181b3c272b36b
David du Colombier <0intro@gmail.com>
2008-12-04 05:00:00 +0000

    Plan 9 from Bell Labs 2008-12-04

--- sys/src/cmd/venti/srv/arena.c
+++ sys/src/cmd/venti/srv/arena.c
@@ -683,8 +683,9 @@ loadarena(Arena *arena)
 			logerr(ECorrupt, "arena tail name %s head %s", 
 				arena->name, head.name);
 		else if(arena->clumpmagic != head.clumpmagic)
-			logerr(ECorrupt, "arena tail clumpmagic 0x%lux head 0x%lux",
-				(ulong)arena->clumpmagic, (ulong)head.clumpmagic);
+			logerr(ECorrupt, "arena %d tail clumpmagic 0x%lux head 0x%lux",
+				debugarena, (ulong)arena->clumpmagic,
+				(ulong)head.clumpmagic);
 		else if(arena->version != head.version)
 			logerr(ECorrupt, "arena tail version %d head version %d",
 				arena->version, head.version);
