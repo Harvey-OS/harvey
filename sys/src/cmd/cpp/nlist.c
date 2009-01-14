039fad7446581fc6e27b0ff748386ac77bf55626
David du Colombier <0intro@gmail.com>
2009-01-14 05:00:00 +0000

    Plan 9 from Bell Labs 2009-01-14

--- sys/src/cmd/cpp/nlist.c
+++ sys/src/cmd/cpp/nlist.c
@@ -33,6 +33,7 @@ struct	kwtab {
 	"undef",	KUNDEF,		ISKW,
 	"line",		KLINE,		ISKW,
 	"error",	KERROR,		ISKW,
+	"warning",	KWARNING,	ISKW,		// extension to ANSI
 	"pragma",	KPRAGMA,	ISKW,
 	"eval",		KEVAL,		ISKW,
 	"defined",	KDEFINED,	ISDEFINED+ISUNCHANGE,
