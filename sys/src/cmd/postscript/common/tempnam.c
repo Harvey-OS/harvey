c64ac5861bfbc652da7ce1267948128745d12ae0
David du Colombier <0intro@gmail.com>
2009-08-01 05:00:00 +0000

    Plan 9 from Bell Labs 2009-08-01

--- sys/src/cmd/postscript/common/tempnam.c
+++ sys/src/cmd/postscript/common/tempnam.c
@@ -1,27 +1,36 @@
-#include <stdio.h>
-#include <errno.h>
-
 #if defined(V9) || defined(BSD4_2) || defined(plan9)
-char *tempnam(char *dir, char *pfx) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+char *
+tempnam(char *dir, char *pfx)
+{
 	int pid;
-	unsigned int len;
-	char *tnm, *malloc();
+	char *tnm;
+	struct stat stb;
 	static int seq = 0;
 
+	if (dir == NULL)
+		dir = ".";
+#ifdef plan9
+	/* our access emulation has a race when checking for write access */
+	if (access(dir, R_OK|X_OK) == -1)
+#else
+	if (access(dir, R_OK|W_OK|X_OK) == -1)
+#endif
+		return NULL;
 	pid = getpid();
-	len = strlen(dir) + strlen(pfx) + 10;
-	if ((tnm = malloc(len)) != NULL) {
-		sprintf(tnm, "%s", dir);
-		if (access(tnm, 7) == -1)
-			return(NULL);
-		do {
-			sprintf(tnm, "%s/%s%d%d", dir, pfx, pid, seq++);
-			errno = 0;
-			if (access(tnm, 7) == -1)
-				if (errno == ENOENT)
-					return(tnm);
-		} while (1);
-	}
-	return(tnm);
+	tnm = malloc(strlen(dir) + 1 + strlen(pfx) + 2*20 + 1);
+	if (tnm == NULL)
+		return NULL;
+	do {
+		sprintf(tnm, "%s/%s.%d.%d", dir, pfx, pid, seq++);
+		errno = 0;
+	} while (stat(tnm, &stb) >= 0 && seq < 256);
+	return tnm;
 }
 #endif
