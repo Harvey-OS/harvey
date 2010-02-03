6980d84db4808752cf6e88f2733faa300737ece2
David du Colombier <0intro@gmail.com>
2010-02-03 05:00:00 +0000

    Plan 9 from Bell Labs 2010-02-03

--- sys/src/9/kw/usbehci.c
+++ sys/src/9/kw/usbehci.c
@@ -3229,8 +3229,12 @@ ehcireset(Ctlr *ctlr)
 	}
 	dprint("ehci: %d frames\n", ctlr->nframes);
 
-	/* Marvell errata FE-USB-340 workaround */
-	opio->usbmode |= 1 << 4;	/* magic from marvell */
+	/*
+	 * Marvell errata FE-USB-340 workaround: 1 << 4 magic.
+	 * Magic 3 from the linux driver makes it work.  Ick.
+	 */
+	opio->usbmode |= 1 << 4 | 3;
+	coherence();
 
 	/*
 	 * Marvell guideline GL-USB-160.
