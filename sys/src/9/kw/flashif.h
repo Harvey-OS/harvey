e33ca76e45e759b25f195229f683d8542b611960
David du Colombier <0intro@gmail.com>
2010-04-30 05:00:00 +0000

    Plan 9 from Bell Labs 2010-04-30

--- sys/src/9/kw/flashif.h
+++ sys/src/9/kw/flashif.h
@@ -29,6 +29,7 @@ struct Flashregion {
 	ulong	end;
 	ulong	erasesize;
 	ulong	pagesize;	/* if non-0, size of pages within erase block */
+	ulong	spares;		/* spare bytes per page, for ecc, etc. */
 };
 
 /*
