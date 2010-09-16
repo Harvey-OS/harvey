8bfb44c22e4ff22bdecea022b14d1c476d11965f
David du Colombier <0intro@gmail.com>
2010-09-16 11:00:10 +0200

    Plan 9 from Bell Labs Fourth Edition 2010-09-16

--- sys/src/9/omap/trap.c
+++ sys/src/9/omap/trap.c
@@ -385,8 +385,14 @@ irq(Ureg* ureg)
 //			splhi();		/* in case v->f lowered pl */
 			handled++;
 		}
-	if(!handled)
-		print("unknown interrupt: irq %d\n", irqno);
+	if(!handled) {
+		print("unknown interrupt: irq %d", irqno);
+		if(irqno < nelem(vctl)) {
+			intcmask(irqno);
+			print(", now masked");
+		}
+		print("\n");
+	}
 	t = perfticks();
 	ninterrupt++;
 	if(t < ticks)
