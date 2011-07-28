878eda0ae2d925c2f0dab8ab53ff86e43c94ea67
David du Colombier <0intro@gmail.com>
2011-07-28 05:00:00 +0000

    Plan 9 from Bell Labs 2011-07-28

--- sys/src/cmd/usb/disk/disk.c
+++ sys/src/cmd/usb/disk/disk.c
@@ -481,6 +481,13 @@ dread(Usbfs *fs, Fid *fid, void *data, long count, vlong offset)
 		count = usbdirread(fs, q, data, count, offset, dirgen, nil);
 		break;
 	case Qctl:
+		/*
+		 * Some usb disks need an extra opportunity to divulge their
+		 * capacity (e.g. M-Systems/SanDisk 1GB flash drive).
+		 */
+		if(lun->lbsize <= 0)
+			umscapacity(lun);
+
 		s = buf;
 		e = buf + sizeof(buf);
 		if(lun->flags & Finqok)
