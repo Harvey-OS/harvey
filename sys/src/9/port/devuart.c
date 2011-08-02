1f03e0e7d5a8466f045dd72caaff1b9750fa9926
David du Colombier <0intro@gmail.com>
2011-08-02 10:50:07 +0200

    Plan 9 from Bell Labs Fourth Edition 2011-08-02

--- sys/src/9/port/devuart.c
+++ sys/src/9/port/devuart.c
@@ -189,12 +189,15 @@ uartreset(void)
 		uartnuart++;
 	}
 
-	if(uartnuart)
+	if(uartnuart) {
 		uart = xalloc(uartnuart*sizeof(Uart*));
+		if (uart == nil)
+			panic("uartreset: no memory");
+	}
 
 	uartndir = 1 + 3*uartnuart;
 	uartdir = xalloc(uartndir * sizeof(Dirtab));
-	if (uart == nil || uartdir == nil)
+	if (uartdir == nil)
 		panic("uartreset: no memory");
 	dp = uartdir;
 	strcpy(dp->name, ".");
