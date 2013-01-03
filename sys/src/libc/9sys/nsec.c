b1460dae55b517f521cdc586ea9f2e9638d1322c
David du Colombier <0intro@gmail.com>
2013-01-03 22:10:13 +0100

    nsec: import from nix (accepted)
    cleanup nsec not to cache anything.
    
    Change-Id: If0247112892e5e6ea6b92bf5f2607957a935e89d
    Reference: /n/patches.lsub.org/patch/nsec
    Signed-off-by: nemo@lsub.org
    Signed-off-by: David du Colombier <0intro@gmail.com>
    Reviewed-on: http://nxm.coreboot.org/review/245
    Tested-by: Jenkins QA
    Reviewed-by: ron minnich <rminnich@gmail.com>

--- sys/src/libc/9sys/nsec.c
+++ sys/src/libc/9sys/nsec.c
@@ -16,60 +16,19 @@ be2vlong(vlong *to, uchar *f)
 		t[o[i]] = f[i];
 }
 
-static int fd = -1;
-static struct {
-	int	pid;
-	int	fd;
-} fds[64];
-
 vlong
 nsec(void)
 {
 	uchar b[8];
 	vlong t;
-	int pid, i, f, tries;
+	int f;
 
-	/*
-	 * Threaded programs may have multiple procs
-	 * with different fd tables, so we may need to open
-	 * /dev/bintime on a per-pid basis
-	 */
-
-	/* First, look if we've opened it for this particular pid */
-	pid = _tos->pid;
-	do{
-		f = -1;
-		for(i = 0; i < nelem(fds); i++)
-			if(fds[i].pid == pid){
-				f = fds[i].fd;
-				break;
-			}
-		tries = 0;
-		if(f < 0){
-			/* If it's not open for this pid, try the global pid */
-			if(fd >= 0)
-				f = fd;
-			else{
-				/* must open */
-				if((f = open("/dev/bintime", OREAD|OCEXEC)) < 0)
-					return 0;
-				fd = f;
-				for(i = 0; i < nelem(fds); i++)
-					if(fds[i].pid == pid || fds[i].pid == 0){
-						fds[i].pid = pid;
-						fds[i].fd = f;
-						break;
-					}
-			}
-		}
-		if(pread(f, b, sizeof b, 0) == sizeof b){
-			be2vlong(&t, b);
-			return t;
-		}
-		close(f);
-		if(i < nelem(fds))
-			fds[i].fd = -1;
-	}while(tries++ == 0);	/* retry once */
-	USED(tries);
-	return 0;
+	f = open("/dev/bintime", OREAD);
+	if(f < 0)
+		return 0;
+	t = 0;
+	if(pread(f, b, sizeof b, 0) == sizeof b)
+		be2vlong(&t, b);
+	close(f);
+	return t;
 }
