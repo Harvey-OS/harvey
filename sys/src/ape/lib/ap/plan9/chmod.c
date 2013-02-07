354ab865bc3e5dadad52235590e3d69db1e603c7
David du Colombier <0intro@gmail.com>
2013-02-07 10:50:03 +0100

    Plan 9 from Bell Labs Fourth Edition 2013-02-07

--- sys/src/ape/lib/ap/plan9/chmod.c
+++ sys/src/ape/lib/ap/plan9/chmod.c
@@ -4,30 +4,41 @@
 #include "sys9.h"
 #include "dir.h"
 
+static int
+seterrno(void)
+{
+	_syserrno();
+	return -1;
+}
+
 int
 chmod(const char *path, mode_t mode)
 {
-	Dir d;
+	Dir d, *dir;
 
+	dir = _dirstat(path);
+	if(dir == nil)
+		return seterrno();
 	_nulldir(&d);
-	d.mode = mode & 0777;
-	if(_dirwstat(path, &d) < 0){
-		_syserrno();	
-		return -1;
-	}
+	d.mode = (dir->mode & ~0777) | (mode & 0777);
+	free(dir);
+	if(_dirwstat(path, &d) < 0)
+		return seterrno();
 	return 0;
 }
 
 int
 fchmod(int fd, mode_t mode)
 {
-	Dir d;
+	Dir d, *dir;
 
+	dir = _dirfstat(fd);
+	if(dir == nil)
+		return seterrno();
 	_nulldir(&d);
-	d.mode = mode & 0777;
-	if(_dirfwstat(fd, &d) < 0){
-		_syserrno();	
-		return -1;
-	}
+	d.mode = (dir->mode & ~0777) | (mode & 0777);
+	free(dir);
+	if(_dirfwstat(fd, &d) < 0)
+		return seterrno();
 	return 0;
 }
