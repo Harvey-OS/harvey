a6326533a8a55614c0bd432facfb75756904b297
Ronald G. Minnich <rminnich@gmail.com>
2013-03-15 21:35:30 +0100

    Store system call parameters in registers
    Go system calls are really complex right now, because
    our args are on the stack. Akshat has requested that we
    get the system calls in registers. We already do this
    for Linux, so it's not a big deal. We indicate the new
    model by putting 0xc000 in AX, instead of 0x8000.
    
    The next step is reworking how we save things in the assembly
    level system call handling, followed by changes to
    syscall(). This one is trick so we're taking it in steps.
    
    Change-Id: I6b77b8cc5c8a921ea169bf0a9c33c01e3340737d
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    Reviewed-on: http://nxm.coreboot.org/review/405
    Tested-by: Jenkins QA
    Reviewed-by: Akshat Kumar <seed@mail.nanosouffle.net>

--- util/mksys.go
+++ util/mksys.go
@@ -33,7 +33,30 @@ func main(){
 			ass = ass + "\tMOVQ RARG, a0+0(FP)\n"
 		}
 		ass = ass + "\tMOVQ $"+ll[2]+",RARG\n"
-		ass = ass + "\tMOVQ $0x8000,AX\n"
+		/* arguments in system call registers
+		 * we already do this for Linux binaries;
+		 * might as well extend it to NxM binaries.
+		 * AX and CX are taken.
+		 * R15, 14, 13, and 11 are taken.
+		 * BP is RARG
+		 * We want to group args as much as we can
+		 * to keep assembly code simple.
+		 * It's a headache.
+		 * We propose to take DI, SI, DX, R10
+		 * so we are compatible with linux usage.
+		 * It might let us have one syscall path
+		 * some day.
+		 * We indicate args in registers with
+		 * 0xc000 in AX.
+		 * We will also minimize
+		 * # args moved later; 4 is the worst case.
+		 */
+		ass = ass + "\tMOVQ $0xc000,AX\n"
+		ass = ass + "\tMOVQ a1+0x8(FP), DI\n"
+		ass = ass + "\tMOVQ a1+0x10(FP), SI\n"
+		ass = ass + "\tMOVQ a1+0x18(FP), DX\n"
+		ass = ass + "\tMOVQ a1+0x20(FP), R10\n"
+
 		ass = ass + "\tSYSCALL\n\tRET\n"
 		err = ioutil.WriteFile(filename, []byte(ass), 0666)
 		if err != nil {
