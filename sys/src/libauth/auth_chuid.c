e21fddd1cadf0e1e464313dfb01e6731af47b7a3
David du Colombier <0intro@gmail.com>
2013-04-12 05:00:00 +0000

    Plan 9 from Bell Labs 2013-04-12

--- sys/src/libauth/auth_chuid.c
+++ sys/src/libauth/auth_chuid.c
@@ -10,7 +10,7 @@ auth_chuid(AuthInfo *ai, char *ns)
 {
 	int rv, fd;
 
-	if(ai == nil || ai->cap == nil){
+	if(ai == nil || ai->cap == nil || *ai->cap == 0){
 		werrstr("no capability");
 		return -1;
 	}
