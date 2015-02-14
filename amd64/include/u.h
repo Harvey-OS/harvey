73c242d9aca748946e8f0f8c62491e9d779c3729
David du Colombier <0intro@gmail.com>
2015-02-14 18:45:30 +0100

    define uint type as unsigned int

--- amd64/include/u.h
+++ amd64/include/u.h
@@ -13,6 +13,7 @@ typedef signed char	int8_t;
 typedef	unsigned short	uint16_t;
 typedef	signed short	int16_t;
 typedef unsigned int	uint32_t;
+typedef unsigned int	uint;
 typedef signed int	int32_t;
 typedef	unsigned long long uint64_t;
 typedef	long long	int64_t;
