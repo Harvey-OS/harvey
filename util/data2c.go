0d764b40b69051aca4872d4dfefe682f604c51d8
Ronald G. Minnich <rminnich@gmail.com>
2015-02-27 09:12:58 -0800

    data2c in Go. Removes dependency on plan9ports.

---
+++ util/data2c.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+	a := flag.Args()
+	i := a[0]
+	o := a[1]
+	if len(a) != 2 {
+		fmt.Fprintf(os.Stderr, "[%v]usage: data2s name\n", a)
+		os.Exit(1)
+	}
+
+	in, err := ioutil.ReadFile(i)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Read %v: %v\n", a[0], err)
+		os.Exit(1)
+	}
+	
+	total := len(in)
+	data := ""
+
+	for len(in) > 0 {
+		for j := 0; j < 16 && len(in) > 0; j++ {
+			data += fmt.Sprintf("0x%02x, ", in[0])
+			in = in[1:]
+		}
+		data += fmt.Sprintf("\n")
+	}
+		
+	out := fmt.Sprintf("unsigned char %vcode[] = {\n", o)
+	out += data
+	out += fmt.Sprintf("0,\n};\nint %vlen = %v;\n", o, total)
+
+	if err = ioutil.WriteFile(o, []byte(out), 0600); err != nil {
+		fmt.Fprintf(os.Stderr, "%v: %v\n", o, err)
+		os.Exit(1)
+	}
+}
