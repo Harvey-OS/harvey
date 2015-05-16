d180b264d172bd5a600ac1a87338f877e19418f2
rafael <rafael.fernandez@taisis.com>
2015-05-17 01:03:18 +0200

    Simple test programs to fork and scanf

---
+++ sys/src/test/to_fork/to_fork.c
@@ -0,0 +1,20 @@
+#include <u.h>
+#include <libc.h>
+#include <stdio.h>
+
+void main()
+{
+	int pid = fork();
+	if (pid == 0)
+	{
+		printf("I'm the father\n");
+	}
+	else if (pid > 0)
+	{
+		printf("I'm the child\n");
+	}
+	else
+	{
+		printf("fork() error\n");
+	}
+}
