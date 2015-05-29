5a86390e1f5a1c14c2746e7fdcf10908248a0a92
Elbing Miss <elbingmiss@gmail.com>
2015-05-30 00:36:05 +0200

    Added some things to k8cpu. Fix boot.fs

--- sys/src/9/k10/boot.fs
+++ sys/src/9/k10/boot.fs
@@ -2,13 +2,11 @@
 # boot script for file servers, including standalone ones
 path=(/boot /amd64/bin /rc/bin .)
 prompt=('harvey@cpu% ' '	')
-echo
 echo 'Hello, I am Harvey :-)'
-echo
+bind -a /boot /bin
 bind -a '#I' /net
 bind -a '#l0' /net
 bind -a '#p' /proc
-bind -a '#d' /fd
 bind -a '#s' /srv
 exec /boot/rc -m/boot/rcmain -i
 
