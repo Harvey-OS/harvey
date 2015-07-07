155f538ab53839c19fa6da65fb567e6912090815
Aki Nyrhinen <anyrhine@gmail.com>
2015-07-07 19:28:41 +0000

    fix execl (varargs mess), add regression test for it
    Change-Id: I93f2d93d933fbeb1fac10bb970bb13e39dfb11cd

---
+++ sys/src/regress/execl.c
@@ -0,0 +1,31 @@
+#include <u.h>
+#include <libc.h>
+
+void
+main(int argc, char *argv[])
+{
+	char buf[512];
+	Waitmsg *w;
+	int i;
+
+	for(i = 0; i < argc; i++){
+		switch(fork()){
+		case -1:
+			fprint(2, "fork fail\n");
+			exits("FAIL");
+		case 0:
+			execl("/bin/echo", "echo", argv[i], nil);
+			fprint(2, "execl fail\n");
+			exits("execl");
+		default:
+			w = wait();
+			if(strcmp(w->msg, "")){
+				print("FAIL\n");
+				exits("FAIL");
+			}
+			break;
+		}
+	}
+	print("PASS\n");
+	exits("PASS");
+}
