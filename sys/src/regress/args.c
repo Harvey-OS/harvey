a0f58cf1dba97ffa781cf209daa6a58c04e227c7
Aki Nyrhinen <anyrhine@gmail.com>
2015-07-07 03:53:26 +0000

    regress/args 	- some simple fiddling with the args 	- some floating point operations that used to crash if the stack wasn't aligned right 	- an explicit check for stack alignment.
    regress/syscall
    	- do a busy-loop of open/read/close on /proc/1/status.
    	- crashes every time,
    		- either a kernel page fault at a low address, or sometimes
    		- just a deadlock in clockintr (ilock).
    
    Change-Id: I73ee5fcebb611c17566ce7fe71bc33a04487f425

---
+++ sys/src/regress/args.c
@@ -0,0 +1,38 @@
+#include <u.h>
+#include <libc.h>
+
+/*
+ *	The whole regression test I am after here is to call regress/args with
+ *	arguments of various lengths, in order to trigger different stack alignments
+ *	due to varying amounts of stuff in args.
+ *
+ *	It turned out that gcc compiles fprintf into something that uses
+ *	fpu instructions which require the stack to be 16-aligned, so in
+ *	fact the fprint for sum here would suicide the process if the stack
+ *	it got happened to be not 16-aligned.
+ *
+ */
+
+void
+main(int argc, char *argv[])
+{
+	char *p;
+	int i;
+	double sum;
+
+	if(((uintptr_t)&p & 15) != 0){
+		fprint(2, "%p not 16-aligned\n", &p);
+		print("FAIL\n");
+		exits("FAIL");
+	}
+
+	sum = 0.0;
+	for(i = 0; i < argc; i++){
+		p = argv[i];
+		sum += strtod(p, nil);
+	}
+	fprint(2, "&sum %p\n", &sum);
+	fprint(2, "sum %f\n", sum);
+	print("PASS\n");
+	exits("PASS");
+}
