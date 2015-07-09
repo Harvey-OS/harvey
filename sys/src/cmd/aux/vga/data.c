63ca1c1a9ea58646c4e4a3c1fe85e45ce296a5d9
John Floren <john@jfloren.net>
2015-07-08 20:01:47 -0700

    First pass at removing old crap from aux/vga
    Some of these cards are over 20 years old and couldn't even run in
    an amd64 system. Let go of the past.
    
    Change-Id: I0e0d0f5a9eb6bbfde87f11bbe453e2a23fa3b6fd

--- sys/src/cmd/aux/vga/data.c
+++ sys/src/cmd/aux/vga/data.c
@@ -18,44 +18,16 @@ int cflag;					/* do not use hwgc */
 int dflag;					/* do the palette */
 
 Ctlr* ctlrs[] = {
-	&ark2000pv,				/* ctlr */
-	&ark2000pvhwgc,				/* hwgc */
-	&att20c490,				/* ramdac */
-	&att20c491,				/* ramdac */
-	&att20c492,				/* ramdac */
-	&att21c498,				/* ramdac */
-	&bt485,					/* ramdac */
-	&bt485hwgc,				/* hwgc */
 	&ch9294,				/* clock */
 	&clgd542x,				/* ctlr */
 	&clgd542xhwgc,				/* hwgc */
 	&clgd546x,				/* ctlr */
 	&clgd546xhwgc,				/* hwgc */
-	&ct65540,				/* ctlr */
-	&ct65545,				/* ctlr */
-	&ct65545hwgc,				/* hwgc */
-	&cyber938x,				/* ctlr */
-	&cyber938xhwgc,				/* hwgc */
-	&et4000,				/* ctlr */
-	&et4000hwgc,				/* hwgc */
 	&generic,				/* ctlr */
-	&hiqvideo,				/* ctlr */
-	&hiqvideohwgc,				/* hwgc */
-	&i81x,				/* ctlr */
-	&i81xhwgc,				/* hwgc */
-	&ibm8514,				/* ctlr */
 	&icd2061a,				/* clock */
 	&ics2494,				/* clock */
 	&ics2494a,				/* clock */
 	&ics534x,				/* gendac */
-	&mach32,				/* ctlr */
-	&mach64,				/* ctlr */
-	&mach64xx,				/* ctlr */
-	&mach64xxhwgc,				/* hwgc */
-	&mga2164w,				/* ctlr */
-	&mga2164whwgc,				/* hwgc */
-	&neomagic,				/* ctlr */
-	&neomagichwgc,				/* hwgc */
 	&nvidia,				/* ctlr */
 	&nvidiahwgc,				/* hwgc */
 	&radeon,				/* ctlr */
@@ -74,8 +46,6 @@ Ctlr* ctlrs[] = {
 	&stg1702,				/* ramdac */
 	&t2r4,					/* ctlr */
 	&t2r4hwgc,				/* hwgc */
-	&tdfx,					/* ctlr */
-	&tdfxhwgc,				/* hwgc */
 	&trio64,				/* ctlr */
 	&tvp3020,				/* ramdac */
 	&tvp3020hwgc,				/* hwgc */
@@ -91,9 +61,6 @@ Ctlr* ctlrs[] = {
 	&vision968,				/* ctlr */
 	&vmware,				/* ctlr */
 	&vmwarehwgc,				/* hwgc */
-	&w30c516,				/* ctlr */
-	&mga4xx,
-	&mga4xxhwgc,
 	0,
 };
 
