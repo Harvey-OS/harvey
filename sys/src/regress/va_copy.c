c3091ae80b32404bfc0ecc86dff1f00858bfd9d1
rafael <rafael.fernandez@taisis.com>
2015-07-09 23:27:16 +0200

    regress test for va_copy problem
    Change-Id: Idb9a29e119715ab3ee9ad7c0ca423cae33699ade

---
+++ sys/src/regress/va_copy.c
@@ -0,0 +1,20 @@
+#include <u.h>
+#include <libc.h>
+#include <stdio.h>
+
+int
+main(){
+	char *correct="0.987650\n";
+	static char result[128];
+
+	sprintf(result, "%f\n", 0.98765f);
+
+	if(!strcmp(result, correct)){
+		print("PASS\n");
+		exits("PASS");
+	}
+	else{
+		print("FAIL\n");
+		exits("FAIL");
+	}
+}
