9ff243a550d949005b98dd43803730362067dfb9
Aki Nyrhinen <anyrhine@gmail.com>
2015-07-29 17:00:23 -0700

    add no-exec mmu support, set the no-exec bit in k10/mmu.c too updated for the latest changes, also make regress/nx more comprehensive. regress/nx PASS
    Change-Id: Ib892ab499774b669c0fded691931624519fa51da

--- sys/src/regress/nx.c
+++ sys/src/regress/nx.c
@@ -2,15 +2,18 @@
 #include <libc.h>
 #define RET 0xc3
 
+int success;
+int cases;
+
 void
 handler(void *v, char *s)
 {
-	print("PASS\n");
-	exits("PASS");
+	success++;
+	exits(nil);
 }
 
 void
-main(void)
+callinsn(char *name, char *buf)
 {
 	void (*f)(void);
 	if (notify(handler)){
@@ -18,10 +21,80 @@ main(void)
 		exits("notify fails");
 	}
 
-	uint8_t *ret = malloc(1);
-	*ret = RET;
-	f = (void *)ret;
+
+	f = (void *)buf;
 	f();
-	print("FAIL");
+	print("FAIL %s\n", name);
+	exits("FAIL");
+}
+
+void
+writeptr(char *name, void *ptr)
+{
+	if (notify(handler)){
+		fprint(2, "%r\n");
+		exits("notify fails");
+	}
+
+	*(uintptr_t*)ptr = 0xdeadbeef;
+	print("FAIL %s\n", name);
+	exits("FAIL");
+}
+
+void
+main(void)
+{
+	char *str = "hello world";
+	char stk[128];
+	char *mem;
+
+	switch(rfork(RFMEM|RFPROC)){
+	case -1:
+		sysfatal("rfork");
+	case 0:
+		stk[0] = RET;
+		callinsn("exec stack", stk);
+	default:
+		cases++;
+		waitpid();
+	}
+
+	switch(rfork(RFMEM|RFPROC)){
+	case -1:
+		sysfatal("rfork");
+	case 0:
+		mem = malloc(128);
+		mem[0] = RET;
+		callinsn("exec heap", mem);
+	default:
+		cases++;
+		waitpid();
+	}
+
+	switch(rfork(RFMEM|RFPROC)){
+	case -1:
+		sysfatal("rfork");
+	case 0:
+		writeptr("write code", (void*)&main);
+	default:
+		cases++;
+		waitpid();
+	}
+
+	switch(rfork(RFMEM|RFPROC)){
+	case -1:
+		sysfatal("rfork");
+	case 0:
+		writeptr("write rodata", (void*)str);
+	default:
+		cases++;
+		waitpid();
+	}
+
+	if(success == cases){
+		print("PASS\n");
+		exits("PASS");
+	}
+	print("FAIL\n");
 	exits("FAIL");
 }
