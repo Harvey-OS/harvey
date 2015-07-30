990b18e442ea197363c7bcda2cf2d735659cfd1d
Ronald G. Minnich <rminnich@gmail.com>
2015-07-30 09:02:31 -0700

    Add a regression test for notify.
    This just does a deref through 0 and passes if we catch
    the note.
    
    Change-Id: Ie8331e5a17f523997e9ed09a645a0c14c690c346
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ sys/src/regress/notify.c
@@ -0,0 +1,24 @@
+#include <u.h>
+#include <libc.h>
+#define RET 0xc3
+
+void
+handler(void *v, char *s)
+{
+	print("PASS\n");
+	exits("PASS");
+}
+
+void
+main(void)
+{
+	void (*f)(void) = nil;
+	if (notify(handler)){
+		fprint(2, "%r\n");
+		exits("notify fails");
+	}
+
+	f();
+	print("FAIL");
+	exits("FAIL");
+}
