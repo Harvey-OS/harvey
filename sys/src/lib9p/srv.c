8f9795718c41c6281ef2d0d9ecf0325bb7629312
Keith Poole <keith.poole@gmail.com>
2015-08-04 21:52:28 +0100

    Malloc header is now 64 bit
    changed uint64_t to uintptr_t & changed getcallerpc to use gcc builtin
    Changed libc.h pointers
    
    Change-Id: Ic6bf432b54db4ae7137e1e7b1d0272951632b13e
    Signed-off-by: Keith Poole <keith.poole@gmail.com>

--- sys/src/lib9p/srv.c
+++ sys/src/lib9p/srv.c
@@ -16,20 +16,16 @@
 
 void (*_forker)(void(*)(void*), void*, int);
 
-static char Ebadattach[] = "unknown specifier in attach";
 static char Ebadoffset[] = "bad offset";
-static char Ebadcount[] = "bad count";
 static char Ebotch[] = "9P protocol botch";
 static char Ecreatenondir[] = "create in non-directory";
 static char Edupfid[] = "duplicate fid";
 static char Eduptag[] = "duplicate tag";
 static char Eisdir[] = "is a directory";
 static char Enocreate[] = "create prohibited";
-static char Enomem[] = "out of memory";
 static char Enoremove[] = "remove prohibited";
 static char Enostat[] = "stat prohibited";
 static char Enotfound[] = "file not found";
-static char Enowrite[] = "write prohibited";
 static char Enowstat[] = "wstat prohibited";
 static char Eperm[] = "permission denied";
 static char Eunknownfid[] = "unknown fid";
