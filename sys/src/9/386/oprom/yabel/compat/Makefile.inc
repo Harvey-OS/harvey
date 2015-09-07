28bd16b32e17a20747d110d56886a5e92ea99a70
Ronald G. Minnich <rminnich@gmail.com>
2015-09-07 10:31:24 -0700

    Add the option ROM support from coreboot
    This is the essential part of doing option ROM support.
    
    I think we'll want to remove the realmode directory, as that requires
    dropping all protections and going to 16-bit x86 mode, rarely a good idea;
    and, besides, it's just gross.
    
    I think our best bet is to use yabel, as coreboot has for many years.
    I'm putting it all here as an initial commit so we can pick over its bones.
    
    Change-Id: I61a979ac4808dd6f1f278b8a8d908749768bba52
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ sys/src/9/386/oprom/yabel/compat/Makefile.inc
@@ -0,0 +1,1 @@
+ramstage-y += functions.c
---
+++ sys/src/9/386/oprom/yabel/compat/functions.c
@@ -0,0 +1,64 @@
+/****************************************************************************
+ * YABEL BIOS Emulator
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the BSD License
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/bsd-license.php
+ *
+ * Copyright (c) 2008 Pattrick Hueper <phueper@hueper.net>
+ ****************************************************************************/
+
+/* this file contains functions provided by SLOF, that the current biosemu implementation needs
+ * they should go away  in the future...
+ */
+
+#include <types.h>
+#include <string.h>
+#include <device/device.h>
+#include "../debug.h"
+#include "../biosemu.h"
+#include <vbe.h>
+#include "../compat/time.h"
+
+#define VMEM_SIZE (1024 * 1024) /* 1 MB */
+
+#if !CONFIG_YABEL_DIRECTHW
+#if CONFIG_YABEL_VIRTMEM_LOCATION
+u8* vmem = (u8 *) CONFIG_YABEL_VIRTMEM_LOCATION;
+#else
+u8* vmem = (u8 *) (16*1024*1024); /* default to 16MB */
+#endif
+#else
+u8* vmem = NULL;
+#endif
+
+extern u8 *biosmem;
+
+void run_bios(struct device * dev, unsigned long addr)
+{
+	biosmem = vmem;
+
+	biosemu(vmem, VMEM_SIZE, dev, addr);
+
+#if CONFIG_FRAMEBUFFER_SET_VESA_MODE
+	vbe_set_graphics();
+#endif
+}
+
+unsigned long tb_freq = 0;
+
+u64 get_time(void)
+{
+    u64 act = 0;
+#if CONFIG_ARCH_X86
+    u32 eax, edx;
+
+    __asm__ __volatile__(
+	"rdtsc"
+        : "=a"(eax), "=d"(edx)
+        : /* no inputs, no clobber */);
+    act = ((u64) edx << 32) | eax;
+#endif
+    return act;
+}
