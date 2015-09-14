108049b669bd4c260e79de2afaa667daf645abc8
Giacomo Tesio <giacomo@tesio.it>
2015-09-14 23:36:37 +0200

    libc: removed old profiling hooks (and tools) based on linking tricks from 8l(1), see #9
    Change-Id: I6eee91869ee31c56aabbe5dc8cef3724454cc389
    Signed-off-by: Giacomo Tesio <giacomo@tesio.it>

--- sys/include/tos.h
+++ sys/include/tos.h
@@ -8,20 +8,8 @@
  */
 
 typedef struct Tos Tos;
-typedef struct Plink Plink;
-
-#pragma incomplete Plink
 
 struct Tos {
-	struct			/* Per process profiling */
-	{
-		Plink	*pp;	/* known to be 0(ptr) */
-		Plink	*next;	/* known to be 4(ptr) */
-		Plink	*last;
-		Plink	*first;
-		uint32_t	pid;
-		uint32_t	what;
-	} prof;
 	uint64_t	cyclefreq;	/* cycle clock frequency if there is one, 0 otherwise */
 	int64_t	kcycles;	/* cycles spent in kernel */
 	int64_t	pcycles;	/* cycles spent in process (kernel + user) */
