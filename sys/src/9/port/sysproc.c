5204ec22bde3e52cb2d0494fa620737dc8abfa8f
Giacomo Tesio <giacomo@tesio.it>
2015-09-29 00:11:22 +0200

    kernel: fix weird bug in awake syscall (see #25); small performance improvements;
    The problem was that when multiple wakeups were sharing the same time,
    forgivewakeup() did not progress toward the exit condition of the
    while loop.
    
    Also, fixed copyright notice in files.
    
    Change-Id: Iadd53e3ecc30c827a4a6ee58c54c0a6ac5dae394
    Signed-off-by: Giacomo Tesio <giacomo@tesio.it>

--- sys/src/9/port/sysproc.c
+++ sys/src/9/port/sysproc.c
@@ -1,12 +1,12 @@
 /*
+ * Copyright (C) 2015 Giacomo Tesio <giacomo@tesio.it>
+ *
  * This file is part of the UCB release of Plan 9. It is subject to the license
  * terms in the LICENSE file found in the top-level directory of this
  * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
  * part of the UCB release of Plan 9, including this file, may be copied,
  * modified, propagated, or distributed except according to the terms contained
  * in the LICENSE file.
- *
- * Copyright (C) 2015 Giacomo Tesio <giacomo@tesio.it>
  */
 
 #include	"u.h"
