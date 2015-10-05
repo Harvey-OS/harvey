ebd9750a830049e11e44f39c788622754909ae78
Giacomo Tesio <giacomo@tesio.it>
2015-10-06 01:46:40 +0200

    travis-ci: introduced runqemu, to pipe commands into Harvey
    util/runqemu	a go (quick&dirty) script that run commands received from stdin in qemu
     -p prompt	=> prompt to expect (default "10.0.2.15#")
    
    Needs $HARVEY defined. For example
    
    	echo 'regress/thread' | ./util/runqemu
    
    This is a first step to get regression tests executed during the continuous build.
    
    Change-Id: I6855600bd85545af8abdd510338fdae38c7ecdfc
    Signed-off-by: Giacomo Tesio <giacomo@tesio.it>

---
+++ util/src/harvey/cmd/runqemu/runqemu.go
@@ -0,0 +1,119 @@
+// Run commands received from stdin in qemu
+//
+// -p prompt	=> prompt to expect (default "10.0.2.15#")
+//
+// ENVIRONMENT
+// Needed: HARVEY
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"flag"
+	"os"
+	"os/exec"
+	"strings"
+	"github.com/kr/pty"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func main() {
+	var prompt string
+	flag.StringVar(&prompt, "p", "10.0.2.15#", "the prompt to expect")
+	flag.Parse()
+	harvey := os.Getenv("HARVEY")
+	if harvey == "" {
+		fmt.Printf("usage: cat cmds.rc | runqemu [-p prompt]\n")
+		fmt.Printf("error: missing $HARVEY\n");
+		os.Exit(1)
+	}
+	if terminal.IsTerminal(0) {
+		fmt.Printf("usage: cat cmds.rc | runqemu [-p prompt]\n")
+		fmt.Printf("error: runqemu is intended for automation, pipe commands in.\n")
+		os.Exit(1)
+	}
+	
+	qemuCmd := "cd $HARVEY/sys/src/9/k10 && sh $HARVEY/util/REGRESS\n"
+	qemuCmd = os.ExpandEnv(qemuCmd)
+
+	sh := exec.Command("/bin/sh")
+	qemu, err := pty.Start(sh)
+	if err != nil {
+		fmt.Printf("REGRESS start (%s): %s", qemuCmd, err)
+		os.Exit(2)
+	}
+	qemu.WriteString(qemuCmd)
+	defer qemu.Close()
+	
+	exitStatus := 0
+	
+	qemuInput  := make(chan string)
+	qemuOutputRaw := make(chan string)
+	wait := make(chan int)
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	go func() {
+		qemuOut := make([]byte, 256)
+		for {
+			r, err := qemu.Read(qemuOut)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "error:", err)
+				wait <- 3
+			}
+			qemuOutputRaw <- string(qemuOut[0:r])
+		}
+	}()
+	go func(){
+		line := ""
+		for {
+			s := <- qemuOutputRaw
+			line += s
+			if strings.Contains(line, prompt) {
+				if scanner.Scan() {
+					cmd	:= scanner.Text()
+					qemuInput <- fmt.Sprintf("%s\n", cmd)
+				} else {
+					if err := scanner.Err(); err != nil {
+						fmt.Fprintln(os.Stderr, "error:", err)
+						wait <- 4
+				    } else {
+						wait <- exitStatus
+					}
+					return
+				}
+				fmt.Printf("%s", line)
+				line = ""
+			} else if strings.ContainsAny(line, "\r\n") {
+				if strings.Contains(line, "FAIL") {
+					exitStatus = 5
+				}
+				fmt.Printf("%s", line)
+				line = ""
+			}
+		}
+	}()
+	go func(){
+		for {
+			s := <- qemuInput
+			i := 0;
+			for {
+				n, err := qemu.WriteString(s[i:])
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "error:", err)
+					wait <- 6
+				}
+				i += n
+				if i == len(s) {
+					break
+				}
+			}
+		}
+	}()
+
+	e := <- wait
+	if e == 0 {
+		fmt.Printf("\nDone.\n")
+	}
+	os.Exit(e)
+}
