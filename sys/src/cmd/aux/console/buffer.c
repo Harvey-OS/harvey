afea5d9c12de640b970e49dfe209ef13f4509ae8
Giacomo Tesio <giacomo@tesio.it>
2015-10-19 23:21:17 +0200

    boot: new aux/console, see #22
    `aux/console -d -s -b program arguments`
    
    Options:
     -d	enable debug (written to standard error)
     -s	use standard input/output instead of keyboard/screen
    	(this is the mode we need with listen1 and friends)
     -b	blind mode: like -s but stdin is not written to stdout; disables rawon/rawoff
    	(this is the mode we need when running from rc)
    
    Currently set $prompt='gconsole$ ' to easy debug.
    
    echo rawon > /dev/consctl
    echo rawoff > /dev/consctl
    	work with and without -s, but not with -b.
    
    When started without -s or -b, it opens #P/cgamem and #P/ps2keyb: if another
    process are using them, you will get a funny session at the console.
    
    Still to do:
     - fix it's integration with aux/tty
     - read keyboard tables from a well designed /dev/kbd/ tree
     - write regression tests (how?)
     - integrate into boot process
    
    Sample session producing (large) debug output in /usr/glenda/console.debug.txt:
    
    ./util/telnet localhost:5555
    10.0.2.15# ls /proc|wc -l
         54
    10.0.2.15# aux/console
    usage: aux/console [-bds] program args
    10.0.2.15# aux/console -bds /bin/rc >[2]console.debug.txt
    10.0.2.15# prompt='>> '
    10.0.2.15# prompt='>> '
    >> ls /proc|wc -l
         58
    >> lc /dev
    bintime    consctl    hostowner  kmesg      pid        sdctl      urandom
    capuse     cputime    ioalloc    kprint     ppid       swap       usb
    cgamem     cputype    iob        null       ps2keyb    syscall    user
    cons       debug      iol        numcores   ps2mouse   sysname    zero
    cons       drivers    iow        osversion  random     sysstat
    consctl    hostdomain irqalloc   pgrpid     reboot     time
    >> ls -l /dev/consctl
    ---w--w--w- M 46 glenda glenda 0 Feb  7 01:28 /dev/consctl
    >> ls -l /dev/cons
    --rw-rw-rw- M 46 glenda glenda 0 Feb  7 01:28 /dev/cons
    >> cat /dev/consctl
    cat: can't open /dev/consctl: '/dev/consctl' permission denied
    >> echo rawon > /dev/consctl
    echo: write error: no raw mode when blind
    >> cat /testfifo < /dev/null > /dev/null >[2=1] &
    >> aux/tty: sent eof to 62
    10.0.2.15# ls /proc|wc -l
         57
    10.0.2.15# echo done > /testfifo
    echo: write error: write /home/giacomo/projects/HarveyOS-Shamar/harvey/testfifo: illegal seek
    10.0.2.15# ls /proc|wc -l
         54
    
    Change-Id: I994411c0de7bb23491a9959c7e0ede8672e43423
    Signed-off-by: Giacomo Tesio <giacomo@tesio.it>

---
+++ sys/src/cmd/aux/console/buffer.c
@@ -0,0 +1,97 @@
+/*
+ * This file is part of Harvey.
+ *
+ * Copyright (C) 2015 Giacomo Tesio <giacomo@tesio.it>
+ *
+ * Harvey is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Harvey is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Harvey.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <u.h>
+#include <libc.h>
+
+#include "console.h"
+
+Buffer*
+balloc(uint32_t size)
+{
+	Buffer* b;
+
+	b = (Buffer*)malloc(sizeof(Buffer));
+	b->written = 0;
+	b->read = 0;
+	b->size = size;
+	b->data = (char*)malloc(size);
+	b->flushlines = 0;
+
+	debug("balloc(%d), allocated %#p\n", size, b);
+	return b;
+}
+uint32_t
+bwrite(Buffer *b, char *source, uint32_t len)
+{
+	uint32_t i;
+	char *dest;
+
+	debug("bwrite(%#p, src, %d): b->written was %d; ", b, len, b->written);
+	if(b->read == b->written){
+		b->written = 0;
+		b->read = 0;
+	}
+	if(len > b->size - b->written)
+		len = b->size - b->written;
+	if(len > 0){
+		dest = b->data + b->written;
+		for(i = 0; i < len; ++i)
+			*dest++ = *source++;
+		b->written += len;
+	}
+	debug("len = %d, b->written = %d, b->size = %d, b->read = %d\n", len, b->written, b->size, b->read);
+	return len;
+}
+void
+bdelete(Buffer *b, uint32_t len)
+{
+	if(len > b->written - b->read)
+		len = b->written - b->read;
+	b->written -= len;
+}
+char *
+bread(Buffer *b, uint32_t *length)
+{
+	uint32_t len;
+	char *data;
+
+	len = *length;
+	data = nil;
+	debug("bread(%#p, %#p): len = %d, b->written was %d, b->read was %d; ", b, length, len, b->written, b->read);
+	if(len > b->written - b->read)
+		len = b->written - b->read;
+	assert(len < b->size);	/* check correctness by detecting overflows */
+	if(len > 0){
+		data = b->data + b->read;
+		b->read += len;
+	}
+	debug("len = %d, b->written = %d, b->size = %d, b->read = %d\n", len, b->written, b->size, b->read);
+	*length = len;
+	return data;
+}
+int
+bempty(Buffer *b)
+{
+	if(b->read == b->written)
+		return 1;
+	if(stdio)
+		return 0;
+	if(b->flushlines)
+		return b->data[b->written] != '\n';
+	return 0;
+}
