cfa03364136bbe1086ce3ed9e3a09c3bf7e4ac98
Giacomo Tesio <giacomo@tesio.it>
2015-11-17 22:56:46 +0100

    zlib: native automated port, first try (not to be merged)
    Currently this is just an not perfect example of how we could port third party tools.
    
    Builds fine, but regress/zlib/example segfaults after creating the first gz.
    
    Change-Id: Ifb1b82da6a8a4a072ab7abd4d2646bee70b24b17
    Signed-off-by: Giacomo Tesio <giacomo@tesio.it>

---
+++ sys/src/libz/stdio.h
@@ -0,0 +1,9 @@
+#ifndef ZLIB_STDIO
+#define ZLIB_STDIO
+
+#  include <amd64/include/u.h>
+#  include <sys/include/libc.h>
+#  include <sys/include/stdio.h>
+#  define exit(n) exits("error: " #n)
+
+#endif
