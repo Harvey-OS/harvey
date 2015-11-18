e8726dba246e0b33371ac5f9a71810608583387e
Hank Donnay <hdonnay@gmail.com>
2015-11-18 14:18:25 -0600

    Run the entire build process though the build tool
    sys/src/**.json: use the "Install" key instead of mv in "Post", where
    equivalent
    
    sys/src/util.json: add json file for building utils
    
    sys/src/9/k10/k8cpu.json: move Systab key to proper spot
    
    bootstrap.sh: combine git setup and initial build of util/build
    
    util/build:
        Added:
    	* Path searching logic
    	* specify multiple targets
    	* harvey support (not tested yet)
    	* online help
    	* documentation of the buildfile format in the godoc
        Did some internal refactoring and file splitting.
    
    Change-Id: I8c7b2c44d3d182a3338c69707e3188d1533fe3b2
    Signed-off-by: Hank Donnay <hdonnay@gmail.com>

---
+++ util/src/harvey/cmd/build/codegen.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"bytes"
+	"debug/elf"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"text/template"
+)
+
+const kernconfTmpl = `
+#include "u.h"
+#include "../port/lib.h"
+#include "mem.h"
+#include "dat.h"
+#include "fns.h"
+#include "../port/error.h"
+#include "io.h"
+
+void
+rdb(void)
+{
+	splhi();
+	iprint("rdb...not installed\n");
+	for(;;);
+}
+
+{{ range .Rootcodes }}
+{{ . }}
+{{ end }}
+
+{{ range .Config.Dev }}extern Dev {{ . }}devtab;
+{{ end }}
+Dev *devtab[] = {
+{{ range .Config.Dev }}
+	&{{ . }}devtab,
+{{ end }}
+	nil,
+};
+
+{{ range .Config.Link }}extern void {{ . }}link(void);
+{{ end }}
+void
+links(void)
+{
+{{ range .Rootnames }}addbootfile("{{ . }}", ramfs_{{ . }}_code, ramfs_{{ . }}_len);
+{{ end }}
+{{ range .Config.Link }}{{ . }}link();
+{{ end }}
+}
+
+#include "../ip/ip.h"
+{{ range .Config.Ip }}extern void {{ . }}init(Fs*);
+{{ end }}
+void (*ipprotoinit[])(Fs*) = {
+{{ range .Config.Ip }}	{{ . }}init,
+{{ end }}
+	nil,
+};
+
+#include "../port/sd.h"
+{{ range .Config.Sd }}extern SDifc {{ . }}ifc;
+{{ end }}
+SDifc* sdifc[] = {
+{{ range .Config.Sd }}	&{{ . }}ifc,
+{{ end }}
+	nil,
+};
+
+{{ range .Config.Uart }}extern PhysUart {{ . }}physuart;
+{{ end }}
+PhysUart* physuart[] = {
+{{ range .Config.Uart }}	&{{ . }}physuart,
+{{ end }}
+	nil,
+};
+
+#define	Image	IMAGE
+#include <draw.h>
+#include <memdraw.h>
+#include <cursor.h>
+#include "screen.h"
+{{ range .Config.VGA }}extern VGAdev {{ . }}dev;
+{{ end }}
+VGAdev* vgadev[] = {
+{{ range .Config.VGA }}	&{{ . }}dev,
+{{ end }}
+	nil,
+};
+
+{{ range .Config.VGA }}extern VGAcur {{ . }}cur;
+{{ end }}
+VGAcur* vgacur[] = {
+{{ range .Config.VGA }}	&{{ . }}cur,
+{{ end }}
+	nil,
+};
+
+Physseg physseg[8] = {
+	{
+		.attr = SG_SHARED,
+		.name = "shared",
+		.size = SEGMAXPG,
+	},
+	{
+		.attr = SG_BSS,
+		.name = "memory",
+		.size = SEGMAXPG,
+	},
+};
+int nphysseg = 8;
+
+{{ range .Config.Code }}{{ . }}
+{{ end }}
+
+char* conffile = "{{ .Path }}";
+
+`
+
+// These are the two big code generation functions.
+
+// data2c takes the file at path and creates a C byte array containing it.
+func data2c(name string, path string) string {
+	var out []byte
+	var in []byte
+
+	if elf, err := elf.Open(path); err == nil {
+		elf.Close()
+		cwd, err := os.Getwd()
+		tmpf, err := ioutil.TempFile(cwd, name)
+		failOn(err)
+
+		run(nil, shellhack, exec.Command(tools["strip"], "-o", tmpf.Name(), path))
+
+		in, err = ioutil.ReadAll(tmpf)
+		failOn(err)
+
+		tmpf.Close()
+		os.Remove(tmpf.Name())
+	} else {
+		var file *os.File
+		var err error
+
+		file, err = os.Open(path)
+		failOn(err)
+
+		in, err = ioutil.ReadAll(file)
+		failOn(err)
+
+		file.Close()
+	}
+
+	total := len(in)
+
+	out = []byte(fmt.Sprintf("static unsigned char ramfs_%s_code[] = {\n", name))
+	for len(in) > 0 {
+		for j := 0; j < 16 && len(in) > 0; j++ {
+			out = append(out, []byte(fmt.Sprintf("0x%02x, ", in[0]))...)
+			in = in[1:]
+		}
+		out = append(out, '\n')
+	}
+
+	out = append(out, []byte(fmt.Sprintf("0,\n};\nint ramfs_%s_len = %v;\n", name, total))...)
+
+	return string(out)
+}
+
+// confcode creates a kernel configuration header.
+func confcode(path string, kern *kernel) []byte {
+	var rootcodes []string
+	var rootnames []string
+	for name, path := range kern.Ramfiles {
+		code := data2c(name, fromRoot(path))
+		rootcodes = append(rootcodes, code)
+		rootnames = append(rootnames, name)
+	}
+
+	vars := struct {
+		Path      string
+		Config    kernconfig
+		Rootnames []string
+		Rootcodes []string
+	}{
+		path,
+		kern.Config,
+		rootnames,
+		rootcodes,
+	}
+	tmpl := template.Must(template.New("kernconf").Parse(kernconfTmpl))
+	codebuf := &bytes.Buffer{}
+	failOn(tmpl.Execute(codebuf, vars))
+	return codebuf.Bytes()
+}
---
+++ util/src/harvey/cmd/build/doc.go
@@ -0,0 +1,181 @@
+/*
+BUILDFILE FORMAT
+
+A buildfile is a json object containing build objects. By convention, it's
+broken out onto multiple lines indented by tabs, and the keys are TitleCased.
+
+The environment varibles HARVEY and ARCH are guarenteed to be set for the
+buildfile. PATH is modified so that "$HARVEY/util" is included.
+
+Any key that takes a path or array of paths as its value has absolute paths
+re-rooted to the harvey tree and variables in the string expanded once.
+
+BUILD OBJECT
+
+A build object has the following keys and types:
+
+	Projects        []string
+	PreFetch        map[string]string
+	Pre             []string
+	Post            []string
+	Cflags          []string
+	Oflags          []string
+	Include         []string
+	SourceFiles     []string
+	ObjectFiles     []string
+	Libs            []string
+	Env             []string
+	SourceFilesCmd  []string
+	Program         string
+	Library         string
+	Install         string
+	Kernel          kernel
+
+These are the steps taken, in order:
+
+	Env
+	Include
+	Projects
+	PreFetch
+	Pre
+	Kernel
+	[compile] SourceFiles SourceFilesCmd Cflags Program
+	[link] ObjectFiles Libs Oflags Library
+	Install
+	Post
+
+"[compile]" and "[link]" are steps synthesized from the specified keys.
+
+The meaning of the keys is as follows:
+
+"Env" is an array of environment variables to be put in the environment of
+every command run in a build. They are expanded once and only available for
+use in other steps.
+
+"Include" is an array of buildfiles to "merge" into the current one. This
+is done before other projects are built.
+
+"Projects" is an array of buildfiles to build in their entirety before
+starting the current build.
+
+"PreFetch" is an object consisting of destination, URL pairs to be fetched
+before beginning the Pre step.
+
+"Pre" is an array of commands to run before starting the build.
+
+"Kernel" is a kernel build object. See the kernel object section.
+
+"Cflags" is an array of flags to pass to the C compiler. They are in addition
+to the standard flags of
+
+	-std=c11 -c -I /$ARCH/include -I /sys/include -I .
+
+The standard include paths are re-rooted to the harvey tree if not on a harvey
+system.
+
+"SourceFilesCmd" is an array of C files where each one should result in an
+executable. If this key is provided, "SourceFiles" and "Program" are ignored.
+
+"SourceFiles" is an array of C files that will ultimately produce a single
+binary or library, named by "Program" or "Library" respectively.
+
+"Program" is the name of the final executable for the files specified by
+"SourceFiles".
+
+"Oflags" is an array of flags to pass to the linker. They are in addition
+to the standard flags of
+
+	-o $program $objfiles -L /$ARCH/lib $libs
+
+The lib path is re-rooted to the harvey tree if not on a harvey system.
+
+"ObjectFiles" is an array of strings specifying object files to be linked
+into the final program specified by "Program". Any object files produced
+by the preceeding "[compile]" step are automatically added to this before
+beginning the "[link]" step.
+
+"Libs" is a an array of library arguments to pass to the linker.
+
+"Library" is the name of the archive resulting from bundling "SourceFiles"
+into a library. The resulting archive has 'ranlib' run on it automatically.
+
+"Install" is a directory where the result of the "[link]" step is moved
+to. If it does not exist, it is created.
+
+"Post" is an array of commands to run last during the build.
+
+KERNEL OBJECT
+
+A build object has the following keys and types:
+
+	Systab   string
+	Ramfiles map[string]string
+	Config   {
+		Code []string
+		Dev  []string
+		Ip   []string
+		Link []string
+		Sd   []string
+		Uart []string
+		VGA  []string
+	}
+
+"Systab" is the header that defines the syscall table.
+
+"Ramfiles" is an object of name, path pairs of binaries at "path" that will
+be baked into the kernel and available at a binary named "name".
+
+"Config" is an object used to generate the kernel configuration source. "Dev",
+"Ip", "Sd", "Uart", "Link", and "VGA" control which drivers of the various
+types are included. "Code" is lines of arbitrary C code.
+
+*/
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var helptext = `
+The buildfile is looked for at these positions, in this order:
+
+  ./$arg
+  ./$arg/build.json
+  /sys/src/$arg.json
+  /sys/src/$arg/build.json
+
+If the buildfile argument is not provided, it defaults to "build.json".
+
+After the buildfile, a number of regexps specifying targets may be provided.
+If a target matches any supplied regexp, it is acted on. These regexps only
+apply to the top-level buildfile.
+
+BUILDFILE
+
+See the build godoc for more information about the buildfile format.
+
+ENVIRONMENT
+
+ARCH is needed. Current acceptable vaules are: amd64
+
+HARVEY may be supplied to point at a harvey tree. The default on harvey is "/".
+The default on Linux and OSX is to attempt to find the top level of a git
+repository.
+`
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "  %s [options] [buildfile] [target...]\n\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, helptext)
+		fmt.Fprintln(os.Stderr, "Tools to be used with current settings:")
+		fmt.Fprintf(os.Stderr, "  prefix ($TOOLPREFIX): %q\n", os.Getenv("TOOLPREFIX"))
+		for k, v := range tools {
+			fmt.Fprintf(os.Stderr, "  %s ($%s): %s\n", k, strings.ToUpper(k), v)
+		}
+	}
+}
