e6a512fbe232e24527f11bc8f07b5198e3ed3621
Hank Donnay <hdonnay@gmail.com>
2015-11-24 21:27:59 -0600

    bootstrap: make a new bootstrap script
    Combines the util-building step of BUILD with git setup (if cloned from
    gerrithub). Modifies README.md to reflect using the new script, and adds
    a util/build.json for building the util tools.
    
    Change-Id: I6035eb5316c334bdb3d1d17d5b125e929294ca82
    Signed-off-by: Hank Donnay <hdonnay@gmail.com>

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ -n "$(git remote -v | awk '$1=="origin"{print $2}' | grep gerrithub)" ]; then
+	echo Setting up git...
+	curl -Lso .git/hooks/commit-msg http://review.gerrithub.io/tools/hooks/commit-msg
+	chmod u+x .git/hooks/commit-msg
+	# this is apparently the preferred method and might even work over HTTP
+	# https://gerrit-review.googlesource.com/Documentation/cmd-receive-pack.html
+	git config remote.origin.push HEAD:refs/for/master%r=rminnich@gmail.com,r=crossd@gmail.com,r=elbingmiss@gmail.com,r=anyrhine@gmail.com,r=0intro@gmail.com,r=giacomo@tesio.it,r=john@jfloren.net,r=rafael.fernandez@taisis.com
+	git submodule init
+	git submodule update
+fi
+echo Building the build tool...
+GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go get -d harvey/cmd/... # should really vendor these bits
+GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go install github.com/rminnich/go9p/ufs harvey/cmd/...
+cat <<EOF
+
+Now just set ARCH, modify PATH, and build:
+
+	export ARCH=amd64
+	export PATH="$(pwd)/util:\$PATH"
+	build all
+
+See \`build -h' for more information on the build tool.
+EOF
