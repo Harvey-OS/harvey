26813dbbb53055c06e56197aba08d3ded91adad7
Ronald G. Minnich <rminnich@gmail.com>
2015-11-25 16:29:22 -0800

    Define new boot type, rc
    If you type 'rc' at boot, it drops you into rc.
    
    You can then type, e.g., startdisk, then exit, and you'll have a system running on
    a local disk.
    
    This lets you debug getting local disk going.
    
    I now have local disk with fossil/venti on my samsung chromebook.
    
    Change-Id: Ib06cae7f68f8b2dc82f989722a5936ad69296bbb
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ sys/src/9/boot/bootrc.c
@@ -0,0 +1,49 @@
+/*
+ * This file is part of the UCB release of Plan 9. It is subject to the license
+ * terms in the LICENSE file found in the top-level directory of this
+ * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
+ * part of the UCB release of Plan 9, including this file, may be copied,
+ * modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+#include <u.h>
+#include <libc.h>
+#include <ip.h>
+
+#include "boot.h"
+
+void
+configrc(Method* m)
+{
+	void configloopback(void);
+	configloopback();
+	bind("#S", "/dev", MAFTER);
+	char *argv[] = {"rc", "-m", "/boot/rcmain", "-i", 0,};
+	switch(fork()){
+	case -1:
+		print("configrc: fork failed: %r\n");
+	case 0:
+		exec("/boot/rc", argv);
+		fatal("can't exec rc");
+	default:
+		break;
+	}
+	while(waitpid() != -1)
+		;
+	print("rc is done, continuing...\n");
+}
+
+int
+connectrc(void)
+{
+	int fd;
+	char buf[64];
+
+	// Later, make this anything.
+	snprint(buf, sizeof buf, "/srv/fossil");
+	fd = open("#s/fossil", 2);
+	if (fd < 0)
+		werrstr("dial %s: %r", buf);
+	return fd;
+}
