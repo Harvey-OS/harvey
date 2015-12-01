016506de03c28654b80cd9806451dbaec30bf0b8
Ronald G. Minnich <rminnich@gmail.com>
2015-11-30 20:56:00 -0800

    Two more tweaks for disk boot.
    The disk is working well; no need to pause with an rc any more.
    
    Leave usbd disabled until we can write a run function that will work.
    
    Change-Id: I133e12601c2c6f655ee8ac2b49a6b0c13890f16d
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/9/boot/boot.c
+++ sys/src/9/boot/boot.c
@@ -273,13 +273,15 @@ HaveMethod:
 static void
 usbinit(void)
 {
-	static char usbd[] = "/boot/usbd";
 	print("NOT RUNNING USBD -- run was broken (varargs problem\n");
 	return;
-
+/*
+	static char usbd[] = "/boot/usbd";
+  verify that shell won't block until usbd exits ...
 	if(access("#u/usb/ctl", 0) >= 0 && bind("#u", "/dev", MAFTER) >= 0 &&
 	    access(usbd, AEXIST) >= 0)
 		shell("-c", "/boot/usbd");
+*/
 }
 
 static void
