8704e5079d1b15d6d35df276866176b10c29c5be
Giacomo Tesio <giacomo@tesio.it>
2015-12-13 02:33:56 +0100

    kernel: move read(2) to libc, see #29
    Turns out that pread was already designed to replace the read() syscall.
    
    When the `offset` argument is ~0LL the pread syscall uses and maintains
    the channel's offset to support sequential access.
    
    Change-Id: Ica0895ad04b4d7f709d5029b055fbbd3fba2c910
    Signed-off-by: Giacomo Tesio <giacomo@tesio.it>

---
+++ sys/src/libc/9sys/read.c
@@ -0,0 +1,26 @@
+/*
+ * This file is part of Harvey.
+ *
+ * Copyright (C) 2015 Giacomo Tesio <giacomo@tesio.it>
+ *
+ * Harvey is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * Harvey is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Harvey.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <u.h>
+#include <libc.h>
+
+int32_t
+read(int fd, void* buf, int32_t nbytes)
+{
+	return pread(fd, buf, nbytes, ~0LL);
+}
