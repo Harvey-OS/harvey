bd1c2f4146e954bf5bd24f61c084f79a0ec7aef1
Ronald G. Minnich <rminnich@gmail.com>
2015-12-17 20:53:00 -0800

    graphics: add initial scaffolding for intel integrated graphics.
    iig will support sandy, ivy, haswell graphics on chromebooks.
    
    This is the initial bit. Next comes pci scan, then frame buffer
    setup, then example use.
    
    The hard part will be plumbing to dev/draw, since that's tightly
    tied to vga.
    
    Change-Id: I4cb9974e4bc756dad726444d56eff9115b85e9f4
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ sys/src/9/amd64/deviig.c
@@ -0,0 +1,173 @@
+/*
+ * This file is part of the UCB release of Plan 9. It is subject to the license
+ * terms in the LICENSE file found in the top-level directory of this
+ * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
+ * part of the UCB release of Plan 9, including this file, may be copied,
+ * modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+/*
+ * VGA controller
+ */
+#include "u.h"
+#include "../port/lib.h"
+#include "mem.h"
+#include "dat.h"
+#include "fns.h"
+#include "io.h"
+#include "../port/error.h"
+
+#define	Image	IMAGE
+#include <draw.h>
+#include <memdraw.h>
+#include <cursor.h>
+#include "screen.h"
+
+enum {
+	Qdir,
+	Qiigctl,
+};
+
+static Dirtab iigdir[] = {
+	".",	{ Qdir, 0, QTDIR },		0,	0550,
+	"iigctl",		{ Qiigctl, 0 },		0,	0660,
+};
+
+enum {
+	CMsize,
+	CMblank,
+	CMunblank,
+};
+
+static Cmdtab iigctlmsg[] = {
+	CMsize,		"size",		1,
+	CMblank,  "blank", 1,
+	CMunblank,  "unblank", 1,
+};
+
+static void
+iigreset(void)
+{
+}
+
+static Chan*
+iigattach(char* spec)
+{
+	if(*spec && strcmp(spec, "0"))
+		error(Eio);
+	return devattach('G', spec);
+}
+
+Walkqid*
+iigwalk(Chan* c, Chan *nc, char** name, int nname)
+{
+	return devwalk(c, nc, name, nname, iigdir, nelem(iigdir), devgen);
+}
+
+static int
+iigstat(Chan* c, unsigned char* dp, int n)
+{
+	return devstat(c, dp, n, iigdir, nelem(iigdir), devgen);
+}
+
+static Chan*
+iigopen(Chan* c, int omode)
+{
+	return devopen(c, omode, iigdir, nelem(iigdir), devgen);
+}
+
+static void
+iigclose(Chan* c)
+{
+}
+
+static int32_t
+iigread(Chan* c, void* a, int32_t n, int64_t off)
+{
+
+	switch((uint32_t)c->qid.path){
+
+	case Qdir:
+		return devdirread(c, a, n, iigdir, nelem(iigdir), devgen);
+
+	default:
+		error(Egreg);
+		break;
+	}
+
+	return 0;
+}
+
+static void
+iigctl(Cmdbuf *cb)
+{
+	Cmdtab *ct;
+
+	ct = lookupcmd(cb, iigctlmsg, nelem(iigctlmsg));
+	switch(ct->index){
+	case CMsize:
+		error("nope");
+		return;
+
+	case CMblank:
+		drawblankscreen(1);
+		return;
+	
+	case CMunblank:
+		drawblankscreen(0);
+		return;
+	}
+
+	cmderror(cb, "bad IIG control message");
+}
+
+static int32_t
+iigwrite(Chan* c, void* a, int32_t n, int64_t off)
+{
+	Proc *up = externup();
+	Cmdbuf *cb;
+	switch((uint32_t)c->qid.path){
+	case Qdir:
+		error(Eperm);
+
+	case Qiigctl:
+		if(off || n >= READSTR)
+			error(Ebadarg);
+		cb = parsecmd(a, n);
+		if(waserror()){
+			free(cb);
+			nexterror();
+		}
+		iigctl(cb);
+		poperror();
+		free(cb);
+		return n;
+	default:
+		error(Egreg);
+		break;
+	}
+
+	return 0;
+}
+
+Dev iigdevtab = {
+	'G',
+	"iig",
+
+	iigreset,
+	devinit,
+	devshutdown,
+	iigattach,
+	iigwalk,
+	iigstat,
+	iigopen,
+	devcreate,
+	iigclose,
+	iigread,
+	devbread,
+	iigwrite,
+	devbwrite,
+	devremove,
+	devwstat,
+};
