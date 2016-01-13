3576eabea0611ae3cec434315a92593142135313
Ronald G. Minnich <rminnich@gmail.com>
2016-01-12 18:38:40 -0800

    gdb server compiles.
    It also responds to simple requests. Now to get it enumerating threads.
    
    Change-Id: Idf950b2fab1c93bcbb3c111c41827bc13f6507c2
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/cmd/gdbserver/debug_core.h
+++ sys/src/cmd/gdbserver/debug_core.h
@@ -8,30 +8,26 @@
  * warranty of any kind, whether express or implied.
  */
 
-#ifndef _DEBUG_CORE_H_
-#define _DEBUG_CORE_H_
 /*
  * These are the private implementation headers between the kernel
  * debugger core and the debugger front end code.
  */
+typedef struct Ureg Ureg;
 
 /* kernel debug core data structures */
-struct kgdb_state {
+struct state {
 	int			ex_vector;
 	int			signo;
 	int			err_code;
 	int			cpu;
 	int			pass_exception;
 	unsigned long		thr_query;
-	unsigned long		threadid;
-	long			kgdb_usethreadid;
-	struct pt_regs		*linux_regs;
+	unsigned long threadid;
+	char *pidname;
+	long			usethreadid;
+	Ureg ureg;
 };
 
-/* Exception state values */
-#define DCPU_WANT_MASTER 0x1 /* Waiting to become a master kgdb cpu */
-#define DCPU_NEXT_MASTER 0x2 /* Transition from one master cpu to another */
-#define DCPU_IS_SLAVE    0x4 /* Slave cpu enter exception */
 #define DCPU_SSTEP       0x8 /* CPU is single stepping */
 
 struct debuggerinfo_struct {
@@ -43,14 +39,14 @@ struct debuggerinfo_struct {
 	int			enter_kgdb;
 };
 
-extern struct debuggerinfo_struct kgdb_info[];
+extern struct debuggerinfo_struct info[];
 
 /* kernel debug core break point routines */
-extern int dbg_remove_all_break(void);
-extern int dbg_set_sw_break(unsigned long addr);
-extern int dbg_remove_sw_break(unsigned long addr);
-extern int dbg_activate_sw_breakpoints(void);
-extern int dbg_deactivate_sw_breakpoints(void);
+extern char * dbg_remove_all_break(struct state *);
+extern char * dbg_set_sw_break(struct state *, unsigned long addr);
+extern char * dbg_remove_sw_break(struct state *, unsigned long addr);
+extern char * dbg_activate_sw_breakpoints(struct state *);
+extern char * dbg_deactivate_sw_breakpoints(struct state *);
 
 /* polled character access to i/o module */
 extern int dbg_io_get_char(void);
@@ -62,21 +58,14 @@ extern int dbg_io_get_char(void);
 extern int dbg_switch_cpu;
 
 /* gdbstub interface functions */
-extern int gdb_serial_stub(struct kgdb_state *ks);
+extern int gdb_serial_stub(struct state *ks);
 extern void gdbstub_msg_write(const char *s, int len);
 
 /* gdbstub functions used for kdb <-> gdbstub transition */
-extern int gdbstub_state(struct kgdb_state *ks, char *cmd);
+extern char *gdbstub_state(struct state *ks, char *cmd);
 extern int dbg_kdb_mode;
 
-#ifdef CONFIG_KGDB_KDB
-extern int kdb_stub(struct kgdb_state *ks);
-extern int kdb_parse(const char *cmdstr);
-#else /* ! CONFIG_KGDB_KDB */
-static inline int kdb_stub(struct kgdb_state *ks)
-{
-	return DBG_PASS_EVENT;
-}
-#endif /* CONFIG_KGDB_KDB */
-
-#endif /* _DEBUG_CORE_H_ */
+char *gpr(Ureg*regs, int pid);
+char *rmem(void *dest, int pid, uint64_t addr, int size);
+char *wmem(uint64_t dest, int pid, void *addr, int size);
+#define MAX_BREAKPOINTS 32768
