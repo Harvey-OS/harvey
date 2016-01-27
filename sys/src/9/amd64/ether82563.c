4fd75744926150212a0c0cd3c5a2ea51d0637d12
Sevki <sevki@spotify.com>
2016-01-27 12:46:31 +0100

    build: added new build files
    kernel compiles and crashes
    
    closes #37
    
    to test use
    
    	go get -u sevki.org/build/cmd/build
    	build //sys/src/9/amd64:harvey
    or
    
    	build -v //sys/src/9/amd64:harvey
    
    inside a editor like emacs or acme.
    
    If you are building on OS X you should also have something like .build
    file at the root of the project and add something like
    
    	CC: gcc
    	TOOLPREFIX: x86_64-elf-
    
    Change-Id: Ib6f6156eee1936ceb5f8b0bfa64ed45589195c31
    Signed-off-by: Sevki <sevki@spotify.com>

--- sys/src/9/amd64/ether82563.c
+++ sys/src/9/amd64/ether82563.c
@@ -16,7 +16,7 @@
 #include "dat.h"
 #include "fns.h"
 #include "io.h"
-#include "../port/error.h"
+#include <error.h>
 #include "../port/netif.h"
 #include "etherif.h"
 
--- sys/src/9/amd64/mouse.c
+++ sys/src/9/amd64/mouse.c
@@ -12,7 +12,7 @@
 #include "mem.h"
 #include "dat.h"
 #include "fns.h"
-#include "../port/error.h"
+#include <error.h>
 #include "io.h"
 
 #define	Image	IMAGE
