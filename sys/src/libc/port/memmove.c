ea0d099e3d4be211a985fabef62550d394fa2a40
Keith Poole <keith.poole@gmail.com>
2016-01-29 13:40:16 +0000

    Identified that memmove is a bottleneck
    During performance testing, I've noticed that memmove
    is a bottleneck, I've replaced it with the BSD memmove
    which is 20x faster.
    
    Change-Id: I2eb8ea0c4aaffa7e9bab0a77bf5ee32ca975b2e5
    Signed-off-by: Keith Poole <keith.poole@gmail.com>

--- sys/src/libc/port/memmove.c
+++ sys/src/libc/port/memmove.c
@@ -1,45 +1,122 @@
-/*
- * This file is part of the UCB release of Plan 9. It is subject to the license
- * terms in the LICENSE file found in the top-level directory of this
- * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
- * part of the UCB release of Plan 9, including this file, may be copied,
- * modified, propagated, or distributed except according to the terms contained
- * in the LICENSE file.
+/*-
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * This code is derived from software contributed to Berkeley by
+ * Chris Torek.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
-
 #include	<u.h>
 #include	<libc.h>
+typedef	int word;		/* "word" used for optimal copy speed */
 
-void*
-memmove(void *a1, const void *a2, size_t n)
+#define	wsize	sizeof(word)
+#define	wmask	(wsize - 1)
+
+/*
+ * Copy a block of memory, handling overlap.
+ * This is the routine that actually implements
+ * (the portable versions of) bcopy, memcpy, and memmove.
+ */
+void *
+memmove(
+	void *dst0,
+	const void *src0,
+	register size_t length
+	)
 {
-	char *s1;
-	const char *s2;
+	register char *dst = dst0;
+	register const char *src = src0;
+	register size_t t;
 
-	if((int32_t)n < 0)
-		abort();
-	s1 = a1;
-	s2 = a2;
-	if((s2 < s1) && (s2+n > s1))
-		goto back;
-	while(n > 0) {
-		*s1++ = *s2++;
-		n--;
-	}
-	return a1;
+	if (length == 0 || dst == src)		/* nothing to do */
+	    goto done;
 
-back:
-	s1 += n;
-	s2 += n;
-	while(n > 0) {
-		*--s1 = *--s2;
-		n--;
+	/*
+	 * Macros: loop-t-times; and loop-t-times, t>0
+	 */
+#define	TLOOP(s) if (t) TLOOP1(s)
+#define	TLOOP1(s) do { s; } while (--t)
+
+	if ((unsigned long)dst < (unsigned long)src) {
+		/*
+		 * Copy forward.
+		 */
+		t = ((size_t)src) & wmask;	/* only need low bits */
+		if ((t | (size_t)dst) & wmask) {
+			/*
+			 * Try to align operands.  This cannot be done
+			 * unless the low bits match.
+			 */
+			if ((t ^ (size_t)dst) & wmask || length < wsize)
+			    t = length;
+			else
+			    t = wsize - (t & wmask);
+			length -= t;
+			TLOOP1(*dst++ = *src++);
+		}
+		/*
+		 * Copy whole words, then mop up any trailing bytes.
+		 */
+		t = length / wsize;
+		TLOOP(*(word *)dst = *(word *)src; src += wsize; dst += wsize);
+		t = length & wmask;
+		TLOOP(*dst++ = *src++);
+	} else {
+		/*
+		 * Copy backwards.  Otherwise essentially the same.
+		 * Alignment works as before, except that it takes
+		 * (t&wmask) bytes to align, not wsize-(t&wmask).
+		 */
+		src += length;
+		dst += length;
+		t = (size_t)src & wmask;
+		if ((t | (size_t)dst) & wmask) {
+			if ((t ^ (size_t)dst) & wmask || length <= wsize)
+			    t = length;
+			else
+			    t &= wmask;
+			length -= t;
+			TLOOP1(*--dst = *--src);
+		}
+		t = length / wsize;
+		TLOOP(src -= wsize; dst -= wsize; *(word *)dst = *(word *)src);
+		t = length & wmask;
+		TLOOP(*--dst = *--src);
 	}
-	return a1;
+    done:
+	return (dst0);
 }
 
 void*
 memcpy(void *a1, const void *a2, size_t n)
 {
-	return memmove(a1, a2, n);
+       return memmove(a1, a2, n);
 }
