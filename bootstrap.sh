d766223618c721da8a0fca03d235dfbcf183e13b
Sevki Hasirci <s@sevki.org>
2016-03-01 19:57:12 +0100

    more travis

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
-echo Building the build tool...
-GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go get -d harvey/cmd/... # should really vendor these bits
-GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go install github.com/rminnich/ninep/srv/examples/ufs harvey/cmd/...
-GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go get sevki.org/build/cmd/build
+echo Downloading the build tool...
+
+curl -L http://sevki.co/get-build -o util/build
+
+cat util/build
+chmod +x util/build
 
 # this will make booting a VM easier
 mkdir -p tmp
 
 cat <<EOF
 
-For now, we have on architecture:
-export ARCH=amd64
-
 And build:
-./util/build
+./util/build //.:kernel
 
 See \`build -h' for more information on the build tool.
 EOF
