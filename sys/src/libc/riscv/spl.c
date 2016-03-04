39dcf5ac8bd15d5376afce962843487f309f2b25
Ronald G. Minnich <rminnich@gmail.com>
2016-03-03 18:14:26 -0800

    riscv: add spl[hi,lo,x] support
    This is not implemented optimally but that's what compilers
    are supposed to take care of. It's easy on the eyes.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ sys/src/libc/riscv/spl.c
@@ -0,0 +1,30 @@
+#include <u.h>
+#include <libc.h>
+#include <ureg.h>
+
+
+/* these are declared in this file as we do not want them externally visible.
+ * inline assembly is not allowed in harvey.
+ */
+
+int64_t _splhi(void);
+int64_t _spllo(void);
+
+int splhi(void)
+{
+	return _splhi();
+}
+
+int spllo(void)
+{
+	return _spllo();
+}
+
+void splx(int s)
+{
+	if (s)
+		_splhi();
+	else
+		_spllo();
+}
+		
