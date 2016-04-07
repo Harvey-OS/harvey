ddd5109e4a7ebc7539b0b1851525fc094bc17fbe
Ronald G. Minnich <rminnich@gmail.com>
2016-04-07 16:04:23 -0700

    Now catching keyboard input.
    I *think* all I need to get it to the 9p server is a write to, uh,
    well, who knows. c1? This code is interesting.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/cmd/cmux/wind.c
+++ sys/src/cmd/cmux/wind.c
@@ -98,14 +98,14 @@ void
 winctl(void *arg)
 {
 	print_func_entry();
-	Rune *rp, *bp, *tp, *up, *kbdr;
-	uint qh;
-	int nr, nb, c, wid, i, npart, initial, lastb;
+	Rune *rp, *bp, *kbdr;
+	int nr, nb, c, wid, i, npart, lastb;
 	char *s, *t, part[3];
 	Window *w;
 	Mousestate *mp, m;
-	enum { WKey, /*WMouse, WMouseread, */WCtl, WCwrite, WCread, WWread, NWALT };
+	enum { WKey, WMouse, WMouseread, WCtl, WCwrite, WCread, WWread, NWALT };
 	Alt alts[NWALT+1];
+	int walt;
 	Mousereadmesg mrm;
 	Conswritemesg cwm;
 	Consreadmesg crm;
@@ -128,14 +128,12 @@ winctl(void *arg)
 	alts[WKey].c = w->ck;
 	alts[WKey].v = &kbdr;
 	alts[WKey].op = CHANRCV;
-#if 0
 	alts[WMouse].c = w->mc.c;
 	alts[WMouse].v = &w->mc.Mouse;
-	alts[WMouse].op = CHANRCV;
+	alts[WMouse].op = CHANNOP; // XXXCHANRCV;
 	alts[WMouseread].c = w->mouseread;
 	alts[WMouseread].v = &mrm;
-	alts[WMouseread].op = CHANSND;
-#endif
+	alts[WMouseread].op = CHANNOP; // XXXCHANSND;
 	alts[WCtl].c = w->cctl;
 	alts[WCtl].v = &wcm;
 	alts[WCtl].op = CHANRCV;
@@ -153,18 +151,22 @@ winctl(void *arg)
 	npart = 0;
 	lastb = -1;
 	for(;;){
-#if 0
-		if(w->mouseopen && w->mouse.counter != w->mouse.lastcounter)
+		// TODO: mouses
+		if(0 && w->mouseopen && w->mouse.counter != w->mouse.lastcounter)
 			alts[WMouseread].op = CHANSND;
 		else
 			alts[WMouseread].op = CHANNOP;
-#endif
 		if(w->deleted || !w->wctlready)
 			alts[WWread].op = CHANNOP;
 		else
 			alts[WWread].op = CHANSND;
 		/* this code depends on NL and EOT fitting in a single byte */
 		/* kind of expensive for each loop; worth precomputing? */
+		/*if(w->holding)
+			alts[WCread].op = CHANNOP;
+			else*/ if(npart || (w->rawing && w->nraw>0))
+			alts[WCread].op = CHANSND;
+		else
 		{
 			alts[WCread].op = CHANNOP;
 			for(i=w->qh; i<w->nr; i++){
@@ -175,15 +177,18 @@ winctl(void *arg)
 				}
 			}
 		}
-		switch(alt(alts)){
+fprint(2, "--------> call alts:");
+		walt = alt(alts);
+fprint(2, "GETS %d\n", walt);
+		switch(walt){
 		case WKey:
-//			for(i=0; kbdr[i]!=L'\0'; i++)
-//				wkeyctl(w, kbdr[i]);
-//			wkeyctl(w, r);
+fprint(2, "WKEY! \n");
+			for(i=0; kbdr[i]!=L'\0'; i++)
+				wkeyctl(w, kbdr[i]);
+			//wkeyctl(w, r);
 ///			while(nbrecv(w->ck, &r))
-//				wkeyctl(w, r);
+				//wkeyctl(w, r);
 			break;
-#if 0
 		case WMouse:
 			if(w->mouseopen) {
 				w->mouse.counter++;
@@ -217,7 +222,6 @@ winctl(void *arg)
 			w->mouse.lastcounter = m.counter;
 			send(mrm.cm, &m.Mouse);
 			continue;
-#endif
 		case WCtl:
 			exits("WCtl can't do");
 #if 0
@@ -245,7 +249,7 @@ winctl(void *arg)
 			free(rp);
 			break;
 		case WCread:
-fprint(2, "Console read\n");
+fprint(2, "------------------>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Console read\n");
 			recv(crm.c1, &pair);
 			t = pair.s;
 			nb = pair.ns;
@@ -301,6 +305,150 @@ fprint(2, "Console read\n");
 		}
 	}
 	print_func_exit();
+}
+
+void
+wkeyctl(Window *w, Rune r)
+{
+
+
+	if(r == 0)
+		return;
+	if(w->deleted)
+		return;
+	fprint(2, "wkyctl: skipping all ctl chars\n");
+#if 0
+	uint q0 ,q1;
+	int n, nb, nr;
+
+	Rune *rp;
+	int *notefd;
+	/* navigation keys work only when mouse is not open */
+	if(!w->mouseopen)
+		switch(r){
+		case Kdown:
+			n = w->maxlines/3;
+			goto case_Down;
+		case Kscrollonedown:
+			n = mousescrollsize(w->maxlines);
+			if(n <= 0)
+				n = 1;
+			goto case_Down;
+		case Kpgdown:
+			n = 2*w->maxlines/3;
+		case_Down:
+			q0 = w->org+frcharofpt(w, Pt(w->Frame.r.min.x, w->Frame.r.min.y+n*w->font->height));
+			wsetorigin(w, q0, TRUE);
+			return;
+		case Kup:
+			n = w->maxlines/3;
+			goto case_Up;
+		case Kscrolloneup:
+			n = mousescrollsize(w->maxlines);
+			if(n <= 0)
+				n = 1;
+			goto case_Up;
+		case Kpgup:
+			n = 2*w->maxlines/3;
+		case_Up:
+			q0 = wbacknl(w, w->org, n);
+			wsetorigin(w, q0, TRUE);
+			return;
+		case Kleft:
+			if(w->q0 > 0){
+				q0 = w->q0-1;
+				wsetselect(w, q0, q0);
+				wshow(w, q0);
+			}
+			return;
+		case Kright:
+			if(w->q1 < w->nr){
+				q1 = w->q1+1;
+				wsetselect(w, q1, q1);
+				wshow(w, q1);
+			}
+			return;
+		case Khome:
+			wshow(w, 0);
+			return;
+		case Kend:
+			wshow(w, w->nr);
+			return;
+		case 0x01:	/* ^A: beginning of line */
+			if(w->q0==0 || w->q0==w->qh || w->run[w->q0-1]=='\n')
+				return;
+			nb = wbswidth(w, 0x15 /* ^U */);
+			wsetselect(w, w->q0-nb, w->q0-nb);
+			wshow(w, w->q0);
+			return;
+		case 0x05:	/* ^E: end of line */
+			q0 = w->q0;
+			while(q0 < w->nr && w->run[q0]!='\n')
+				q0++;
+			wsetselect(w, q0, q0);
+			wshow(w, w->q0);
+			return;
+		}
+	if(w->rawing && (w->q0==w->nr || w->mouseopen)){
+		waddraw(w, &r, 1);
+		return;
+	}
+	if(r==0x1B || (w->holding && r==0x7F)){	/* toggle hold */
+		if(w->holding)
+			--w->holding;
+		else
+			w->holding++;
+		wrepaint(w);
+		if(r == 0x1B)
+			return;
+	}
+	if(r != 0x7F){
+		wsnarf(w);
+		wcut(w);
+	}
+	switch(r){
+	case 0x7F:		/* send interrupt */
+		w->qh = w->nr;
+		wshow(w, w->qh);
+		notefd = emalloc(sizeof(int));
+		*notefd = w->notefd;
+		proccreate(interruptproc, notefd, 4096);
+		return;
+	case 0x06:	/* ^F: file name completion */
+	case Kins:		/* Insert: file name completion */
+		rp = namecomplete(w);
+		if(rp == nil)
+			return;
+		nr = runestrlen(rp);
+		q0 = w->q0;
+		q0 = winsert(w, rp, nr, q0);
+		wshow(w, q0+nr);
+		free(rp);
+		return;
+	case 0x08:	/* ^H: erase character */
+	case 0x15:	/* ^U: erase line */
+	case 0x17:	/* ^W: erase word */
+		if(w->q0==0 || w->q0==w->qh)
+			return;
+		nb = wbswidth(w, r);
+		q1 = w->q0;
+		q0 = q1-nb;
+		if(q0 < w->org){
+			q0 = w->org;
+			nb = q1-q0;
+		}
+		if(nb > 0){
+			wdelete(w, q0, q0+nb);
+			wsetselect(w, q0, q0);
+		}
+		return;
+	}
+
+	/* otherwise ordinary character; just insert */
+	q0 = w->q0;
+	q0 = winsert(w, &r, 1, q0);
+	wshow(w, q0+1);
+#endif
 }
 
 void
@@ -403,6 +551,7 @@ void
 wcurrent(Window *w)
 {
 	print_func_entry();
+	if (0) {
 	Window *oi;
 
 	if(wkeyboard!=nil && w==wkeyboard) {
@@ -421,6 +570,8 @@ wcurrent(Window *w)
 			wsendctlmesg(w, Wakeup, nil);
 		}
 	}
+	}
+	input = w;
 	print_func_exit();
 }
 
