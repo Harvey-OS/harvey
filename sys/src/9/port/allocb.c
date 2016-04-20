2b07a4c3e8aa8d634cae901009481cc07e068f04
Ronald G. Minnich <rminnich@gmail.com>
2016-04-19 19:44:55 -0700

    Further de-anonymizing. Harvey still boots fine.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/9/port/allocb.c
+++ sys/src/9/port/allocb.c
@@ -21,7 +21,7 @@ enum
 
 struct
 {
-	Lock;
+	Lock Lock;
 	uint32_t	bytes;
 } ialloc;
 
@@ -107,9 +107,9 @@ iallocb(int size)
 	}
 	b->flag = BINTR;
 
-	ilock(&ialloc);
+	ilock(&ialloc.Lock);
 	ialloc.bytes += b->lim - b->base;
-	iunlock(&ialloc);
+	iunlock(&ialloc.Lock);
 
 	return b;
 }
@@ -132,9 +132,9 @@ freeb(Block *b)
 		return;
 	}
 	if(b->flag & BINTR) {
-		ilock(&ialloc);
+		ilock(&ialloc.Lock);
 		ialloc.bytes -= b->lim - b->base;
-		iunlock(&ialloc);
+		iunlock(&ialloc.Lock);
 	}
 
 	p = b->base;
