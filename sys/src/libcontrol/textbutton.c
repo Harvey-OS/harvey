c8f6adcfeb4c6aa2d76df41e8d7a50b0218f7d72
Ronald G. Minnich <rminnich@gmail.com>
2016-04-30 08:24:22 -0700

    deanono libcontrol
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/libcontrol/textbutton.c
+++ sys/src/libcontrol/textbutton.c
@@ -94,7 +94,7 @@ textbuttonmouse(Control *c, Mouse *m)
 
 	t = (Textbutton *)c;
 	if(m->buttons&7) {
-		if (ptinrect(m->xy,t->rect)) {
+		if (ptinrect(m->xy,t->Control.rect)) {
 			if (t->off) {
 				t->off = 0;
 				textbuttonshow(t);
@@ -115,8 +115,8 @@ textbuttonmouse(Control *c, Mouse *m)
 				t->pressed = 1;
 			textbuttonshow(t);
 		}else{	/* generate event on button up */
-			if (ptinrect(m->xy,t->rect))
-				chanprint(t->event, t->format, t->name, t->pressed);
+			if (ptinrect(m->xy,t->Control.rect))
+				chanprint(t->Control.event, t->Control.format, t->Control.name, t->pressed);
 			else {
 				t->off = 0;
 				t->pressed = t->prepress;
@@ -156,12 +156,12 @@ textbuttonshow(Textbutton *t)
 	Point p, q;
 	Image *im;
 
-	if(t->hidden || (t->lastshow == t->pressed && t->showoff == t->off))
+	if(t->Control.hidden || (t->lastshow == t->pressed && t->showoff == t->off))
 		return;
 	f = t->font->font;
-	draw(t->screen, t->rect, t->image->image, nil, t->image->image->r.min);
+	draw(t->Control.screen, t->Control.rect, t->image->image, nil, t->image->image->r.min);
 	if(t->border > 0)
-		border(t->screen, t->rect, t->border, t->bordercolor->image, ZP);
+		border(t->Control.screen, t->Control.rect, t->border, t->bordercolor->image, ZP);
 	/* text goes here */
 	dx = 0;
 	for(i=0; i<t->nline; i++){
@@ -170,7 +170,7 @@ textbuttonshow(Textbutton *t)
 			dx = w;
 	}
 	dy = t->nline*f->height;
-	clipr = insetrect(t->rect, t->border);
+	clipr = insetrect(t->Control.rect, t->border);
 	p = _ctlalignpoint(clipr, dx, dy, t->align);
 	im = t->textcolor->image;
 	if(t->off)
@@ -182,13 +182,13 @@ textbuttonshow(Textbutton *t)
 		r.max.x = p.x+dx;
 		r.max.y = p.y+f->height;
 		q = _ctlalignpoint(r, stringwidth(f, t->line[i]), f->height, t->align%3);
-		_string(t->screen, q, im,
+		_string(t->Control.screen, q, im,
 			ZP, f, t->line[i], nil, strlen(t->line[i]),
 			clipr, nil, ZP, SoverD);
 		p.y += f->height;
 	}
 	if(t->off || t->pressed)
-		draw(t->screen, t->rect, t->light->image, t->mask->image, t->mask->image->r.min);
+		draw(t->Control.screen, t->Control.rect, t->light->image, t->mask->image, t->mask->image->r.min);
 	t->lastshow = t->pressed;
 	t->showoff = t->off;
 	flushimage(display, 1);
@@ -205,82 +205,82 @@ textbuttonctl(Control *c, CParse *cp)
 	cmd = _ctllookup(cp->args[0], cmds, nelem(cmds));
 	switch(cmd){
 	default:
-		ctlerror("%q: unrecognized message '%s'", t->name, cp->str);
+		ctlerror("%q: unrecognized message '%s'", t->Control.name, cp->str);
 		break;
 	case EAlign:
-		_ctlargcount(t, cp, 2);
+		_ctlargcount(&t->Control, cp, 2);
 		t->align = _ctlalignment(cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EBorder:
-		_ctlargcount(t, cp, 2);
+		_ctlargcount(&t->Control, cp, 2);
 		t->border = cp->iargs[1];
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EBordercolor:
-		_ctlargcount(t, cp, 2);
-		_setctlimage(t, &t->bordercolor, cp->args[1]);
+		_ctlargcount(&t->Control, cp, 2);
+		_setctlimage(&t->Control, &t->bordercolor, cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EFocus:
 		break;
 	case EFont:
-		_ctlargcount(t, cp, 2);
+		_ctlargcount(&t->Control, cp, 2);
 		_setctlfont(t, &t->font, cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EFormat:
-		_ctlargcount(t, cp, 2);
-		t->format = ctlstrdup(cp->args[1]);
+		_ctlargcount(&t->Control, cp, 2);
+		t->Control.format = ctlstrdup(cp->args[1]);
 		break;
 	case EHide:
-		_ctlargcount(t, cp, 1);
-		t->hidden = 1;
+		_ctlargcount(&t->Control, cp, 1);
+		t->Control.hidden = 1;
 		break;
 	case EImage:
-		_ctlargcount(t, cp, 2);
-		_setctlimage(t, &t->image, cp->args[1]);
+		_ctlargcount(&t->Control, cp, 2);
+		_setctlimage(&t->Control, &t->image, cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case ELight:
-		_ctlargcount(t, cp, 2);
-		_setctlimage(t, &t->light, cp->args[1]);
+		_ctlargcount(&t->Control, cp, 2);
+		_setctlimage(&t->Control, &t->light, cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EMask:
-		_ctlargcount(t, cp, 2);
-		_setctlimage(t, &t->mask, cp->args[1]);
+		_ctlargcount(&t->Control, cp, 2);
+		_setctlimage(&t->Control, &t->mask, cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EPaletextcolor:
-		_ctlargcount(t, cp, 2);
-		_setctlimage(t, &t->paletextcolor, cp->args[1]);
+		_ctlargcount(&t->Control, cp, 2);
+		_setctlimage(&t->Control, &t->paletextcolor, cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EPressedtextcolor:
-		_ctlargcount(t, cp, 2);
-		_setctlimage(t, &t->pressedtextcolor, cp->args[1]);
+		_ctlargcount(&t->Control, cp, 2);
+		_setctlimage(&t->Control, &t->pressedtextcolor, cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case ERect:
-		_ctlargcount(t, cp, 5);
+		_ctlargcount(&t->Control, cp, 5);
 		r.min.x = cp->iargs[1];
 		r.min.y = cp->iargs[2];
 		r.max.x = cp->iargs[3];
 		r.max.y = cp->iargs[4];
 		if(Dx(r)<=0 || Dy(r)<=0)
-			ctlerror("%q: bad rectangle: %s", t->name, cp->str);
-		t->rect = r;
+			ctlerror("%q: bad rectangle: %s", t->Control.name, cp->str);
+		t->Control.rect = r;
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EReveal:
-		_ctlargcount(t, cp, 1);
-		t->hidden = 0;
+		_ctlargcount(&t->Control, cp, 1);
+		t->Control.hidden = 0;
 		t->lastshow = -1;	/* force redraw */
 		textbuttonshow(t);
 		break;
 	case EShow:
-		_ctlargcount(t, cp, 1);
+		_ctlargcount(&t->Control, cp, 1);
 		t->lastshow = -1;	/* force redraw */
 		textbuttonshow(t);
 		break;
@@ -288,16 +288,16 @@ textbuttonctl(Control *c, CParse *cp)
 		if (cp->nargs == 3)
 			r.max = Pt(0x7fffffff, 0x7fffffff);
 		else{
-			_ctlargcount(t, cp, 5);
+			_ctlargcount(&t->Control, cp, 5);
 			r.max.x = cp->iargs[3];
 			r.max.y = cp->iargs[4];
 		}
 		r.min.x = cp->iargs[1];
 		r.min.y = cp->iargs[2];
 		if(r.min.x<=0 || r.min.y<=0 || r.max.x<=0 || r.max.y<=0 || r.max.x < r.min.x || r.max.y < r.min.y)
-			ctlerror("%q: bad sizes: %s", t->name, cp->str);
-		t->size.min = r.min;
-		t->size.max = r.max;
+			ctlerror("%q: bad sizes: %s", t->Control.name, cp->str);
+		t->Control.size.min = r.min;
+		t->Control.size.max = r.max;
 		break;
 	case EText:
 		/* free existing text */
@@ -311,12 +311,12 @@ textbuttonctl(Control *c, CParse *cp)
 		textbuttonshow(t);
 		break;
 	case ETextcolor:
-		_ctlargcount(t, cp, 2);
-		_setctlimage(t, &t->textcolor, cp->args[1]);
+		_ctlargcount(&t->Control, cp, 2);
+		_setctlimage(&t->Control, &t->textcolor, cp->args[1]);
 		t->lastshow = -1;	/* force redraw */
 		break;
 	case EValue:
-		_ctlargcount(t, cp, 2);
+		_ctlargcount(&t->Control, cp, 2);
 		if((cp->iargs[1]!=0) != t->pressed){
 			t->pressed ^= 1;
 			textbuttonshow(t);
@@ -341,11 +341,11 @@ createtextbutton(Controlset *cs, char *name)
 	t->pressedtextcolor = _getctlimage("black");
 	t->paletextcolor = _getctlimage("paleyellow");
 	t->font = _getctlfont("font");
-	t->format = ctlstrdup("%q: value %d");
+	t->Control.format = ctlstrdup("%q: value %d");
 	t->lastshow = -1;
-	t->mouse = textbuttonmouse;
-	t->ctl = textbuttonctl;
-	t->exit = textbuttonfree;
+	t->Control.mouse = textbuttonmouse;
+	t->Control.ctl = textbuttonctl;
+	t->Control.exit = textbuttonfree;
 	t->prepress = 0;
 	t->off = 0;
 	t->showoff = -1;
