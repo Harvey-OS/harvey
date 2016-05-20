cadd8b3ca2ecb4b2ec74fb8a5d61e2fd6ae0f5fd
Sevki <s@sevki.org>
2016-05-20 05:50:02 +0300

    ci: add scanbuild

--- util/continuous-build.sh
+++ util/continuous-build.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 set -e
 
-if [ "${COVERITY_SCAN_BRANCH}" != 1 ]; then
-	if [ $OLD_BUILD == true ]; then
-		export SH=`which rc`
-		git clean -x -d -f
-		(cd $TRAVIS_BUILD_DIR && ./bootstrap.sh)
+if [ $OLD_BUILD == true ]; then
+	export SH=`which rc`
+	git clean -x -d -f
+	(cd $TRAVIS_BUILD_DIR && ./bootstrap.sh)
 
-		echo
-		echo "Vendorized code verification..."
-		echo
-		for v in `find|grep vendor.json`; do
-			echo "cd `dirname $v`"
-			(cd `dirname $v`; vendor -check)
-		done
-		echo
+	echo
+	echo "Vendorized code verification..."
+	echo
+	for v in `find|grep vendor.json`; do
+		echo "cd `dirname $v`"
+		(cd `dirname $v`; vendor -check)
+	done
+	echo
 
-		build all
-	else
-		curl -L http://sevki.co/get-build -o util/build
-		chmod +x util/build
+	build all
+else
 
+	curl -L http://sevki.co/get-build -o util/build
+	chmod +x util/build
+
+	if [[  ($CC == "clang") && ($TRAVIS_PULL_REQUEST !=  true) ]]; then 
+			scan-build ./util/build -v //.:kernel
+
+			curl -L http://sevki.co/4qf_NS -o util/scanscan
+			chmod +x util/scanscan
+
+			#scanscan finds and uploads scan-build files
+			./util/scanscan t
+ 	else 
 		./util/build -v //.:kernel
+
 	fi
 fi
+
