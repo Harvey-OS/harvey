82a271ca45e37dcd9320d29e0adf2e21cb5a83c8
Sevki <s@sevki.org>
2016-06-08 03:15:53 +0300

    Update bootstrap.sh
    what's up googlers?

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,11 +6,10 @@ git submodule update
 GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go get -d harvey/cmd/... # should really vendor these bits
 GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go install github.com/rminnich/ninep/srv/examples/ufs harvey/cmd/...
 
-if [ $OLD_BUILD != true ]; then
-	echo Downloading the build tool...
-	curl -L http://sevki.co/get-build -o util/build
-	chmod +x util/build
-fi
+echo Downloading the blaze tool...
+curl -L http://sevki.co/get-build -o util/blaze
+chmod +x util/blaze
+
 
 # this will make booting a VM easier
 mkdir -p tmp
@@ -19,8 +18,12 @@ cat <<EOF
 # We support RISC-V, but the default is x86_64 (which we call amd64 for historical reasons):
 export ARCH=amd64
 # And build:
-./util/build -v //:kernel
+./util/build
 # See \`build -h' for more information on the build tool.
+
+or try the experimental build 
+./util/blaze -v //:kernel
+
 To enable access to files, create a harvey and none user:
 sudo useradd harvey
 sudo useradd none
