9ba14acb71f54c591f054ba764439d7bfa87cf8c
Ray Lai <ray@raylai.com>
2016-06-08 16:43:51 +0800

    Quote strings. I guess I missed some bashisms

--- util/continuous-build.sh
+++ util/continuous-build.sh
@@ -2,10 +2,10 @@
 set -e
 
 if [ "${COVERITY_SCAN_BRANCH}" != 1 ]; then
-	if [ $OLD_BUILD == true ]; then
+	if [ "$OLD_BUILD" == true ]; then
 		export SH=`which rc`
 		git clean -x -d -f
-		(cd $TRAVIS_BUILD_DIR && ./bootstrap.sh)
+		(cd "$TRAVIS_BUILD_DIR" && ./bootstrap.sh)
 
 		echo
 		echo "Vendorized code verification..."
