584800aee9ecfb9ade216b47b515477dfe4adc91
Sevki <s@sevki.org>
2016-06-08 03:30:22 +0300

    vendor: nuked

--- util/ci.sh
+++ util/ci.sh
@@ -6,15 +6,6 @@ if [ $OLD_BUILD == true ]; then
 	git clean -x -d -f
 	(cd $TRAVIS_BUILD_DIR && ./bootstrap.sh)
 
-	echo
-	echo "Vendorized code verification..."
-	echo
-	for v in `find|grep vendor.json`; do
-		echo "cd `dirname $v`"
-		(cd `dirname $v`; vendor -check)
-	done
-	echo
-
 	build all
 else
 
