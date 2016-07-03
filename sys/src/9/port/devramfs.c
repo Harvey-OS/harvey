8d00408388b47b11ebdb78955dc4e6ee27b1c877
Keith Poole <keith.poole@gmail.com>
2016-07-04 00:37:08 +0100

    Can now read file in subdir

--- sys/src/9/port/devramfs.c
+++ sys/src/9/port/devramfs.c
@@ -58,23 +58,18 @@ raminit(void)
 	ramroot->ramdir[2].length = 0;
         ramroot->ramdir[2].perm = DMDIR|0777;
 
-	subdir->inuse = 3;
+	subdir->inuse = 2;
         subdir->allocated = DIR_EXTEND_SIZE;
-        strcpy(subdir->ramdir[0].name, "..");
-        subdir->ramdir[0].qid = (Qid){(int64_t)ramroot, 0,QTDIR};
+        strcpy(subdir->ramdir[0].name, ".");
+        subdir->ramdir[0].qid = (Qid){(int64_t)subdir, 0,QTDIR};
         subdir->ramdir[0].length = 0;
         subdir->ramdir[0].perm = DMDIR|0777;
 
-        strcpy(subdir->ramdir[1].name, ".");
-        subdir->ramdir[1].qid = (Qid){(int64_t)subdir, 0,QTDIR};
-        subdir->ramdir[1].length = 0;
-        subdir->ramdir[1].perm = DMDIR|0777;
 
-
-        strcpy(subdir->ramdir[2].name, "README2");
-        subdir->ramdir[2].qid = (Qid){(int64_t)file};
-        subdir->ramdir[2].length = file->length;
-        subdir->ramdir[2].perm = 0444;
+        strcpy(subdir->ramdir[1].name, "README2");
+        subdir->ramdir[1].qid = (Qid){(int64_t)file};
+        subdir->ramdir[1].length = file->length;
+        subdir->ramdir[1].perm = 0444;
 
 }
 
@@ -105,8 +100,107 @@ ramattach(char *spec)
 static Walkqid*
 ramwalk(Chan *c, Chan *nc, char **name, int nname)
 {
-	struct Directory *dir = (struct Directory *)c->qid.path;
-	return devwalk(c,  nc, name, nname, dir->ramdir, dir->inuse, devgen);
+        Proc *up = externup();
+	struct Directory *directory = (struct Directory *)c->qid.path;
+	Dirtab *tab = directory->ramdir;
+	int ntab = directory->inuse;
+        int i, j, alloc;
+        Walkqid *wq;
+        char *n;
+        Dir dir;
+
+        if(nname > 0)
+                isdir(c);
+
+        alloc = 0;
+        wq = smalloc(sizeof(Walkqid)+(nname-1)*sizeof(Qid));
+        if(waserror()){
+                if(alloc && wq->clone!=nil)
+                        cclose(wq->clone);
+                free(wq);
+                return nil;
+        }
+        if(nc == nil){
+                nc = devclone(c);
+                alloc = 1;
+        }
+        wq->clone = nc;
+
+        for(j=0; j<nname; j++){
+                if(!(nc->qid.type & QTDIR)){
+                        if(j==0)
+                                error(Enotdir);
+                        goto Done;
+                }
+                n = name[j];
+                if(strcmp(n, ".") == 0){
+    Accept:
+                        wq->qid[wq->nqid++] = nc->qid;
+                        continue;
+                }
+                if(strcmp(n, "..") == 0){
+                        /*
+                         * Use c->dev->name in the error because
+                         * nc->dev should be nil here.
+                         */
+                        if(devgen(nc, nil, tab, ntab, DEVDOTDOT, &dir) != 1){
+                                print("devgen walk .. in dev%s %#llux broken\n",
+                                        c->dev->name, nc->qid.path);
+                                error("broken devgen");
+                        }
+                        nc->qid = dir.qid;
+                        goto Accept;
+                }
+                /*
+                 * Ugly problem: If we're using devgen, make sure we're
+                 * walking the directory itself, represented by the first
+                 * entry in the table, and not trying to step into a sub-
+                 * directory of the table, e.g. /net/net. Devgen itself
+                 * should take care of the problem, but it doesn't have
+                 * the necessary information (that we're doing a walk).
+                 */
+                if(nc->qid.path!=tab[0].qid.path)
+                        goto Notfound;
+                for(i=0;; i++) {
+                        switch(devgen(nc, n, tab, ntab, i, &dir)){
+                        case -1:
+                        Notfound:
+                                if(j == 0)
+                                        error(Enonexist);
+                                kstrcpy(up->errstr, Enonexist, ERRMAX);
+                                goto Done;
+                        case 0:
+                                continue;
+                        case 1:
+                                if(strcmp(n, dir.name) == 0){
+                                        nc->qid = dir.qid;
+					directory = (struct Directory *)nc->qid.path;
+					tab = directory->ramdir;
+					ntab = directory->inuse;
+                                        goto Accept;
+                                }
+                                continue;
+                        }
+                }
+        }
+        /*
+         * We processed at least one name, so will return some data.
+         * If we didn't process all nname entries succesfully, we drop
+         * the cloned channel and return just the Qids of the walks.
+         */
+Done:
+        poperror();
+        if(wq->nqid < nname){
+                if(alloc)
+                        cclose(wq->clone);
+                wq->clone = nil;
+        }else if(wq->clone){
+                /* attach cloned channel to same device */
+                wq->clone->dev = c->dev;
+                //if(wq->clone->dev)                    //XDYNX
+                //      devtabincr(wq->clone->dev);
+        }
+        return wq;
 }
 
 static int32_t
