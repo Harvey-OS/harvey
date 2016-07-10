99bd696ca5ca385ce6f2a780a0e3655f9cd47814
Keith Poole <keith.poole@gmail.com>
2016-07-10 22:36:55 +0100

    Can now create files.
    Can't delete or make directories.

--- sys/src/9/port/devramfs.c
+++ sys/src/9/port/devramfs.c
@@ -110,27 +110,27 @@ ramgen(Chan *c, char *name, Dirtab *tab, int ntab, int pos, Dir *dp)
 static Walkqid*
 ramwalk(Chan *c, Chan *nc, char **name, int nname)
 {
-	//qlock(&ramlock);
+	qlock(&ramlock);
 	Walkqid* wqid =  devwalk(c, nc, name, nname, 0, 0, ramgen);
-	//qunlock(&ramlock);
+	qunlock(&ramlock);
 	return wqid;
 }
 
 static int32_t
 ramstat(Chan *c, uint8_t *dp, int32_t n)
 {
-	//qlock(&ramlock);
+	qlock(&ramlock);
 	int32_t ret = devstat(c, dp, n, nil, 0, ramgen);
-	//qunlock(&ramlock);
+	qunlock(&ramlock);
 	return ret;
 }
 
 static Chan*
 ramopen(Chan *c, int omode)
 {
-	//qlock(&ramlock);
+	qlock(&ramlock);
 	Chan* ret = devopen(c, omode, nil, 0, ramgen);
-	//qunlock(&ramlock);
+	qunlock(&ramlock);
 	return ret;
 }
 
@@ -145,34 +145,82 @@ ramclose(Chan* c)
 static int32_t
 ramread(Chan *c, void *buf, int32_t n, int64_t off)
 {
-	//qlock(&ramlock);
+	qlock(&ramlock);
 	if (c->qid.type == QTDIR){
 		int32_t len =  devdirread(c, buf, n, nil, 0, ramgen);
-	//	qunlock(&ramlock);
+		qunlock(&ramlock);
 		return len;
 	}
 	// Read file
 	struct RamFile *file = (void*)c->qid.path;
 	int filelen = file->length;
 	if (off > filelen){
-	//	qunlock(&ramlock);
+		qunlock(&ramlock);
 		return 0;
 	}
 	if (off + n > filelen){
 		n = filelen - off;
 	}
 	memcpy(buf, file->data, n);
-//	qunlock(&ramlock);
+	qunlock(&ramlock);
 	return n;
 }
 
 static int32_t
-ramwrite(Chan* c, void* v, int32_t n, int64_t m)
+ramwrite(Chan* c, void* v, int32_t n, int64_t off)
 {
-//	qlock(&ramlock);
-	error(Egreg);		// TODO
-//	qunlock(&ramlock);
-	return 0;
+	qlock(&ramlock);
+	struct RamFile *file = (void*)c->qid.path;
+	if(n+off > file->length){
+		void *newfile = realloc(file->data, n+off);
+		if(newfile == nil){
+			return 0;
+		}
+		file->data = newfile;
+		file->length = n+off;
+	}
+	memcpy(file->data + off, v, n);
+	qunlock(&ramlock);
+	return n;
+}
+
+void
+ramcreate(Chan* c, char *name, int omode, int i)
+{
+        Proc *up = externup();
+
+        if(c->qid.type != QTDIR)
+                error(Eperm);
+
+	struct RamFile* parent = (struct RamFile *)c->qid.path;
+
+        omode = openmode(omode);
+        struct RamFile* file = (struct RamFile*)smalloc(sizeof(struct RamFile));
+        file->length = 0;
+        file->data = smalloc(4096);
+        strcpy(file->name, name);
+        file->perm = c->mode;
+        file->parent = parent;
+
+	qlock(&ramlock);
+        if(waserror()) {
+		free(file->data);
+		free(file);
+		qunlock(&ramlock);
+                nexterror();
+        }
+
+	file->sibling = parent->sibling;
+	parent->sibling = file;
+	qunlock(&ramlock);
+
+	mkqid(&c->qid, (uintptr_t)file, 0, file->directory ? QTDIR : 0);
+
+        poperror();
+
+        c->offset = 0;
+        c->mode = omode;
+        c->flag |= COPEN;
 }
 
 Dev ramdevtab = {
@@ -186,7 +234,7 @@ Dev ramdevtab = {
 	.walk = ramwalk,
 	.stat = ramstat,
 	.open = ramopen,
-	.create = devcreate,
+	.create = ramcreate,
 	.close = ramclose,
 	.read = ramread,
 	.bread = devbread,
