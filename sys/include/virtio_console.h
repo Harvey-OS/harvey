5108b3fe6b9b9ae98f13ffcae88f616f3787a1d0
Ronald G. Minnich <rminnich@gmail.com>
2016-09-19 14:26:43 -0700

    virtio: add pci definitions for qumra and virtio device names.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/include/virtio_console.h
+++ sys/include/virtio_console.h
@@ -53,9 +53,9 @@ struct virtio_console_config {
  * particular port.
  */
 struct virtio_console_control {
-	__virtio32 id;		/* Port number */
-	__virtio16 event;	/* The kind of control event (see below) */
-	__virtio16 value;	/* Extra information for the key */
+	uint32_t id;		/* Port number */
+	uint16_t event;	/* The kind of control event (see below) */
+	uint16_t value;	/* Extra information for the key */
 };
 
 /* Some events for control messages */
