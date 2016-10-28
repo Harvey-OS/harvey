2c6bbfd8b113c0bce3ddbd0d79e2d612042223b7
Dan Cross <cross@gajendra.net>
2016-10-28 15:01:23 +0000

    Rewrite cistrcmp and strcmp.
    Make strcmp and cistrcmp more straight-forward.  I believe
    the originals were correct, but these are more obvious.
    Note: Please review these *very* carefully.
    
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- sys/src/libc/port/strcmp.c
+++ sys/src/libc/port/strcmp.c
@@ -13,17 +13,16 @@
 int
 strcmp(const char *s1, const char *s2)
 {
-	unsigned c1, c2;
+	unsigned int c1, c2;
 
-	for(;;) {
+	do{
 		c1 = *s1++;
 		c2 = *s2++;
-		if(c1 != c2) {
-			if(c1 > c2)
-				return 1;
+		if(c1 < c2)
 			return -1;
-		}
-		if(c1 == 0)
-			return 0;
-	}
+		if(c1 > c2)
+			return 1;
+	}while(c1 != 0);
+
+	return 0;
 }
