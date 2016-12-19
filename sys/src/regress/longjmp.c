169da015d90793b6379454d1f8770ecd6acb8116
Ronald G. Minnich <rminnich@gmail.com>
2016-12-19 14:44:08 -0800

    riscv: fix setjmp/longjmp test regress/longjmp.c
    regress/longjmp.c had an architectural dependency
    in it; that's fixed.
    
    setjmp/longjmp were a bit wrong for riscv. Note
    that they're still wrong for FP.
    
    regress needs to be built after kernel since we
    include regression in riscv ramfs now.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/regress/longjmp.c
+++ sys/src/regress/longjmp.c
@@ -15,30 +15,39 @@ main(void)
 	int fail = 0;
 	jmp_buf label;
 
-	njmp = 0;
-	while((njmp = setjmp(label)) < Njmps)
+	while((njmp = setjmp(label)) < Njmps) {
 		longjmp(label, njmp+1);
+	}
 
 	for(i = 0; i < nelem(label); i++)
 		fprint(2, "label[%d] = %p\n", i, label[i]);
 	fprint(2, "main: %p foo: %p\n", main, foo);
 
-	if(njmp != Njmps)
+	if(njmp != Njmps) {
+		print("njmp is %d, wanted %d\n", njmp, Njmps);
 		fail++;
-	if(label[JMPBUFPC] < (uintptr_t)main)
+	}
+	if(label[JMPBUFPC] < (uintptr_t)main) {
+		print("label[%d] is %p, which is < main(%p)\n",
+			JMPBUFPC, label[JMPBUFPC], main);
 		fail++;
-	if(label[JMPBUFPC] > (uintptr_t)foo)
+	}
+	if(label[JMPBUFPC] > (uintptr_t)foo) {
+		print("label[%d] is %p, which is > foo(%p)\n",
+			JMPBUFPC, label[JMPBUFPC], foo);
 		fail++;
-	if(label[JMPBUFSP] > (uintptr_t)&label[nelem(label)])
+	}
+	if(label[JMPBUFSP] > (uintptr_t)&label[nelem(label)]) {
+		print("label[%d] is %p, which is > (uintptr_t)&label[nelem(label)](%p)\n",
+			JMPBUFSP, label[JMPBUFSP], foo);
 		fail++;
-	if(label[JMPBUFSP] < 0x7fffffd00000)
-		fail++;
+	}
 
 	if(fail == 0){
 		print("PASS\n");
 		exits(nil);
 	}
-	print("FAIL\n");
+	print("FAIL (%d errors)\n", fail);
 	exits("FAIL");
 }
 
