7ae80719bbb0c2c40e0c833d1e4477c7380b9c73
Ronald G. Minnich <rminnich@gmail.com>
2016-12-28 17:08:55 -0800

    riscv: Add a test for getcallerpc
    It doesn't work yet because inline only happens with -O2 or such.
    But at least it's there.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ sys/src/regress/getcallerpc.c
@@ -0,0 +1,41 @@
+#include <u.h>
+#include <libc.h>
+
+/*
+ * Do nothing. We can't exits from this one. It tests correctness of programs
+ * that just return.
+ */
+
+void c(void);
+void b(void);
+
+void a(void)
+{
+	uintptr_t d = getcallerpc()
+;
+	fprint(2, "This should print something more than %p and less than %p: %p\n",
+		b, c, d);
+	if ((d > b) && (d < c)) {
+		print("PASS\n");
+		exits("PASS");
+		return;
+	}
+	print("FAIL\n");
+	exits("FAIL");
+}
+
+void b(void)
+{
+	a();
+}
+
+void c(void)
+{
+	b();
+}
+
+void
+main(int argc, char *argv[])
+{
+	c();
+}
