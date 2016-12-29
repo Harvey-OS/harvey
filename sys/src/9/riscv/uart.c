df72d08beba07bb21e1b2032e1643a4289bfea4e
Ronald G. Minnich <rminnich@gmail.com>
2016-12-29 13:32:02 -0800

    riscv: extend uart support to include lowrisc uart
    The lowrisc uart is a memory-mapped 8250 with 32-bit
    aligned registers (as opposed to byte aligned).
    
    The support code is from coreboot.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/9/riscv/uart.c
+++ sys/src/9/riscv/uart.c
@@ -19,25 +19,72 @@
 #include "dat.h"
 #include "fns.h"
 
-uintptr_t uart_platform_base(int idx)
+void lowrisc_uart_init(int idx);
+void lowrisc_putchar(int idx, int data);
+unsigned char lowrisc_getchar(int idx);
+void lowrisc_flush(int idx);
+
+// riscv has no standard uart. Further, if we use a bad address,
+// the hardware explodes. So, the uart, if 0, is ignored.
+// At some point it will be ready.
+
+extern uint8_t *uart;
+extern uintptr_t uartpa;
+
+static void spike_putchar(uint8_t c)
 {
-	return (uintptr_t) 0x40001000;
+	*uart = c;
+}
+
+// Get a 7-bit char. < 0 means err.
+static int spike_getchar(void)
+{
+	if (uart[5] & 1) {
+		int c = uart[0];
+		if (0) print("getchar: got 0x%x\n", c);
+		return c;
+	}
+	return -1;
+}
+
+void uart_init(void)
+{
+	switch(uartpa){
+	case 0x42000000:
+		lowrisc_uart_init(0);
+	default:
+		break;
+	}
 }
 
 void putchar(uint8_t c)
 {
-	uint8_t *cp = KADDR(uart_platform_base(0));
-	*cp = c;
+	switch(uartpa){
+	case 0x40001000:
+		spike_putchar(c);
+		break;
+	case 0x42000000:
+		//lowrisc_flush(0);
+		lowrisc_putchar(0, c);
+		*uart = c;
+		break;
+	default:
+		break;
+	}
 }
 
 // Get a 7-bit char. < 0 means err.
 int getchar(void)
 {
-	uint8_t *cp = KADDR(uart_platform_base(0));
-	if (cp[5] & 1) {
-		int c = cp[0];
-		if (0) print("getchar: got 0x%x\n", c);
-		return c;
+	switch(uartpa){
+	case 0x40001000:
+		return spike_getchar();
+		break;
+	case 0x42000000:
+		return lowrisc_getchar(0);
+		break;
+	default:
+		break;
 	}
 	return -1;
 }
