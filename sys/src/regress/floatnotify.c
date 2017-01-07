32a4372e5551e61d0d356c6056fb58c7347f4dd8
Keith Poole <keith.poole@gmail.com>
2017-01-07 16:56:58 +0000

    Added comment to floatnotify.c
    Signed-off-by: Keith Poole <keith.poole@gmail.com>

--- sys/src/regress/floatnotify.c
+++ sys/src/regress/floatnotify.c
@@ -1,6 +1,28 @@
+/*
+ * This file is part of Harvey.
+ *
+ * Harvey is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * Harvey is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Harvey.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <u.h>
 #include <libc.h>
-#define RET 0xc3
+
+/* 
+ * This is a test which uses flaoting point in a notify handler
+ * currently this will fail with:
+ * "floatnotify 89: suicide: sys: floating point in note handler"
+ * if you remove this check from fpu.c, the kernel will panic
+ * This is a test for issue #152
+ */
 
 void
 handler(void *v, char *s)
@@ -19,9 +41,7 @@ main(void)
 		fprint(2, "%r\n");
 		exits("notify fails");
 	}
-	double y = 47.1;
 	f();
-	print("50=%f\n", y + 2.9);
 	print("FAIL");
 	exits("FAIL");
 }
