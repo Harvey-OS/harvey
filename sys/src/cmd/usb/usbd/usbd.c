0a2f271ef07260094ccfba5e0fc85f5544217750
Graham MacDonald <grahamamacdonald@gmail.com>
2017-03-04 18:10:55 +0000

    Some newlines to make kernel output a little nicer
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- sys/src/cmd/usb/usbd/usbd.c
+++ sys/src/cmd/usb/usbd/usbd.c
@@ -210,7 +210,7 @@ Fail:
 		devctl(d, "detach");
 	free(h->port);
 	free(h);
-	dprint(2, "%s: hub %#p failed to start:", argv0, h);
+	dprint(2, "%s: hub %#p failed to start\n", argv0, h);
 	return nil;
 }
 
@@ -428,7 +428,7 @@ portattach(Hub *h, int p, int sts)
 		if(usbcmd(nd, Rh2d|Rstd|Rdev, Rsetconf, 1, 0, nil, 0) < 0)
 			goto Fail;
 	}
-	dprint(2, "%s: %U", argv0, nd);
+	dprint(2, "%s: %U\n", argv0, nd);
 	pp->state = Pconfiged;
 	dprint(2, "%s: %s: port %d: configed: %s\n",
 			argv0, d->dir, p, nd->dir);
