986d431af821a9114b3bc0338eacf10008865db9
Dan Cross <cross@gajendra.net>
2017-04-18 20:12:48 +0000

    Cleaned up warnings in libfreetype
    Mostly unused variables, or set-but-unused.
    
    The unfortunate practice seems to be to define a variable,
    initialize it to zero, and set it to one later.  Then, presumably,
    someone could look at it with a debugger or something and see
    that it had been set at some point (or not).  Perhaps this is
    used as a rough signal to see that something has happened in a
    function.
    
    Unfortunately, an optimizing compiler will probably note that
    the variable is set but unused and just remove it.  So we just
    remove some of them ourselves.  Or whatever.
    
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- sys/src/libfreetype/src/base/ftstroker.c
+++ sys/src/libfreetype/src/base/ftstroker.c
@@ -1398,14 +1398,11 @@
     FT_Int   n;         /* index of contour in outline     */
     FT_UInt  first;     /* index of first point in contour */
     FT_Int   tag;       /* current point's state           */
-    FT_Int   in_path;
 
     if ( !outline || !stroker )
       return FT_Err_Invalid_Argument;
 
     first = 0;
-
-    in_path = 0;
 
     for ( n = 0; n < outline->n_contours; n++ )
     {
@@ -1455,8 +1452,6 @@
       error = FT_Stroker_BeginSubPath( stroker, &v_start, opened );
       if ( error )
         goto Exit;
-
-      in_path = 1;
 
       while ( point < limit )
       {
