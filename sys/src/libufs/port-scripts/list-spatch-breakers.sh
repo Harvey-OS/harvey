19c190235564fa04ace6c3bdf78f12c8a61dc618
Graham MacDonald <grahamamacdonald@gmail.com>
2017-05-16 23:17:22 +0100

    Add some FreeBSD macros to allow spatch to better parse the UFS code
    list-spatch-breakers.sh now reports only the following files as problematic:
    ffs/ffs_softdep.c
    ffs/softdep.h
    
    At least some of these problems are caused by #defines inside struct definitions.
    
    Perhaps they won't really cause problems.  If they do, then we could move the #defines.
    
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- sys/src/libufs/port-scripts/list-spatch-breakers.sh
+++ sys/src/libufs/port-scripts/list-spatch-breakers.sh
@@ -3,7 +3,7 @@
 # Prints out a list of all UFS files that cannot be fully parsed by spatch
 
 for file in ../{ufs,ffs}/*.[ch]; do
-	if [[ $(spatch --parse-c $file 2>&1 | grep ^bad:) ]]; then
+	if [[ $(spatch --parse-c $file --macro-file macros.h 2>&1 | grep ^bad:) ]]; then
 		echo $file
 	fi
 done
