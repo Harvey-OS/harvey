6334e43e62534f52c012eb037afa38e3424d1052
Ronald G. Minnich <rminnich@gmail.com>
2017-05-17 13:09:32 -0700

    ufs: create example script and apply it
    This change was created via:
    
    spatch --in-place --dir . --sp-file port-scripts/scalar.cocci
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/libufs/ufs/ufs_dirhash.c
+++ sys/src/libufs/ufs/ufs_dirhash.c
@@ -1026,7 +1026,7 @@ ufsdirhash_checkblock(struct inode *ip, char *buf, doff_t offset)
 static int
 ufsdirhash_hash(struct dirhash *dh, char *name, int namelen)
 {
-	u_int32_t hash;
+	uint32_t hash;
 
 	/*
 	 * We hash the name and then some other bit of data that is
