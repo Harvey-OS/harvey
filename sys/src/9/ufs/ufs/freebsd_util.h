aa6a6a645d5479ae29b5848cc4bdcf0cd6160178
Graham MacDonald <grahamamacdonald@gmail.com>
2017-06-23 23:07:22 +0100

    Remove commented out code we don't yet need
    (Also fixes gcc build error about a multi line comment)
    
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- sys/src/9/ufs/ufs/freebsd_util.h
+++ sys/src/9/ufs/ufs/freebsd_util.h
@@ -79,21 +79,12 @@ typedef int64_t intmax_t;	/* FIXME: This should probably be moved to <u.h> or re
  *      and MNT_BYFSID and MNT_NONBUSY are only used for unmount(2),
  *      it's harmless.
  */
-//#define	MNT_UPDATE	0x0000000000010000ULL /* not real mount, just update */
-//#define	MNT_DELEXPORT	0x0000000000020000ULL /* delete export host lists */
-//#define	MNT_RELOAD	0x0000000000040000ULL /* reload filesystem data */
 #define	MNT_FORCE	0x0000000000080000ULL /* force unmount or readonly */
-//#define	MNT_SNAPSHOT	0x0000000001000000ULL /* snapshot the filesystem */
-//#define	MNT_NONBUSY	0x0000000004000000ULL /* check vnode use counts. */
-//#define	MNT_BYFSID	0x0000000008000000ULL /* specify filesystem by ID. */
-//#define MNT_CMDFLAGS   (MNT_UPDATE	| MNT_DELEXPORT	| MNT_RELOAD	| \
-//			MNT_FORCE	| MNT_SNAPSHOT	| MNT_NONBUSY	| \
-//			MNT_BYFSID)
 
 
 /*
  * Error codes used by UFS.
- * TODO HARVEY Translate to error striongs.
+ * TODO HARVEY Translate to error strings.
  */
 #define	EPERM		1		/* Operation not permitted */
 #define	EINVAL		22		/* Invalid argument */
