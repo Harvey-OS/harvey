4f7428306b74e28ec0e7e05453c4383a2dd2a516
Graham MacDonald <grahamamacdonald@gmail.com>
2017-11-04 16:20:32 +0000

    A bit of UFS spring cleaning - move all UFS source into one folder
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- sys/src/9/ufs/ffs_vnops.c
+++ sys/src/9/ufs/ffs_vnops.c
@@ -62,10 +62,10 @@
  */
 
 #include "u.h"
-#include "../../port/lib.h"
+#include "../port/lib.h"
 #include "mem.h"
 #include "dat.h"
-#include "../../port/portfns.h"
+#include "../port/portfns.h"
 
 #include <ufs/ufsdat.h>
 #include <ufs/freebsd_util.h>
