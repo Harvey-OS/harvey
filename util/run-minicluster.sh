1903ecc01ae594ed02c3345c605dc185662b7a0c
Sevki <s@sevki.org>
2018-01-03 11:29:23 +0100

    util: add Harvey OS copyrights
    Signed-off-by: Sevki <s@sevki.org>

--- util/run-minicluster.sh
+++ util/run-minicluster.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
+#!/bin/sh -eu
+#
+# Copyright (C) 2018 Harvey OS
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 if [ $# -lt 1 ]; then
 	echo usage: $0 ethername
@@ -20,10 +36,9 @@ pids=$!
 #	port install x86_64-elf-binutils
 #
 
-sudo ifconfig $ethif 10.0.0.10/24 
+sudo ifconfig $ethif 10.0.0.10/24
 sudo tftpd -vvvv -L --address 10.0.0.10 -s $HARVEY/cfg/pxe/tftpboot &
 pids="$pids $!"
 sudo dhcpd -d -cf $HARVEY/util/minicluster-dhcpd.conf -lf $HARVEY/util/minicluster-dhcpd.leases $ethif
 sudo kill $pids
 wait
-
--- util/src/harvey/cmd/build/build.go
+++ util/src/harvey/cmd/build/build.go
@@ -1,3 +1,19 @@
+// Copyright (C) 2018 Harvey OS
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 // Build builds code as directed by json files.
 // We slurp in the JSON, and recursively process includes.
 // At the end, we issue a single cc command for all the files.
@@ -182,7 +198,7 @@ func fromRoot(p string) string {
 }
 
 func include(f string, targ string, b *build) {
-       debug("include(%s, %s, %v)", f, targ, b)
+	debug("include(%s, %s, %v)", f, targ, b)
 
 	if b.jsons[f] {
 		return
