f82025a12f9c073f20b503aad32e5e685368432f
Álvaro Jurado <elbingmiss@gmail.com>
2018-01-17 13:57:28 +0100

    Importing read and write from APEX (#718)
    Bringing up wirte and read from ap/lib/_write.c and ap/lib/_read.c
      in APEX.
    
    Signed-off-by: Álvaro Jurado <elbingmiss@gmail.com>

--- amd64/include/u.h
+++ amd64/include/u.h
@@ -15,6 +15,7 @@ typedef	signed short	int16_t;
 typedef unsigned int	uint32_t;
 typedef unsigned int	uint;
 typedef signed int	int32_t;
+typedef long		ssize_t;
 typedef	unsigned long long uint64_t;
 typedef	long long	int64_t;
 typedef uint64_t uintptr;
