56a3ddffd96845609790d232cf5e96ffc6eca358
Álvaro Jurado <elbingmiss@gmail.com>
2018-02-05 12:34:10 +0100

    Fixing a few things for read/write syscalls and an uneeded abort declaration
    Signed-off-by: Álvaro Jurado <elbingmiss@gmail.com>

--- sys/src/9/port/sysfile.c
+++ sys/src/9/port/sysfile.c
@@ -717,11 +717,10 @@ mountfix(Chan *c, uint8_t *op, int32_t n, int32_t maxn)
 }
 
 static int32_t
-read(int fd, void *p, int32_t n, int64_t off)
+read(int ispread, int fd, void *p, int32_t n, int64_t off)
 {
 	Proc *up = externup();
 	int32_t nn, nnn;
-	int sequential;
 	Chan *c;
 
 	p = validaddr(p, n, 1);
@@ -742,21 +741,24 @@ read(int fd, void *p, int32_t n, int64_t off)
 	 * The number of bytes read on this fd (c->offset) may be different
 	 * due to rewritings in mountfix.
 	 */
+	if(ispread){	/* use and maintain channel's offset */
 		if(off == -1LL){	/* use and maintain channel's offset */
 			off = c->offset;
-		sequential = 1;
-	} else {
-		sequential = 0;
+			ispread = 0;
+		}
 	}
+	else
+			off = c->offset;
+
 	if(c->qid.type & QTDIR){
 		/*
 		 * Directory read:
 		 * rewind to the beginning of the file if necessary;
 		 * try to fill the buffer via mountrockread;
-		 * set sequential to always maintain the Chan offset.
+		 * clear ispread to always maintain the Chan offset.
 		 */
-		if(off == 0LL){
-			if(sequential){
+		if(off == -1LL){
+			if(!ispread){
 				c->offset = 0;
 				c->devoffset = 0;
 			}
@@ -775,12 +777,12 @@ read(int fd, void *p, int32_t n, int64_t off)
 		}
 		nnn = mountfix(c, p, nn, n);
 
-		sequential = 1;
+		ispread = 0;
 	}
 	else
 		nnn = nn = c->dev->read(c, p, n, off);
 
-	if(sequential){
+	if(!ispread){
 		lock(&c->r.l);
 		c->devoffset += nn;
 		c->offset += nnn;
@@ -810,15 +812,14 @@ syspread(Ar0* ar0, ...)
 	/*
 	 * long pread(int fd, void* buf, long nbytes, int64_t offset);
 	 */
-	ar0->l = read(fd, p, n, off);
+	ar0->l = read(1, fd, p, n, off);
 }
 
 static int32_t
-write(int fd, void *p, int32_t n, int64_t off)
+write(int fd, void *p, int32_t n, int64_t off, int ispwrite)
 {
 	Proc *up = externup();
 	int32_t r;
-	int sequential;
 	Chan *c;
 
 	r = n;
@@ -827,13 +828,8 @@ write(int fd, void *p, int32_t n, int64_t off)
 	n = 0;
 	c = fdtochan(fd, OWRITE, 1, 1);
 
-	if(off == -1LL)
-		sequential = 1;
-	else
-		sequential = 0;
-
 	if(waserror()) {
-		if(sequential){
+		if(!ispwrite){
 			lock(&c->r.l);
 			c->offset -= n;
 			unlock(&c->r.l);
@@ -847,7 +843,7 @@ write(int fd, void *p, int32_t n, int64_t off)
 
 	n = r;
 
-	if(sequential){	/* use and maintain channel's offset */
+	if(off == -1LL){	/* use and maintain channel's offset */
 		lock(&c->r.l);
 		off = c->offset;
 		c->offset += n;
@@ -856,7 +852,7 @@ write(int fd, void *p, int32_t n, int64_t off)
 
 	r = c->dev->write(c, p, n, off);
 
-	if(sequential && r < n){
+	if(!ispwrite && r < n){
 		lock(&c->r.l);
 		c->offset -= n - r;
 		unlock(&c->r.l);
@@ -881,7 +877,7 @@ syspwrite(Ar0* ar0, ...)
 	/*
 	 * long pwrite(int fd, void *buf, long nbytes, int64_t offset);
 	 */
-	ar0->l = write(fd, buf, nbytes, offset);
+	ar0->l = write(fd, buf, nbytes, offset, 1);
 }
 
 static int64_t
