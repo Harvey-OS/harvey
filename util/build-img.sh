89d95f1a28e5f0918f1b06347db48105aba730a8
Graham MacDonald <grahamamacdonald@gmail.com>
2018-06-05 23:07:39 +0100

    Create 2 paritions, one for kernel, rest for fossil (though not set)
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- util/build-img.sh
+++ util/build-img.sh
@@ -6,16 +6,28 @@ SRC=$HARVEY/util/syslinux-bios
 DEST=$HARVEY/harvey.qcow2
 
 echo "Creating harvey image $DEST"
+rm -f $DEST
 
-virt-make-fs --partition=mbr --type=ext4 --format=qcow2 --size=1G $SRC $DEST
-guestfish -a "$DEST" <<EOF
+guestfish <<EOF
+disk-create $DEST qcow2 1G
+add-drive $DEST
+
 launch
-mount /dev/sda1 /
+
+part-init /dev/sda mbr
+part-add /dev/sda p 2048 104447
+part-add /dev/sda p 104448 1992704
+mke2fs /dev/sda1
 part-set-bootable /dev/sda 1 true
+
+mount /dev/sda1 /
 extlinux /
-copy-file-to-device /mbr.bin /dev/sda size:440
+copy-in $HARVEY/util/syslinux-bios/syslinux /
 copy-in $HARVEY/sys/src/9/amd64/harvey.32bit /
 rename /harvey.32bit /harvey
+
+copy-in $HARVEY/util/syslinux-bios/mbr.bin /
+copy-file-to-device /mbr.bin /dev/sda size:440
 rm /mbr.bin
 EOF
 
