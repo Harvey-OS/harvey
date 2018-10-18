8545e36da33421dcad983fe729fdbea09cc071e7
Graham MacDonald <grahamamacdonald@gmail.com>
2018-10-18 12:46:56 +0100

    Try to run regression tests in travis
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- util/ci.sh
+++ util/ci.sh
@@ -4,3 +4,17 @@
 
 GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go get harvey/cmd/...
 ./util/build build.json
+
+RESULTS=usr/harvey/tmp/runallresults.txt
+rm -f $RESULTS
+
+./util/GO9PCPUREGRESS
+
+cat $RESULTS
+
+if tail -1 ./usr/harvey/tmp/runallresults.txt | grep -vq DONE; then
+	echo Regression tests failed
+	exit 1
+fi
+
+echo Regression tests passed
