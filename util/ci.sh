d79fae10443a11b4c6fa4c0ab3ba8614d3333ca7
Graham MacDonald <grahamamacdonald@gmail.com>
2018-10-21 18:19:49 +0100

    travis - build ufs
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- util/ci.sh
+++ util/ci.sh
@@ -2,7 +2,9 @@
 : ${CC?"Need to set CC as clang, gcc or a specific version like clang-3.6"}
 : ${ARCH?"Need to set ARCH as aarch64, amd64 or riscv"}
 
+GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go get -u github.com/Harvey-OS/ninep/cmd/ufs
 GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go get harvey/cmd/...
+
 ./util/build build.json
 
 RESULTS=usr/harvey/tmp/runallresults.txt
