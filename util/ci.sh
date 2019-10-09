267e2605ce64d55a01a8fb475745e7ad628ae6c9
Fazlul Shahriar <fshahriar@gmail.com>
2019-10-08 21:54:18 -0400

    Fix CI build
    Signed-off-by: Fazlul Shahriar <fshahriar@gmail.com>

--- util/ci.sh
+++ util/ci.sh
@@ -2,5 +2,5 @@
 : ${CC?"Need to set CC as clang, gcc or a specific version like clang-3.6"}
 : ${ARCH?"Need to set ARCH as aarch64, amd64 or riscv"}
 
-GOBIN="$(pwd)/util" GOPATH="$(pwd)/util/third_party:$(pwd)/util" go get harvey/cmd/...
+GO111MODULE=on GOBIN="$(pwd)/util" go get ./util/src/harvey/cmd/...
 ./util/build build.json
