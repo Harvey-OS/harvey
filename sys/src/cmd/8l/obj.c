deb5a6ad89f1e36624b221b753fab9192b944337
cinap_lenrek <cinap_lenrek@felloff.net>
2020-05-12 22:04:30 +0200

    ?l: remove direct hunk manipulation from linkers, just call malloc()
    as with recent changes, cc's malloc() could make the hunk pointer
    misaligned. in the the compilers, the hunk pointer is used directly
    by the lexer with no effort to to keep the hunk pointer aligned.
    
    alloc/malloc still return aligned pointers, but hunk itself can
    be on a odd address after allocation of a odd sized amount of bytes.
    
    however, in the linkers, this assumption appears to be differnet. as
    most allocations mostly allocate padded structures. however, symbol
    lookup allocates strings on byte-size ganularity and the cc's malloc
    would misalign the hunk pointer after the malloc() call. while the
    rest of the code assumed hunk pointer was always aligned.
    
    this change removes all the hunk pointer fiddling from the linker,
    and we just call malloc() (which will use the fast implmenentation
    of cc, and should not really make much of a performance difference).
    
    (Cherry pick from plan9front ecdf3f921e2a2ec768807bb09e0e7da995612565)
    
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- sys/src/cmd/8l/obj.c
+++ sys/src/cmd/8l/obj.c
@@ -606,12 +606,7 @@ zaddr(uchar *p, Adr *a, Sym *h[])
 		}
 	}
 
-	while(nhunk < sizeof(Auto))
-		gethunk();
-	u = (Auto*)hunk;
-	nhunk -= sizeof(Auto);
-	hunk += sizeof(Auto);
-
+	u = malloc(sizeof(Auto));
 	u->link = curauto;
 	curauto = u;
 	u->asym = s;
@@ -912,12 +907,7 @@ loop:
 		goto loop;
 	}
 
-	while(nhunk < sizeof(Prog))
-		gethunk();
-	p = (Prog*)hunk;
-	nhunk -= sizeof(Prog);
-	hunk += sizeof(Prog);
-
+	p = malloc(sizeof(Prog));
 	p->as = o;
 	p->line = bloc[2] | (bloc[3] << 8) | (bloc[4] << 16) | (bloc[5] << 24);
 	p->back = 2;
@@ -1174,12 +1164,7 @@ lookup(char *symb, int v)
 		if(memcmp(s->name, symb, l) == 0)
 			return s;
 
-	while(nhunk < sizeof(Sym))
-		gethunk();
-	s = (Sym*)hunk;
-	nhunk -= sizeof(Sym);
-	hunk += sizeof(Sym);
-
+	s = malloc(sizeof(Sym));
 	s->name = malloc(l + 1);
 	memmove(s->name, symb, l);
 
@@ -1196,14 +1181,7 @@ lookup(char *symb, int v)
 Prog*
 prg(void)
 {
-	Prog *p;
-
-	while(nhunk < sizeof(Prog))
-		gethunk();
-	p = (Prog*)hunk;
-	nhunk -= sizeof(Prog);
-	hunk += sizeof(Prog);
-
+	Prog *p = malloc(sizeof(Prog));
 	*p = zprg;
 	return p;
 }
--- sys/src/cmd/kl/obj.c
+++ sys/src/cmd/kl/obj.c
@@ -757,12 +757,7 @@ loop:
 		goto loop;
 	}
 
-	if(nhunk < sizeof(Prog))
-		gethunk();
-	p = (Prog*)hunk;
-	nhunk -= sizeof(Prog);
-	hunk += sizeof(Prog);
-
+	p = malloc(sizeof(Prog));
 	p->as = o;
 	p->reg = bloc[1] & 0x7f;
 	if(bloc[1] & 0x80)
@@ -1011,12 +1006,7 @@ lookup(char *symb, int v)
 		if(memcmp(s->name, symb, l) == 0)
 			return s;
 
-	while(nhunk < sizeof(Sym))
-		gethunk();
-	s = (Sym*)hunk;
-	nhunk -= sizeof(Sym);
-	hunk += sizeof(Sym);
-
+	s = malloc(sizeof(Sym));
 	s->name = malloc(l + 1);
 	memmove(s->name, symb, l);
 
@@ -1031,17 +1021,7 @@ lookup(char *symb, int v)
 Prog*
 prg(void)
 {
-	Prog *p;
-	int n;
-
-	n = (sizeof(Prog) + 3) & ~3;
-	while(nhunk < n)
-		gethunk();
-
-	p = (Prog*)hunk;
-	nhunk -= n;
-	hunk += n;
-
+	Prog *p = malloc(sizeof(Prog));
 	*p = zprg;
 	return p;
 }
