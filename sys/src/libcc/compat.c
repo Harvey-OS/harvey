3f99b6e72ec0eb7e5c734b5986d9bcb373871efa
Álvaro Jurado <elbingmiss@gmail.com>
2020-07-03 01:41:04 +0200

    Bringing back Ken cc: first step, libcc as a common lib for compilers and utilities
    Signed-off-by: Álvaro Jurado <elbingmiss@gmail.com>

---
+++ sys/src/libcc/compat.c
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the UCB release of Plan 9. It is subject to the license
+ * terms in the LICENSE file found in the top-level directory of this
+ * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
+ * part of the UCB release of Plan 9, including this file, may be copied,
+ * modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file.
+ */
+
+#include	"cc.h"
+#include	"compat"
+
+/*
+ * fake mallocs
+ */
+void*
+malloc(ulong n)
+{
+	return alloc(n);
+}
+
+void*
+calloc(uint32_t m, size_t n)
+{
+	return alloc(m*n);
+}
+
+void*
+realloc(void* v, ulong u)
+{
+	fprint(2, "realloc called\n");
+	abort();
+	return 0;
+}
+
+void
+free(void* v)
+{
+}
+
+/* needed when profiling */
+void*
+mallocz(uint32_t size, int clr)
+{
+	void *v;
+
+	v = alloc(size);
+	if(clr && v != nil)
+		memset(v, 0, size);
+	return v;
+}
+
+void
+setmalloctag(void* v, uintptr_t u)
+{
+}
