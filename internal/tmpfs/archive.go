305ce824af6c8a733ad7a929307fe2abe1a6ccf6
Graham MacDonald <grahamamacdonald@gmail.com>
2020-07-19 10:16:57 +0100

    Load package from file
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- internal/tmpfs/archive.go
+++ internal/tmpfs/archive.go
@@ -2,7 +2,6 @@ package tmpfs
 
 import (
 	"archive/tar"
-	"bytes"
 	"compress/gzip"
 	"fmt"
 	"io"
@@ -183,54 +182,9 @@ func (a *Archive) createFile(d *Directory, filename string, file *File) error {
 	return nil
 }
 
-// Create and add some files to the archive.
-func CreateTestImageTemp() *bytes.Buffer {
-	var buf bytes.Buffer
-
-	gztw := gzip.NewWriter(&buf)
-	defer func() {
-		if err := gztw.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	tw := tar.NewWriter(gztw)
-	defer func() {
-		if err := tw.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	var files = []struct {
-		Name, Body string
-	}{
-		{"readme.txt", "This archive contains some text files."},
-		{"foo/gopher.txt", "Gopher names:\nGeorge\nGeoffrey\nGonzo"},
-		{"bar/todo.txt", "Get animal handling license."},
-		{"foo/todo2.txt", "harvey lalal"},
-		{"abc/123/sean.txt", "lorem ipshum."},
-		{"foo/bar/hello.txt", "hello world!"},
-	}
-	for _, file := range files {
-		hdr := &tar.Header{
-			Name: file.Name,
-			Mode: 0600,
-			Size: int64(len(file.Body)),
-		}
-		if err := tw.WriteHeader(hdr); err != nil {
-			log.Fatal(err)
-		}
-		if _, err := tw.Write([]byte(file.Body)); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	return &buf
-}
-
-// Read a compressed tar and produce a file hierarchy
-func ReadImage(buf *bytes.Buffer) *Archive {
-	gzr, err := gzip.NewReader(buf)
+// ReadImage reads a compressed tar to produce a file hierarchy
+func ReadImage(r io.Reader) *Archive {
+	gzr, err := gzip.NewReader(r)
 	if err != nil {
 		log.Fatal(err)
 	}
