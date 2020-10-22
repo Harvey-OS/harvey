382f207994d1c86224df12c4a88dff8209f77c7c
John Floren <john@jfloren.net>
2020-10-21 21:06:20 -0600

    add license and copyright notice for logf.c
    Signed-off-by: John Floren <john@jfloren.net>

--- sys/src/cmd/acme/logf.c
+++ sys/src/cmd/acme/logf.c
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the Harvey distribution (https://harvey-os.org).
+ * Copyright (c) 2014 Russ Cox
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file originates from plan9port and is re-licensed GPLv2 for Harvey
+ * with permission from Russ Cox
+ */
 #include <u.h>
 #include <libc.h>
 #include <draw.h>
