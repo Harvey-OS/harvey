c4a0d7bc1141ccde5da0e09b1b2f9f0a55b7a006
Graham MacDonald <grahamamacdonald@gmail.com>
2021-05-11 20:08:34 +0100

    devshr: add postsharesrv and threadpostsharesrv functions These are used by the 9front usb stack
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- sys/include/9p.h
+++ sys/include/9p.h
@@ -219,6 +219,8 @@ struct Srv {
 void		srv(Srv*);
 void		postmountsrv(Srv*, char*, char*, int);
 void		_postmountsrv(Srv*, char*, char*, int);
+void		postsharesrv(Srv*, char*, char*, char*);
+void		_postsharesrv(Srv*, char*, char*, char*);
 void		listensrv(Srv*, char*);
 void		_listensrv(Srv*, char*);
 int 		postfd(char*, int);
@@ -226,6 +228,7 @@ int		chatty9p;
 void		respond(Req*, char*);
 void		responderror(Req*);
 void		threadpostmountsrv(Srv*, char*, char*, int);
+void		threadpostsharesrv(Srv*, char*, char*, char*);
 void		threadlistensrv(Srv *s, char *addr);
 
 /*
