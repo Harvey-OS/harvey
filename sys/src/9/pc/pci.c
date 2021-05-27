f75773421b21a0482f5b25c85548ddbc28328666
David du Colombier <0intro@gmail.com>
2021-05-28 00:38:00 +0200

    sys/src/9/pc: add VT8235 and VT8251 devices to PCI (thanks Jerome Ibanes)

--- sys/src/9/pc/pci.c
+++ sys/src/9/pc/pci.c
@@ -673,7 +673,9 @@ static Bridge southbridges[] = {
 	{ 0x1106, 0x0586, viaget, viaset },	/* Viatech 82C586 */
 	{ 0x1106, 0x0596, viaget, viaset },	/* Viatech 82C596 */
 	{ 0x1106, 0x0686, viaget, viaset },	/* Viatech 82C686 */
+	{ 0x1106, 0x3177, viaget, viaset },	/* Viatech VT8235 */
 	{ 0x1106, 0x3227, viaget, viaset },	/* Viatech VT8237 */
+	{ 0x1106, 0x3287, viaget, viaset },	/* Viatech VT8251 */
 	{ 0x1045, 0xc700, optiget, optiset },	/* Opti 82C700 */
 	{ 0x10b9, 0x1533, aliget, aliset },	/* Al M1533 */
 	{ 0x1039, 0x0008, pIIxget, pIIxset },	/* SI 503 */
