b2a1b522c31a201eb4b9609475ba89626df87e05
David du Colombier <0intro@gmail.com>
2021-05-28 02:06:00 +0200

    sys/src/cmd/cpu.c: ensure secrets are at least 8 bytes (thanks Charles Forsyth)

--- sys/src/cmd/cpu.c
+++ sys/src/cmd/cpu.c
@@ -592,7 +592,11 @@ p9auth(int fd)
 	ai = auth_proxy(fd, auth_getkey, "proto=%q role=client %s", p9authproto, keyspec);
 	if(ai == nil)
 		return -1;
-	memmove(key+4, ai->secret, ai->nsecret);
+	if(ai->nsecret < 8){
+		werrstr("negotiated secret too short");
+		return -1;
+	}
+	memmove(key+4, ai->secret, 8);
 	if(ealgs == nil)
 		return fd;
 
@@ -663,7 +667,11 @@ srvp9auth(int fd, char *user)
 	if(auth_chuid(ai, nil) < 0)
 		return -1;
 	strecpy(user, user+MaxStr, ai->cuid);
-	memmove(key+4, ai->secret, ai->nsecret);
+	if(ai->nsecret < 8){
+		werrstr("negotiated secret too short");
+		return -1;
+	}
+	memmove(key+4, ai->secret, 8);
 
 	if(ealgs == nil)
 		return fd;
