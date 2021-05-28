fc753f5bfd14288aaef1de8f8334b8f25672dd81
David du Colombier <0intro@gmail.com>
2021-05-28 03:15:00 +0200

    sys/src/cmd/fossil: move from liboventi to libthread and libventi

--- sys/src/cmd/fossil/fs.h
+++ sys/src/cmd/fossil/fs.h
@@ -6,6 +6,11 @@ typedef struct DirEntryEnum DirEntryEnum;
 #pragma incomplete File
 #pragma incomplete DirEntryEnum
 
+enum
+{
+	STACK = 32*1024,
+};
+
 /* modes */
 
 enum {
@@ -21,7 +26,7 @@ void	fsClose(Fs*);
 int	fsEpochLow(Fs*, u32int);
 File	*fsGetRoot(Fs*);
 int	fsHalt(Fs*);
-Fs	*fsOpen(char*, VtSession*, long, int);
+Fs	*fsOpen(char*, VtConn*, long, int);
 int	fsRedial(Fs*, char*);
 void	fsSnapshotCleanup(Fs*, u32int);
 int	fsSnapshot(Fs*, char*, char*, int);
