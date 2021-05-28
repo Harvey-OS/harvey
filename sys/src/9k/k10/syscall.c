4e550730898a49f8c9de6729793e340a7c6b5bc1
David du Colombier <0intro@gmail.com>
2021-05-28 04:51:00 +0200

    sys/src/9k/k10: fix typo in syscall

--- sys/src/9k/k10/syscall.c
+++ sys/src/9k/k10/syscall.c
@@ -273,7 +273,7 @@ syscall(int scallnr, Ureg* ureg)
 	/*
 	 * Put return value in frame.
 	 * Which element of Ar0 to use is based on specific
-	 * knowldge of the architecture.
+	 * knowledge of the architecture.
 	 */
 	ureg->ax = ar0.p;
 
