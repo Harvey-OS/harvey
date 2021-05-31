005c3bdab10b20da671609e7400c51344bde6936
Graham MacDonald <grahamamacdonald@gmail.com>
2021-05-31 11:03:19 +0100

    Revert "Add 9front nusb"
    This reverts commit 7e1118bd07081920e269d61672629bcd5c7f1555.
    
    Signed-off-by: Graham MacDonald <grahamamacdonald@gmail.com>

--- sys/src/cmd/frontusb/cam/fns.h
+++
@@ -1,8 +0,0 @@
-char *ctlread(Cam *);
-void printDescriptor(Fmt *, Iface *, void *);
-int videoopen(Cam *, int);
-void videoclose(Cam *);
-void videoread(Req *, Cam *, int);
-void videoflush(Req *, Cam *);
-int getframedesc(Cam *, int, int, Format **, VSUncompressedFrame **);
-int ctlwrite(Cam *, char *);
