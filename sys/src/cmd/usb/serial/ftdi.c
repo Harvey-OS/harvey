762c69a61560be6860fdbbefa6b1faca4f05a279
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:42:25 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2011-01-08"
    This reverts commit 6f756d421e301d3b6a3d9e3be3872a27b7fbdeea.

--- sys/src/cmd/usb/serial/ftdi.c
+++ sys/src/cmd/usb/serial/ftdi.c
@@ -452,15 +452,6 @@ ftsetparam(Serialport *p)
 	return res;
 }
 
-static int
-hasjtag(Usbdev *udev)
-{
-	/* no string, for now, by default we detect no jtag */
-	if(udev->product != nil && cistrstr(udev->product, "jtag") != nil)
-		return 1;
-	return 0;
-}
-
 /* ser locked */
 static void
 ftgettype(Serial *ser)
@@ -496,8 +487,7 @@ ftgettype(Serial *ser)
 		} else
 			ser->type = FT2232C;
 
-		if(hasjtag(ser->dev->usb))
-			ser->jtag = 0;
+		ser->jtag = 0;
 
 		/*
 		 * BM-type devices have a bug where dno gets set
