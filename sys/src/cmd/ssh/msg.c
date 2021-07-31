799ca6276989aa34da4b581b2a15ba489c87c873
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 15:00:55 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2004-08-15"
    This reverts commit 565bacbc3cb08fda69762ae44686c81e8464c5a3.

--- sys/src/cmd/ssh/msg.c
+++ sys/src/cmd/ssh/msg.c
@@ -102,7 +102,6 @@ allocmsg(Conn *c, int type, int len)
 void
 unrecvmsg(Conn *c, Msg *m)
 {
-	debug(DBG_PROTO, "unreceived %s len %d\n", msgnames[m->type], m->ep - m->rp);
 	free(c->unget);
 	c->unget = m;
 }
