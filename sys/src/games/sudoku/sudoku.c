f2a92a72d6827f8465fb85c9dc444cb771f9efe4
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:58:39 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2005-06-02"
    This reverts commit d8f3f65ed104b5f1fe09dbf8d8d4b5289e0e4acf.

--- sys/src/games/sudoku/sudoku.c
+++ sys/src/games/sudoku/sudoku.c
@@ -286,9 +286,9 @@ eresized(int new)
 	}
 
 	p = Pt(Dx(screen->r), Dy(screen->r));
-	if(!new || !eqpt(p, Pt(Maxx - 8, Maxy - 8)))
+	if(!new || !eqpt(p, Pt(Maxx, Maxy))) {
 		resize();
-
+	}
 	drawscreen();
 }
 
