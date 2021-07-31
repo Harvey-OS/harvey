72a00e56fef34357952e8419c5e9da475d1dde4b
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:54:01 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2006-11-09"
    This reverts commit ee17ab8015e4a168cf53c39988aec397b589220f.

--- sys/src/9/port/devsrv.c
+++ sys/src/9/port/devsrv.c
@@ -139,7 +139,6 @@ srvopen(Chan *c, int omode)
 static void
 srvcreate(Chan *c, char *name, int omode, ulong perm)
 {
-	char *sname;
 	Srv *sp;
 
 	if(openmode(omode) != OWRITE)
@@ -148,13 +147,9 @@ srvcreate(Chan *c, char *name, int omode, ulong perm)
 	if(omode & OCEXEC)	/* can't happen */
 		panic("someone broke namec");
 
-	sp = smalloc(sizeof *sp);
-	sname = smalloc(strlen(name)+1);
-	if(sp == nil || sname == nil) {
-		free(sp);
-		free(sname);
+	sp = malloc(sizeof(Srv)+strlen(name)+1);
+	if(sp == 0)
 		error(Enomem);
-	}
 
 	qlock(&srvlk);
 	if(waserror()){
@@ -167,8 +162,8 @@ srvcreate(Chan *c, char *name, int omode, ulong perm)
 
 	sp->path = qidpath++;
 	sp->link = srv;
-	strcpy(sname, name);
-	sp->name = sname;
+	sp->name = (char*)(sp+1);
+	strcpy(sp->name, name);
 	c->qid.type = QTFILE;
 	c->qid.path = sp->path;
 	srv = sp;
@@ -226,25 +221,21 @@ srvremove(Chan *c)
 
 	if(sp->chan)
 		cclose(sp->chan);
-	free(sp->name);
 	free(sp);
 }
 
 static int
 srvwstat(Chan *c, uchar *dp, int n)
 {
-	char *strs;
 	Dir d;
 	Srv *sp;
 
 	if(c->qid.type & QTDIR)
 		error(Eperm);
 
-	strs = nil;
 	qlock(&srvlk);
 	if(waserror()){
 		qunlock(&srvlk);
-		free(strs);
 		nexterror();
 	}
 
@@ -252,24 +243,18 @@ srvwstat(Chan *c, uchar *dp, int n)
 	if(sp == 0)
 		error(Enonexist);
 
-	if(strcmp(sp->owner, up->user) != 0 && !iseve())
+	if(strcmp(sp->owner, up->user) && !iseve())
 		error(Eperm);
 
-	strs = smalloc(n);
-	n = convM2D(dp, n, &d, strs);
+	n = convM2D(dp, n, &d, nil);
 	if(n == 0)
-		error(Eshortstat);
+		error (Eshortstat);
 	if(d.mode != ~0UL)
 		sp->perm = d.mode & 0777;
 	if(d.uid && *d.uid)
 		kstrdup(&sp->owner, d.uid);
-	if(d.name && *d.name && strcmp(sp->name, d.name) != 0) {
-		if(strchr(d.name, '/') != nil)
-			error(Ebadchar);
-		kstrdup(&sp->name, d.name);
-	}
+
 	qunlock(&srvlk);
-	free(strs);
 	poperror();
 	return n;
 }
