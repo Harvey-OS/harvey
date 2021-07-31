bd9ed678485df317a2a23e9d576fc5bab2dfb334
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:36:58 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2014-01-25"
    This reverts commit 7874c72cdf15e7bba44575d64a95358fcedce352.

--- sys/src/9/pc/vgavesa.c
+++ sys/src/9/pc/vgavesa.c
@@ -62,6 +62,7 @@ vbecall(Ureg *u)
 		nexterror();
 	}
 	pa = PADDR(RMBUF);
+	/* TODO: check read and write return values */
 	devtab[cmem->type]->write(cmem, modebuf, sizeof modebuf, pa);
 	u->trap = 0x10;
 	devtab[creg->type]->write(creg, u, sizeof *u, 0);
