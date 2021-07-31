7b695943f7efe0e99d89fd0641dc317ed6b4d4f9
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:38:38 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2013-01-05"
    This reverts commit b2ebcd4d1aeacb3fc8c3d85492d03278b32bed3f.

--- sys/src/cmd/ndb/dnsdebug.c
+++ sys/src/cmd/ndb/dnsdebug.c
@@ -323,7 +323,6 @@ squirrelserveraddrs(void)
 			l = &(*l)->next;
 			continue;
 		}
-		memset(&req, 0, sizeof req);
 		req.isslave = 1;
 		req.aborttime = NS2MS(nowns) + Maxreqtm;
 		*l = dnresolve(rp->host->name, Cin, Ta, &req, 0, 0, Recurse, 0, 0);
