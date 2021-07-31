64b90daca6b227d9186421544b5a0858186f7958
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:40:24 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2012-01-11"
    This reverts commit 22c8337b26b03559e4868f9300ec13f66150fcd6.

--- sys/src/cmd/ip/tftpd.c
+++ sys/src/cmd/ip/tftpd.c
@@ -507,13 +507,12 @@ sendfile(int fd, char *name, char *mode, int opts)
 	uchar buf[Maxsegsize+Hdrsize];
 	char errbuf[Maxerr];
 
-	file = -1;
 	syslog(dbg, flog, "tftpd %d send file '%s' %s to %s",
 		pid, name, mode, raddr);
 	name = sunkernel(name);
 	if(name == 0){
 		nak(fd, 0, "not in our database");
-		goto error;
+		return;
 	}
 
 	notify(catcher);
@@ -522,7 +521,7 @@ sendfile(int fd, char *name, char *mode, int opts)
 	if(file < 0) {
 		errstr(errbuf, sizeof errbuf);
 		nak(fd, 0, errbuf);
-		goto error;
+		return;
 	}
 	block = 0;
 	rexmit = Ackok;
@@ -548,7 +547,7 @@ sendfile(int fd, char *name, char *mode, int opts)
 			if(n < 0) {
 				errstr(errbuf, sizeof errbuf);
 				nak(fd, 0, errbuf);
-				goto error;
+				return;
 			}
 			txtry = 0;
 		}
@@ -574,8 +573,6 @@ sendfile(int fd, char *name, char *mode, int opts)
 		if(ret != blksize+Hdrsize && rexmit == Ackok)
 			break;
 	}
-	syslog(dbg, flog, "tftpd %d done sending file '%s' %s to %s",
-		pid, name, mode, raddr);
 error:
 	close(fd);
 	close(file);
