899a61a3fc592c06dfb56795f739ebfe7917d2e3
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:50:49 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2007-08-29"
    This reverts commit ff55b41d4e9d2c13b36de2101465a357b4133185.

--- sys/src/libsec/port/hmac.c
+++ sys/src/libsec/port/hmac.c
@@ -2,25 +2,27 @@
 #include <libsec.h>
 
 /* rfc2104 */
-DigestState*
+static DigestState*
 hmac_x(uchar *p, ulong len, uchar *key, ulong klen, uchar *digest, DigestState *s,
 	DigestState*(*x)(uchar*, ulong, uchar*, DigestState*), int xlen)
 {
 	int i;
-	uchar pad[Hmacblksz+1], innerdigest[256];
+	uchar pad[65], innerdigest[256];
 
 	if(xlen > sizeof(innerdigest))
 		return nil;
-	if(klen > Hmacblksz)
+
+	if(klen>64)
 		return nil;
 
 	/* first time through */
 	if(s == nil || s->seeded == 0){
-		memset(pad, 0x36, Hmacblksz);
-		pad[Hmacblksz] = 0;
-		for(i = 0; i < klen; i++)
+		for(i=0; i<64; i++)
+			pad[i] = 0x36;
+		pad[64] = 0;
+		for(i=0; i<klen; i++)
 			pad[i] ^= key[i];
-		s = (*x)(pad, Hmacblksz, nil, s);
+		s = (*x)(pad, 64, nil, s);
 		if(s == nil)
 			return nil;
 	}
@@ -30,12 +32,25 @@ hmac_x(uchar *p, ulong len, uchar *key, ulong klen, uchar *digest, DigestState *
 		return s;
 
 	/* last time through */
-	memset(pad, 0x5c, Hmacblksz);
-	pad[Hmacblksz] = 0;
-	for(i = 0; i < klen; i++)
+	for(i=0; i<64; i++)
+		pad[i] = 0x5c;
+	pad[64] = 0;
+	for(i=0; i<klen; i++)
 		pad[i] ^= key[i];
 	(*x)(nil, 0, innerdigest, s);
-	s = (*x)(pad, Hmacblksz, nil, nil);
+	s = (*x)(pad, 64, nil, nil);
 	(*x)(innerdigest, xlen, digest, s);
 	return nil;
+}
+
+DigestState*
+hmac_sha1(uchar *p, ulong len, uchar *key, ulong klen, uchar *digest, DigestState *s)
+{
+	return hmac_x(p, len, key, klen, digest, s, sha1, SHA1dlen);
+}
+
+DigestState*
+hmac_md5(uchar *p, ulong len, uchar *key, ulong klen, uchar *digest, DigestState *s)
+{
+	return hmac_x(p, len, key, klen, digest, s, md5, MD5dlen);
 }
