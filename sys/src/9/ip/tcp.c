b3da02fe29b9e3083b0705fc46b42b14ba1c3cb8
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:36:47 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2014-03-06"
    This reverts commit 8e860520aa6bfda4bc4f23391be5d54c6baed7e6.

--- sys/src/9/ip/tcp.c
+++ sys/src/9/ip/tcp.c
@@ -128,7 +128,6 @@ struct Tcp4hdr
 	uchar	tcplen[2];
 	uchar	tcpsrc[4];
 	uchar	tcpdst[4];
-	/* same as v6 from here on */
 	uchar	tcpsport[2];
 	uchar	tcpdport[2];
 	uchar	tcpseq[4];
@@ -150,7 +149,6 @@ struct Tcp6hdr
 	uchar	ttl;
 	uchar	tcpsrc[IPaddrlen];
 	uchar	tcpdst[IPaddrlen];
-	/* same as v4 from here on */
 	uchar	tcpsport[2];
 	uchar	tcpdport[2];
 	uchar	tcpseq[4];
