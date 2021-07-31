e2e259b773a5806348d624b753be8de2295d0566
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 15:04:22 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2003-01-08"
    This reverts commit 3a72b2717cc75944f514da3e7dfb22e9db5f0ea0.

--- sys/src/9/port/devfs.c
+++ sys/src/9/port/devfs.c
@@ -20,6 +20,7 @@ enum {
 	Fcat,		// catenation of others
 	Finter,		// interleaving of others
 	Fpart,		// part of others
+	Fconf,		// configuration command, not a device.
 
 	Blksize	= 8*1024,	// for Finter only
 	Maxconf	= 1024,		// max length for config
@@ -64,10 +65,12 @@ static Cmdtab configs[] = {
 	Fcat,	"cat",		0,
 	Finter,	"inter",	0,
 	Fpart,	"part",		5,
+	Fconf,	"config",	3,
 };
 
 static char	confstr[Maxconf];
 static int	configed;
+static int	bootconfig;	// true while configuring from fscfg
 
 
 static Fsdev*
@@ -160,6 +163,21 @@ mpshut(Fsdev *mp)
 	memset(mp, 0, sizeof(*mp));
 }
 
+static void
+wrconf(Fsdev *mp)
+{
+	Chan	*cc;
+
+	cc = mp->idev[0];
+	if (waserror()){
+		print("#k: can't write config\n");
+		nexterror();
+	}
+	devtab[cc->type]->write(cc, confstr, strlen(confstr)+1, 0);
+	mpshut(mp);
+	poperror();
+}
+
 
 static void
 mconfig(char* a, long n)	// "name idev0 idev1"
@@ -173,6 +191,8 @@ mconfig(char* a, long n)	// "name idev0 idev1"
 	char	*c;
 	vlong	size, start;
 
+	size = 0;
+	start = 0;
 	if (confstr[0] == 0)
 		seprint(confstr, confstr+sizeof(confstr), Cfgstr);
 	mp = nil;
@@ -223,7 +243,10 @@ mconfig(char* a, long n)	// "name idev0 idev1"
 			error(Egreg);
 		mp->ndevs++;
 	}
-	setdsize(mp);
+	if (ct->index == Fconf && !bootconfig)
+		wrconf(mp);
+	else
+		setdsize(mp);
 	poperror();
 	configed = 1;
 	qunlock(&lck);
@@ -241,6 +264,7 @@ rdconf(void)
 	char	*e;
 	Chan	*cc;
 
+	bootconfig = 1;
 	s = getconf("fsconfig");
 	if (s == nil){
 		mustrd = 0;
@@ -250,6 +274,7 @@ rdconf(void)
 	cc = nil;
 	c = nil;
 	if (waserror()){
+		bootconfig = 0;
 		configed = 1;
 		if (cc != nil)
 			cclose(cc);
@@ -275,6 +300,7 @@ rdconf(void)
 		mconfig(p, e - p);
 	}
 	poperror();
+	bootconfig = 0;
 	free(s);	
 }
 
@@ -395,7 +421,6 @@ catio(Fsdev *mp, int isread, void *a, long n, vlong off)
 	int	i;
 	Chan*	mc;
 	long	l, wl, res;
-	char*	s;
 	//print("catio %d %p %ld %lld\n", isread, a, n, off);
 	res = n;
 	for (i = 0; n >= 0 && i < mp->ndevs ; i++){
@@ -409,7 +434,6 @@ catio(Fsdev *mp, int isread, void *a, long n, vlong off)
 		else
 			l = n;
 		//print("\tdev %d %p %ld %lld\n", i, a, l, off);
-		s = a;
 
 		if (isread)
 			wl = devtab[mc->type]->read(mc, a, l, off);
