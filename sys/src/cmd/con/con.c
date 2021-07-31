feb732db3fcdc9fe15d2ecafbd44c87926e7aba9
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:48:55 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2008-02-20"
    This reverts commit f845c31286582fd734130329644d49d9e60ca711.

--- sys/src/cmd/con/con.c
+++ sys/src/cmd/con/con.c
@@ -232,10 +232,8 @@ device(char *dest, char *cmd)
 	snprint(cname, sizeof cname, "%sctl", dest);
 	ctl = open(cname, ORDWR);
 	if (baud > 0) {
-		if(ctl >= 0){
-			/* set speed and use fifos if available */
-			fprint(ctl, "b%d i1", baud);
-		}
+		if(ctl >= 0)
+			fprint(ctl, "b%d", baud);
 		else
 			fprint(2, "con: cannot open %s: %r\n", cname);
 	}
