f9d64f787a56a7c5c35cec1dbeb99b6f825c3ae7
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:54:18 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2006-10-03"
    This reverts commit cb82d19a937d71b740387edad1cfcc1ca8a697ce.

--- sys/src/9/port/devuart.c
+++ sys/src/9/port/devuart.c
@@ -681,7 +681,7 @@ uartrecv(Uart *p,  char ch)
 	/* receive the character */
 	if(p->putc)
 		p->putc(p->iq, ch);
-	else if (p->iw) {		/* maybe the line isn't enabled yet */
+	else{
 		ilock(&p->rlock);
 		next = p->iw + 1;
 		if(next == p->ie)
