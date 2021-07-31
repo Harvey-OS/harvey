2f57e5926ec4e3c8e4d54fb8966e1d7cea2d0adf
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:43:32 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2010-05-13"
    This reverts commit 286201976aab0af40a2d6f1438887bee9b2242d3.

--- sys/src/9/kw/fns.h
+++ sys/src/9/kw/fns.h
@@ -28,10 +28,13 @@ extern void cacheiinv(void);
 extern void cacheuwbinv(void);
 extern uintptr cankaddr(uintptr pa);
 extern void clockshutdown(void);
-extern int clz(ulong);
 int	cmpswap(long*, long, long);
 
-#define coherence barriers
+/*
+ * cachedwb seems like overkill, but just calling barriers isn't enough.
+ */
+#define coherence cachedwb
+// #define coherence barriers
 
 extern u32int controlget(void);
 extern u32int cpctget(void);
