6997d9b379f22722b447b0080c5f78d939a5a58b
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:32:55 +0200

    Revert "sys/src/9k/k10: use named constants"
    This reverts commit 01f51cadd1a675469357d755ae374b4c67388dfd.

--- sys/src/9k/k10/trap.c
+++ sys/src/9k/k10/trap.c
@@ -21,13 +21,13 @@ static void unexpected(Ureg*, void*);
 static void dumpstackwithureg(Ureg*);
 
 static Lock vctllock;
-static Vctl *vctl[IdtMAX+1];
+static Vctl *vctl[256];
 
 enum
 {
 	Ntimevec = 20		/* number of time buckets for each intr */
 };
-ulong intrtimes[IdtMAX+1][Ntimevec];
+ulong intrtimes[256][Ntimevec];
 
 void*
 intrenable(int irq, void (*f)(Ureg*, void*), void* a, int tbdf, char *name)
@@ -140,7 +140,7 @@ trapenable(int vno, void (*f)(Ureg*, void*), void* a, char *name)
 {
 	Vctl *v;
 
-	if(vno < 0 || vno > IdtMAX)
+	if(vno < 0 || vno >= 256)
 		panic("trapenable: vno %d", vno);
 	v = malloc(sizeof(Vctl));
 	v->tbdf = BUSUNKNOWN;
