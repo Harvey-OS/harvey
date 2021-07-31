0a4c1ee83a2968a4dcc9e717da3cef80b5031479
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:32:53 +0200

    Revert "sys/src/9k/boot: replace frequent warning by dprint"
    This reverts commit 3d546630306bddc38eaa48351853e04b23734f8c.

--- sys/src/9k/boot/usb.c
+++ sys/src/9k/boot/usb.c
@@ -106,7 +106,7 @@ mountusb(void)
 	dprint("mount usbd...");
 	fd = open("/srv/usb", ORDWR);
 	if(fd < 0)
-		dprint("can't open /srv/usb");
+		warning("can't open /srv/usb");
 	else if(mount(fd, -1, "/dev", MBEFORE, "") < 0) {
 		warning("mount -a /srv/usb /dev");
 		close(fd);
