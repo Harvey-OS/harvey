538b2c7fc37ec6f1aa87a307f67df16f823f629a
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:49:02 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2008-02-07"
    This reverts commit 1dce3b0095c05f7fbb6059120e76a96cc947ac86.

--- sys/src/ape/lib/v/plan9/getpass.c
+++ sys/src/ape/lib/v/plan9/getpass.c
@@ -7,8 +7,8 @@
 char *
 getpass(char *prompt)
 {
-	int c;
-	char *p;
+	register char *p;
+	register c;
 	FILE *fi;
 	static char pbuf[9];
 	void (*sig)(int);
@@ -16,24 +16,16 @@ getpass(char *prompt)
 	if ((fi = fopen("/dev/cons", "r")) == NULL)
 		fi = stdin;
 	else
-		setbuf(fi, NULL);
+		setbuf(fi, (char *)NULL);
 	sig = signal(SIGINT, SIG_IGN);
 	tty_echooff(fileno(fi));
-	fprintf(stderr, "%s", prompt);
-	fflush(stderr);
-
-	for (p = pbuf; (c = getc(fi)) != '\n' && c != EOF; )
-		if (c == ('u' & 037))
-			p = pbuf;
-		else if (c == '\b') {
-			if (p > pbuf)
-				p--;
-		} else if (p < &pbuf[8])
+	fprintf(stderr, "%s", prompt); fflush(stderr);
+	for (p=pbuf; (c = getc(fi))!='\n' && c!=EOF;) {
+		if (p < &pbuf[8])
 			*p++ = c;
+	}
 	*p = '\0';
-
-	fprintf(stderr, "\n");
-	fflush(stderr);
+	fprintf(stderr, "\n"); fflush(stderr);
 	tty_echoon(fileno(fi));
 	signal(SIGINT, sig);
 	if (fi != stdin)
