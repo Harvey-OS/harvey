4ccb8bc4232616db68fc59cc9a6bc666b801dfdb
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:33:02 +0200

    Revert "sys/src/9k/port: call iunlock before freeblist in qpass and qpassnolim"
    This reverts commit 660bf26be12d839761a522a64959b0dfb6b1b8bf.

--- sys/src/9k/port/qio.c
+++ sys/src/9k/port/qio.c
@@ -577,14 +577,14 @@ qpass(Queue *q, Block *b)
 	dowakeup = 0;
 	ilock(q);
 	if(q->len >= q->limit){
-		iunlock(q);
 		freeblist(b);
+		iunlock(q);
 		return -1;
 	}
 	if(q->state & Qclosed){
 		len = BALLOC(b);
-		iunlock(q);
 		freeblist(b);
+		iunlock(q);
 		return len;
 	}
 
@@ -632,8 +632,8 @@ qpassnolim(Queue *q, Block *b)
 
 	if(q->state & Qclosed){
 		len = BALLOC(b);
-		iunlock(q);
 		freeblist(b);
+		iunlock(q);
 		return len;
 	}
 
