71e2a06ca534ed81acd667ae302916fd5baa2fb2
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:46:10 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2008-12-17"
    This reverts commit 387c4c53c623eb3009f51a4454197017e92468df.

--- sys/src/ape/lib/ap/posix/sysconf.c
+++ sys/src/ape/lib/ap/posix/sysconf.c
@@ -1,4 +1,3 @@
-#include	<stdio.h>
 #include	<unistd.h>
 #include	<limits.h>
 #include	<time.h>
@@ -34,8 +33,6 @@ sysconf(int name)
 #endif
 	case _SC_VERSION:
 		return _POSIX_VERSION;
-	case _SC_LOGIN_NAME_MAX:
-		return L_cuserid;
 	}
 	errno = EINVAL;
 	return -1;
