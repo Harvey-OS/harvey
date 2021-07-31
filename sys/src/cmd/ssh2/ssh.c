33416e3125b5e060aa96140954b90b9028175d27
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:39:50 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2012-04-25"
    This reverts commit a3fbd16063946e19237285db486672a21b4a1977.

--- sys/src/cmd/ssh2/ssh.c
+++ sys/src/cmd/ssh2/ssh.c
@@ -599,6 +599,11 @@ doauth(int cfd1, char *whichkey)
 	}
 	if (nopw)
 		return -1;
+	/*
+	up = auth_getuserpasswd(iflag? auth_getkey: nil,
+		"proto=pass service=ssh server=%q user=%q !password?",
+		remote, user);
+	 */
 	up = auth_getuserpasswd(iflag? auth_getkey: nil,
 		"proto=pass service=ssh server=%q user=%q", remote, user);
 	if (up == nil) {
@@ -608,7 +613,6 @@ doauth(int cfd1, char *whichkey)
 	n = fprint(cfd1, "ssh-userauth k %s %s", user, up->passwd);
 	if (n >= 0)
 		return 0;
-
 	path[0] = '\0';
 	fd2path(cfd1, path, sizeof path);
 	fprint(2, "%s: auth ctl msg `ssh-userauth k %s <password>' for %s: %r\n",
