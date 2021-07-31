15d2370ad965fe4b0f316c3b966505531c6afeb9
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:46:33 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2008-11-04"
    This reverts commit b51d2625c40d69189e832341e18a1588449a10c1.

--- sys/src/cmd/ip/httpd/redirect.c
+++ sys/src/cmd/ip/httpd/redirect.c
@@ -161,7 +161,7 @@ redirect(HConnect *hc, char *path)
 		redir = lookup(redirtab, path);
 		*s = c;
 		if(redir != nil){
-			repl = redir->repl;	/* leave decorations on */
+			repl = undecorated(redir->repl);
 			n = strlen(repl) + strlen(s) + 2 + UTFmax;
 			newpath = halloc(hc, n);
 			snprint(newpath, n, "%s%s", repl, s);
