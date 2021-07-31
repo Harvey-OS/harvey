d992851f7f41f7c9f52ca480bbc8890b0d066935
officialplan9foundationofficial <p9f@bell-labs.co>
2021-07-31 14:37:22 +0200

    Revert "Plan 9 from Bell Labs Fourth Edition 2013-09-05"
    This reverts commit 6fb99756e632c541fd805bd4b1f9559e2ab276e9.

--- sys/src/9/port/proc.c
+++ sys/src/9/port/proc.c
@@ -419,7 +419,7 @@ ready(Proc *p)
 		return;
 	}
 
-	if(up != p && (p->wired == nil || p->wired == m))
+	if(up != p)
 		m->readied = p;	/* group scheduling */
 
 	updatecpu(p);
@@ -505,7 +505,6 @@ runproc(void)
 
 	/* cooperative scheduling until the clock ticks */
 	if((p=m->readied) && p->mach==0 && p->state==Ready
-	&& (p->wired == nil || p->wired == m)
 	&& runq[Nrq-1].head == nil && runq[Nrq-2].head == nil){
 		skipscheds++;
 		rq = &runq[p->priority];
@@ -849,7 +848,7 @@ tsleep(Rendez *r, int (*fn)(void*), void *arg, ulong ms)
 		nexterror();
 	}
 	sleep(r, tfn, arg);
-	if(up->tt)
+	if (up->tt)
 		timerdel(up);
 	up->twhen = 0;
 	poperror();
