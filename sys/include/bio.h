da9b10d17db00671258a6cb6d4f6d5755264f09c
Graham MacDonald <grahamamacdonald@gmail.com>
2021-08-11 19:48:25 +0100

    Add Bfdopen() to libbio (#1152)
    Required for git9.
    From plan9port via 9front (commit 035aacf6f5c23f0513e240046818ef203e8d87cb).
    
    Signed-off-by: Graham MacDonald grahamamacdonald@gmail.com

--- sys/include/bio.h
+++ sys/include/bio.h
@@ -58,6 +58,7 @@ int	Binits(Biobufhdr*, int, int, uchar*, int);
 int	Blinelen(Biobufhdr*);
 vlong	Boffset(Biobufhdr*);
 Biobuf*	Bopen(char*, int);
+Biobuf*	Bfdopen(int, int);
 int	Bprint(Biobufhdr*, char*, ...);
 int	Bvprint(Biobufhdr*, char*, va_list);
 int	Bputc(Biobufhdr*, int);
