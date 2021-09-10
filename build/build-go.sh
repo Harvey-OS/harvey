6434bfe8a959403a17cf2c7e92b7e3f1b23b6a7c
John Floren <john.floren@gravwell.io>
2021-09-09 22:02:02 -0700

    Rework the way we include Go. No more HARVEY_GOLANG. If you want go, run build-go.sh or just drop something into ../sys/go
    Signed-off-by: John Floren <john.floren@gravwell.io>

--- build/build-go.sh
+++ build/build-go.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Fetches the Go source and compiles it for Plan 9
-# Run this from the top level of the Harvey tree.
-git clone --depth 1 --branch go1.16.6 https://github.com/golang/go.git
+cd ../sys/
+if [ ! -e go ]
+then
+	git clone --depth 1 --branch go1.16.6 https://github.com/golang/go.git
+fi
 cd go/src
-GOOS=plan9 GOARCH=amd64 ./make.bash
+GOROOT_FINAL=/sys/go GOOS=plan9 GOARCH=386 ./make.bash
+GOROOT_FINAL=/sys/go GOOS=plan9 GOARCH=amd64 ./make.bash
+
