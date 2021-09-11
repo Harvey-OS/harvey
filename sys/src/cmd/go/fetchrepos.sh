5aa0cf9f6eaf0d7f119d2145c099a88d483ceddd
Ronald G. Minnich <rminnich@gmail.com>
2021-09-11 12:56:25 -0700

    Allow for more Go commands
    The format of the command file changes slightly.
    The first field is the directory to cd into to build the command.
    The rest are the git URL and directory name as before.
    
    Some URLs comtain many commants.
    Only commands with a URL are used in the fetch.
    Lines like this:
    name ...
    are not used in a fetch, only a build.
    
    To simplify the build step, use build -o /$objtype/bin/
    instead of go install, so even names of the form a/b/c
    will work easily.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- sys/src/cmd/go/fetchrepos.sh
+++ sys/src/cmd/go/fetchrepos.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-while read line
+grep git: commands | while read line
 do
-	dest=`echo $line | awk '{print $2}'`
+	dest=`echo $line | awk '{print $3}'`
+	cmd=`echo $line | awk '{print $2 " " $3}'`
 	if [ ! -e $dest ]
 	then
-		echo git clone --depth 1 $line
-		git clone --depth 1 $line
+		echo git clone --depth 1 $cmd
+		git clone --depth 1 $cmd
 	fi
-done < commands
+done
